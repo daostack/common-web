90a25e49daf0ad8e4467519a2aaadd215d5036d3
jelle <jellegerbrandy@gmail.com>
2020-06-02 15:00:17 +0200

    update endpoint

--- packages/common-firebase/functions/settings.js
+++ packages/common-firebase/functions/settings.js
@@ -1,5 +1,5 @@
-const graphHttpLink = 'https://api.thegraph.com/subgraphs/name/daostack/v8_1_exp_xdai'
-const graphwsLink = 'wss://api.thegraph.com/subgraphs/name/daostack/v8_1_exp_xdai'
+const graphHttpLink = 'https://api.thegraph.com/subgraphs/name/daostack/v8_2_exp_xdai'
+const graphwsLink = 'wss://api.thegraph.com/subgraphs/name/daostack/v8_2_exp_xdai'
 
 module.exports = {
     graphwsLink,
