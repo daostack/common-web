5e8aba896daf2a34b396a8352a7d333b19ae44b6
Jelle <jellegerbrandy@gmail.com>
2020-08-07 12:57:35 +0200

    Merge pull request #92 from daostack/CM-713-get-current-balance-from-mangopay
    CM-713 get current balance from mangopay

--- packages/common-firebase/functions/graphql/updateDAOBalance.js
+++ packages/common-firebase/functions/graphql/updateDAOBalance.js
@@ -1,34 +1,35 @@
-const abi = require('../relayer/abi.json');
-const { env } = require('../env');
-const ethers = require('ethers');
-const admin = require('firebase-admin');
-const { provider } = require("../settings.js")
-
-const db = admin.firestore()
-
-// async function getBalance(address) {
-//     const balance = await provider.getBalance(address)
-//     console.log(`Balance of ${address} is ${balance}`)
-//     return balance.toNumber()
-// }
-
-
-// get the balance ${address} of the CommonToken
-async function getBalance (address) {
-    const contract = new ethers.Contract(env.commonInfo.commonToken, abi.CommonToken, provider);
-    const balance = await contract.balanceOf(address);
-    console.log(`Balance of ${address} is ${balance}`)
-    return balance.toNumber();
-}
+const { mangopayClient } = require('../util/mangoPay');
+const db = require('firebase-admin').firestore();
 
 async function updateDAOBalance(daoId) {
-    const address = daoId
-    const balance = await getBalance(address)
-    await db.collection('daos').doc(daoId).set({balance}, {merge: true})
-    return {balance}
+    const daoWallet = await getCurrentDaoWallet(daoId);
+
+    await db.collection('daos').doc(daoId).set({
+        balance: daoWallet.Balance.Amount
+    }, {merge: true})
+
+    return {
+        balance: daoWallet.Balance.Amount
+    }
+}
+
+const getCurrentDaoWallet = async (daoId) => {
+    const dao = (await db.collection('daos')
+      .doc(daoId).get()).data();
+
+    return mangopayClient.Wallets.get(dao.mangopayWalletId);
+};
+
+const getBalance = async (daoId) => {
+    const wallet = await getCurrentDaoWallet(daoId);
+
+    return {
+        balance: wallet.Balance.Amount
+    }
 }
 
 module.exports = {
     getBalance,
-    updateDAOBalance
+    updateDAOBalance,
+    getCurrentDaoWallet
 }
---
+++ packages/common-firebase/functions/util/mangoPay.js
@@ -0,0 +1,15 @@
+const mangopay = require('mangopay2-nodejs-sdk');
+const { env } = require('../env');
+
+const mangopayClient = new mangopay({
+  clientId: env.mangopay.clientId,
+  clientApiKey: env.mangopay.apiKey,
+
+  // Set this base url when using it in production.
+  // By default the sandbox api is used
+  // baseUser: 'https://api.mangopay.com'
+});
+
+module.exports = {
+  mangopayClient
+};
