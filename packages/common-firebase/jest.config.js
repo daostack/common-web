f20c228f87ebf1a13ffa6e1ca2eb57c190840bcd
jelle <jellegerbrandy@gmail.com>
2020-09-04 17:49:44 +0200

    reorganize so that deploymnet works

--- packages/common-firebase/jest.config.js
+++ packages/common-firebase/jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   "moduleNameMapper": {
     "^@root(.*)$": "<rootDir>/$1",
-    "^@env(.*)$": "<rootDir>/env/$1",
+    "^@env(.*)$": "<rootDir>/functions/env/$1",
     "^@util(.*)$": "<rootDir>/functions/util/$1",
     "^@functions(.*)$": "<rootDir>/functions/$1",
     "^@helpers(.*)$": "<rootDir>/__tests__/helpers/$1",
