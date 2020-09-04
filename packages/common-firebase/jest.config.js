eee4c87968c1c023fd1213325e34342be0338377
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-09-04 15:55:39 +0300

    Jest fix and actions change (#122)
    * Initial Jest configuration
    
    * Add badges and more
    
    * Add tests form email templating
    
    * Refresh badges
    
    * WIP - check message
    
    Created the skeleton for testing the application in CI environment. One problem that we run into is that to initialise the firebase app (needed for running the tests) we need some secrets that are not available in GitHub.
    
    * Add documentation for testing
    
    * Add documentation for testing
    
    * Dev environment
    
    * Working jest action
    
    * Actions hanging fix?
    
    * Fix jest setup
    
    * Run actions on pull_request and push to dev branch

--- packages/common-firebase/jest.config.js
+++ packages/common-firebase/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testMatch: [
-    // '**/__tests__/**/*.ts',
     '**/?(*.)+(spec|test).ts'
   ],
   preset: 'ts-jest',
@@ -10,10 +9,10 @@ module.exports = {
     'text',
     'lcov'
   ],
-  // setupFilesAfterEnv: ['<rootDir>/__tests__/setup.tests.ts'],
   "moduleNameMapper": {
     "^@root(.*)$": "<rootDir>/$1",
     "^@env(.*)$": "<rootDir>/env/$1",
+    "^@util(.*)$": "<rootDir>/functions/util/$1",
     "^@functions(.*)$": "<rootDir>/functions/$1",
     "^@helpers(.*)$": "<rootDir>/__tests__/helpers/$1",
   }
