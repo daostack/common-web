1859926d0324f20194f7f08aefe48a3e304beeab
Jelle <jellegerbrandy@gmail.com>
2020-10-20 14:49:54 +0200

    Hotfix more retries (#189)
    * More reties for common creation
    
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>
    
    # Conflicts:
    #	functions/src/creation/common.js
    
    * Retry to update the dao as well
    
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>
    
    * more info in log
    
    Co-authored-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

--- packages/common-firebase/functions/src/graphql/dao.js
+++ packages/common-firebase/functions/src/graphql/dao.js
@@ -204,9 +204,10 @@ async function updateDaoById(daoId, customRetryOptions = {}) {
     throw new CommonError(`You must provide a daoId (current value is "${daoId}")`);
   }
   daoId = daoId.toLowerCase();
-  const dao = await promiseRetry(
+
+  const res =  await promiseRetry(
     async (retryFunc, number) => {
-      console.log(`Try #${number} to get Dao...`);
+      console.log(`Try #${number} to get Dao ${daoId}...`);
       const currDaosResult = await arc.daos({ where: { id: daoId } }, { fetchPolicy: 'no-cache' }).first();
 
       if(number > 7) {
@@ -220,21 +221,23 @@ async function updateDaoById(daoId, customRetryOptions = {}) {
       if (!currDaosResult[0].coreState.metadata) {
         retryFunc(`The dao with id "${daoId}" has no metadata`);
       }
-      return currDaosResult[0];
+
+      const dao = currDaosResult[0];
+
+      // @todo: _updateDaoDb should throw en error, not return error messages
+      const { updatedDoc, errorMsg } = await _updateDaoDb(dao);
+
+      if (errorMsg) {
+        throw new CommonError(errorMsg);
+      }
+
+      console.log('UPDATED DAO WITH ID: ', daoId);
+      return updatedDoc;
     },
     { ...retryOptions, ...customRetryOptions }
   );
 
-  // TODO: _updateDaoDb should throw en error, not ereturn error messages
-  const { updatedDoc, errorMsg } = await _updateDaoDb(dao);
-  if (errorMsg) {
-    console.error(`Dao update failed for id: ${dao.id}!`);
-    console.error(errorMsg);
-    throw new CommonError(errorMsg);
-  }
-  console.log('UPDATED DAO WITH ID: ', daoId);
-  console.log('----------------------------------------------------------');
-  return updatedDoc;
+  return res;
 }
 
 module.exports = {
