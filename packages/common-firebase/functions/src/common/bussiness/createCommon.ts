ece44b1941db62875d738d06a8b4bb9be8e60837
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-11-06 08:18:02 +0200

    Create common works
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

--- packages/common-firebase/functions/src/common/bussiness/createCommon.ts
+++ packages/common-firebase/functions/src/common/bussiness/createCommon.ts
@@ -3,6 +3,7 @@ import { ICommonEntity } from '../types';
 import { validate } from '../../util/validate';
 import { commonDb } from '../database';
 
+// The validation schema for creating commons (and creating typings by inferring them)
 const createCommonDataValidationScheme = yup.object({
   userId: yup.string().defined(),
 
@@ -10,7 +11,7 @@ const createCommonDataValidationScheme = yup.object({
 
   image: yup.string()
     .url()
-    .nullable(),
+    .defined(),
 
   action: yup.string().defined(),
   byline: yup.string().defined(),
@@ -27,6 +28,17 @@ const createCommonDataValidationScheme = yup.object({
 
 type CreateCommonPayload = yup.InferType<typeof createCommonDataValidationScheme>
 
+/**
+ * Creates common for the provided payload. Please note that this is
+ * not *Authenticated* so use it with care
+ *
+ * @throws { ValidationError } - If there are validation errors in the payload
+ * @throws { NotFoundError } - If there was no user found for the user id
+ *
+ * @param payload - The data from witch the common will be created
+ *
+ * @returns - The created common
+ */
 export const createCommon = async (payload: CreateCommonPayload): Promise<ICommonEntity> => {
   await validate<CreateCommonPayload>(payload, createCommonDataValidationScheme);
 
@@ -40,6 +52,8 @@ export const createCommon = async (payload: CreateCommonPayload): Promise<ICommo
     contributionType,
     contributionAmount
   } = payload;
+
+  // @todo Check if user exists
 
   const common = await commonDb.addCommon({
     name,
