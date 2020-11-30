f263e6217a67944a7f15975f03c740f4f3dee58e
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-11-30 15:38:52 +0200

    CM-1356 validation chage (#259)
    * Remove the description validation on request to join
    
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>
    
    * Ooops
    
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

--- packages/common-firebase/functions/src/proposals/business/createJoinRequest.ts
+++ packages/common-firebase/functions/src/proposals/business/createJoinRequest.ts
@@ -31,10 +31,6 @@ const createRequestToJoinValidationSchema = yup.object({
     .string()
     .required(),
 
-  description: yup
-    .string()
-    .required(),
-
   funding: yup
     .number()
     .required(),
@@ -112,7 +108,7 @@ export const createJoinRequest = async (payload: CreateRequestToJoinPayload): Pr
     type: 'join',
 
     description: {
-      description: payload.description,
+      description: `Request to join ${payload.commonId} from ${payload.proposerId}`,
       links: payload.links as Nullable<IProposalLink[]> || []
     },
 
