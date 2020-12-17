9c1d7eaa8bcd6eafb1db76b76d4f1cc80f596cb9
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-12-17 10:53:43 +0200

    Revert "Oh"
    This reverts commit 3edc1ce0def7afe1515459e5ac2e0176f99080ef.

--- packages/common-firebase/functions/src/event/index.ts
+++ packages/common-firebase/functions/src/event/index.ts
@@ -32,6 +32,6 @@ const processEvent = async (event: IEventModel) => {
 exports.commonEventListeners = functions
   .firestore
   .document('/event/{id}')
-  .onCreate(async (snap) => {
-    await processEvent(snap.data() as IEventModel)
+  .onUpdate(async (snap) => {
+    await processEvent(snap.after.data() as IEventModel)
   })
