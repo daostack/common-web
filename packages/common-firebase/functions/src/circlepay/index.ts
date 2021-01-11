a1e7d9e7797de55b0b2e9be6909e13a19f8a0bf7
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-01-11 15:57:52 +0200

    Small cleanup (#322)

--- packages/common-firebase/functions/src/circlepay/index.ts
+++ packages/common-firebase/functions/src/circlepay/index.ts
@@ -1,20 +1,14 @@
 import * as functions from 'firebase-functions';
-import request from 'request';
-import axios from 'axios';
 import { v4 } from 'uuid';
+import axios from 'axios';
 
 import * as payoutCrons from './payouts/crons';
 
 import { commonApp, commonRouter, externalRequestExecutor } from '../util';
 import { responseExecutor } from '../util/responseExecutor';
-import { ICircleNotification } from '../util/types';
-import { CommonError } from '../util/errors';
 import { circlePayApi, getSecret } from '../settings';
+import { ArgumentError } from '../util/errors';
 import { ErrorCodes } from '../constants';
-
-
-import { handleNotification } from './notifications/bussiness/handleNotification';
-import { subscribeToNotifications } from './notifications/bussiness/subscribeToNotifications';
 
 import { createCard } from './cards/business/createCard';
 import { createBankAccount } from './backAccounts/bussiness/createBankAccount';
@@ -23,9 +17,6 @@ import { approvePayout } from './payouts/business/approvePayout';
 import { createProposalPayout } from './payouts/business/createProposalPayout';
 import { createIndependentPayout } from './payouts/business/createIndependentPayout';
 import { updatePaymentFromCircle } from './payments/business/updatePaymentFromCircle';
-import { updatePaymentsFromCircle } from './payments/business/updatePaymentsFromCircle';
-import { updatePayments } from './payments/helpers';
-import { updatePaymentStructure } from './payments/helpers/converter';
 
 const runtimeOptions = {
   timeoutSeconds: 540
@@ -87,32 +78,21 @@ circlepay.get('/encryption', async (req, res, next) => {
 });
 
 // ----- Payment Related requests
-circlepay.get('/payments/update/structure', async (req, res, next) => {
-  await responseExecutor(async () => {
-    const trackId = req.requestId || v4();
-
-    logger.notice('User requested update for all payments structure', {
-      userId: req.user?.uid
-    });
-
-    await updatePayments(trackId);
-  }, {
-    req,
-    res,
-    next,
-    successMessage: 'Payment update succeeded'
-  });
-});
 
 circlepay.get('/payments/update/data', async (req, res, next) => {
   await responseExecutor(async () => {
     const trackId = req.requestId || v4();
+    const paymentId = req.query.paymentId;
+
+    if (!paymentId) {
+      throw new ArgumentError('paymentId');
+    }
 
     logger.notice('User requested update for all payments from circle', {
       userId: req.user?.uid
     });
 
-    await updatePaymentsFromCircle(trackId);
+    await updatePaymentFromCircle(paymentId as string, trackId);
   }, {
     req,
     res,
@@ -135,55 +115,6 @@ circlepay.get('/testIP', async (req, res, next) => {
       next,
       successMessage: `Test Ip generated`
     });
-});
-
-circlepay.post('/notification/ping', async (req, res, next) => {
-  logger.info('Received notification from Circle');
-
-  await responseExecutor(async () => {
-    const envelope = JSON.parse(req.body);
-
-    if (envelope.Type === 'SubscriptionConfirmation') {
-      logger.info('Trying to confirm subscription!', envelope.SubscribeURL);
-
-      request(envelope.SubscribeURL, (err) => {
-        if (err) {
-          throw new CommonError('Something wrong happened verifying the request', {
-              error: err,
-              errorString: JSON.stringify(err)
-            }
-          );
-        }
-
-        logger.info('Successfully subscribed to the notifications!');
-      });
-    } else if (envelope.Type === 'Notification') {
-      await handleNotification(JSON.parse(envelope.Message) as ICircleNotification);
-    } else {
-      throw new CommonError(`Unsupported type: ${envelope.Type}`, {
-        envelope,
-        envelopeString: JSON.stringify(envelope)
-      });
-    }
-
-    logger.info('Successfully handled notification');
-  }, {
-    req,
-    res,
-    next,
-    successMessage: 'Successfully handled notification'
-  });
-});
-
-circlepay.post('/notification/register', async (req, res, next) => {
-  await responseExecutor(async () => {
-    return await subscribeToNotifications();
-  }, {
-    req,
-    res,
-    next,
-    successMessage: 'Endpoints registered!'
-  });
 });
 
 // ----- Bank Accounts
@@ -262,8 +193,6 @@ export const circlePayApp = functions
   .runWith(runtimeOptions)
   .https.onRequest(commonApp(circlepay, {
     unauthenticatedRoutes: [
-      '/payments/update/structure',
-      '/payments/update/data',
       '/payouts/approve',
       '/charge/subscription',
       '/payouts/create',
--- packages/common-firebase/functions/src/circlepay/payments/helpers/index.ts
+++ packages/common-firebase/functions/src/circlepay/payments/helpers/index.ts
@@ -1,6 +1,4 @@
 export { isFinalized, isSuccessful } from './statusHelper';
 
-export { updatePayments } from './converter';
-
 export { feesHelper } from './feesHelper';
 export { failureHelper } from './failureHelper';
