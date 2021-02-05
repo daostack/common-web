85ba55a2b22c5128fe866b7623cece61e1d674c0
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-02-05 10:03:38 +0200

    Fix tests

--- packages/firebase/functions/src/circlepay/payments/tests/data/testPaymentPayloads.ts
+++ packages/firebase/functions/src/circlepay/payments/tests/data/testPaymentPayloads.ts
@@ -2,7 +2,6 @@ export const createPaymentInvalidPayload = {
   userId: 'ae6b689c-5dcb-41ed-84a7-51e6ce7faa76',
   cardId: 'ae6b689c-5dcb-41ed-84a7-51e6ce7faa76',
   proposalId: 'ae6b689c-5dcb-41ed-84a7-51e6ce7faa76',
-  ipAddress: '127.0.0.1',
   sessionId: 'ae6b689c-5dcb-41ed-84a7-51e6ce7faa76',
   amount: 10000,
   type: 'subscription'
@@ -21,11 +20,9 @@ export const createPaymentValidPayload = {
 export const createProposalPaymentInvalidPayload = {
   proposalId: 'malformed-proposal-id',
   sessionId: 'ae6b689c-5dcb-41ed-84a7-51e6ce7faa76',
-  ipAddress: '127.0.0.1'
 };
 
 export const createProposalPaymentValidPayload = {
   proposalId: 'ae6b689c-5dcb-41ed-84a7-51e6ce7faa76',
   sessionId: 'ae6b689c-5dcb-41ed-84a7-51e6ce7faa76',
-  ipAddress: '127.0.0.1'
 };
