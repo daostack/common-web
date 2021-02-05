4e58cc31eb8b6e1ceeaabec583e3041e6ffd8ab9
watchtower <30501647+watchtower314@users.noreply.github.com>
2021-02-05 07:35:33 +0200

    update discussion in backend (#20)

--- packages/firebase/functions/src/discussion/database/index.ts
+++ packages/firebase/functions/src/discussion/database/index.ts
@@ -1,9 +1,11 @@
 import { db } from '../../util';
 import { Collections } from '../../constants';
 import { getDiscussion } from './getDiscussion';
+import { updateLastMessage } from './updateLastMessage';
 
 export const discussionCollection = db.collection(Collections.Discussion);
 
 export const discussionDb = {
-	getDiscussion
+	getDiscussion,
+	updateLastMessage,
 }
