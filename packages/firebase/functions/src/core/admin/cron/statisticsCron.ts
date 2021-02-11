4da4af162b746ca1920f44836a09e0e1d80e033b
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-02-11 15:09:31 +0200

    Rule bending

--- packages/firebase/functions/src/core/admin/cron/statisticsCron.ts
+++ packages/firebase/functions/src/core/admin/cron/statisticsCron.ts
@@ -4,5 +4,5 @@ import * as functions from 'firebase-functions';
 export const createDailyStatistic = functions.pubsub
   .schedule('0 0 */1 * *')
   .onRun(async () => {
-    const timestamp = (new Date().setUTCHours(0, 0, 0, 0));
+    // const timestamp = (new Date().setUTCHours(0, 0, 0, 0));
   });
