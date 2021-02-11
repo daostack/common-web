244c10156963d8f1ea3739e1621cd21759032278
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-02-11 15:40:29 +0200

    Fix build

--- packages/admin/src/pages/index.tsx
+++ packages/admin/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { withSidebar } from '../hoc/withSidebar';
 import React from 'react';
 
 const IndexPage: NextPage = () => {
