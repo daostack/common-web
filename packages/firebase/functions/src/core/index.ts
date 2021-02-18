38048536ab625c96c2dc53134cd329e181e5ee5f
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-02-18 09:16:34 +0200

    Add the context to each graph request
    Resolves #100
    
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

--- packages/firebase/functions/src/core/index.ts
+++ packages/firebase/functions/src/core/index.ts
@@ -1,18 +1,42 @@
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
+import { auth } from 'firebase-admin';
 import { ApolloServer } from 'apollo-server-express';
 
 import { schema } from './graph';
+import { sessions, ipMiddleware } from '../util/middleware';
+
+
+export interface IRequestContext {
+  request: {
+    id: string;
+    ip: string;
+  }
+  getUserId: () => Promise<string>
+}
 
 const app = express();
 const apollo = new ApolloServer({
-  schema
+  schema,
+  context: ({ req }): IRequestContext => ({
+    request: {
+      id: req.requestId,
+      ip: req.ipAddress
+    },
+
+    getUserId: async () => {
+      return (await auth().verifyIdToken(req.headers.authorization)).uid;
+    }
+  })
 });
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(sessions);
+app.use(ipMiddleware);
 
 apollo.applyMiddleware({ app });
 
