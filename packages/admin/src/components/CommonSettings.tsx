3b46af8d50b0715bd21328dc6f0598f6ca883c29
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-02-25 11:20:43 +0200

    Finish the UI #122

--- packages/admin/src/components/CommonSettings.tsx
+++ packages/admin/src/components/CommonSettings.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
 
-import { Tooltip, Row } from '@geist-ui/react';
+import { gql } from '@apollo/client';
+import { Tooltip, Row, useToasts } from '@geist-ui/react';
 import { Settings, Award } from '@geist-ui/react-icons';
+
 import { Centered } from '@components/Centered';
+import { useWhitelistCommonMutation } from '@graphql';
 
 interface ICommonSettingsProps {
   commonId: string;
 }
 
-export const CommonSettings: React.FC<ICommonSettingsProps> = ({ commonId }) => {
-  const onCommonWhitelisted = () => {
-
+const WhitelistCommonMutation = gql`
+  mutation whitelistCommon($commonId: ID!) {
+    whitelistCommon(commonId: $commonId)
   }
+`;
+
+export const CommonSettings: React.FC<ICommonSettingsProps> = ({ commonId }) => {
+  const [, setToast] = useToasts();
+
+  // - Networking
+  const [whitelistCommon, { data }] = useWhitelistCommonMutation();
+
+  // - Actions
+  const onCommonWhitelisted = async () => {
+    await whitelistCommon({
+      variables: {
+        commonId
+      }
+    });
+  };
+
+  // - Effects
+  React.useEffect(() => {
+    if (data?.whitelistCommon) {
+      setToast({
+        text: 'Common whitelisted!'
+      });
+    }
+  }, [data]);
 
   return (
     <Tooltip
       placement="bottomEnd"
       trigger="click"
       text={(
-        <div style={{ width: "250px" }}>
-          <Row style={{ cursor: 'pointer' }}>
-            <Award />{'  '}
+        <div style={{ width: '250px' }}>
+          <Row style={{ cursor: 'pointer' }} onClick={onCommonWhitelisted}>
+            <Award/>{'  '}
             Whitelist common
           </Row>
 
@@ -29,7 +57,7 @@ export const CommonSettings: React.FC<ICommonSettingsProps> = ({ commonId }) => 
     >
       <div style={{ width: 40 }}>
         <Centered>
-          <Settings />
+          <Settings/>
         </Centered>
       </div>
     </Tooltip>
