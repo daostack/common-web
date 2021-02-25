d0e884309b58de131f40e28de12399122ea2a571
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-02-25 12:17:10 +0200

    Change the common settings

--- packages/admin/src/components/CommonSettings.tsx
+++ packages/admin/src/components/CommonSettings.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import { gql } from '@apollo/client';
-import { Tooltip, Row, useToasts } from '@geist-ui/react';
-import { Settings, Award } from '@geist-ui/react-icons';
+import { Tooltip, Row, useToasts, Spacer } from '@geist-ui/react';
+import { Settings, Award, RefreshCcw, Edit, Trash2 } from '@geist-ui/react-icons';
 
 import { Centered } from '@components/Centered';
 import { useWhitelistCommonMutation } from '@graphql';
@@ -46,12 +46,36 @@ export const CommonSettings: React.FC<ICommonSettingsProps> = ({ commonId }) => 
       placement="bottomEnd"
       trigger="click"
       text={(
-        <div style={{ width: '250px' }}>
+        <div style={{ width: '250px', margin: '.7rem 0' }}>
           <Row style={{ cursor: 'pointer' }} onClick={onCommonWhitelisted}>
-            <Award/>{'  '}
-            Whitelist common
+            <Award/>
+            <Spacer x={.5} />
+            Whitelist Common
           </Row>
 
+          <Spacer y={.7} />
+
+          <Row style={{ cursor: 'pointer' }}>
+            <RefreshCcw/>
+            <Spacer x={.5} />
+            Refresh Common Members
+          </Row>
+
+          <Spacer y={.7} />
+
+          <Row style={{ cursor: 'pointer' }}>
+            <Edit/>
+            <Spacer x={.5} />
+            Edit Common Details
+          </Row>
+
+          <Spacer y={.7} />
+
+          <Row style={{ cursor: 'pointer', color: 'red' }}>
+            <Trash2 />
+            <Spacer x={.5} />
+            Delete Common
+          </Row>
         </div>
       )}
     >
