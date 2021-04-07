e73410ffb53b4ddb78a9d04b90b61ff967d0c8a6
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-04-07 10:34:10 +0300

    Remove junk
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

--- packages/graphql/src/schema/Types/Votes/Enums/VoteOutcome.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VoteOutcomeEnum = void 0;
-var client_1 = require("@prisma/client");
-var nexus_1 = require("nexus");
-exports.VoteOutcomeEnum = nexus_1.enumType({
-    name: 'VoteOutcome',
-    members: client_1.VoteOutcome
-});
