2d4e6342b87df520aa153927524b654dc5771185
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-04-17 08:37:44 +0300

    Max active user tokens guard
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

--- packages/core/src/services/users/commands/createUserNotificationTokenCommand.ts
+++ packages/core/src/services/users/commands/createUserNotificationTokenCommand.ts
@@ -1,8 +1,9 @@
 import * as z from 'zod';
-import { UserNotificationToken } from '@prisma/client';
+import { UserNotificationToken, UserNotificationTokenState } from '@prisma/client';
 
 import { logger } from '@logger';
 import { prisma } from '@toolkits';
+import { CommonError } from '@errors';
 
 const schema = z.object({
   userId: z.string()
@@ -20,6 +21,22 @@ export const createUserNotificationTokenCommand = async (payload: z.infer<typeof
   schema.parse(payload);
 
   logger.debug('Creating user notification token from valid payload');
+
+  // Check how many active notification tokens user has
+  const activeUserTokensCount = await prisma.userNotificationToken.count({
+    where: {
+      userId: payload.userId,
+      state: UserNotificationTokenState.Active
+    }
+  });
+
+  if (activeUserTokensCount >= 10) {
+    throw new CommonError(
+      'User cannot have more than 10 active notification tokens. To add ' +
+      'new tokens, please deactivate or delete some of the old ones!', {
+        userId: payload.userId
+      });
+  }
 
   // Create the token
   const notificationToken = await prisma.userNotificationToken.create({
