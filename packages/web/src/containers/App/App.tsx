058b1fe7a60cd0b586570b6ddb2342347ce34f2f
Roie Natan <roie.natan@gmail.com>
2021-04-19 11:23:53 +0300

    remove unecessary dashboard file

--- packages/web/src/containers/App/App.tsx
+++ packages/web/src/containers/App/App.tsx
@@ -1,30 +1,24 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-//import { useSelector } from "react-redux";
 import { AuthContainer } from "../Auth";
 import PrivateRoute from "./PrivateRoute";
-//import { authentificated } from "../Auth/store/selectors";
-import { Content, NotFound, Footer, Header } from "../../shared/components"; // Dashboard
+import { Content, NotFound, Footer, Header } from "../../shared/components";
 import { CommonContainer } from "../Common";
 import { LandingContainer } from "../Landing";
 import { ROUTE_PATHS } from "../../shared/constants";
 
 const App = () => {
-  //const is_authorized = useSelector(authentificated());
-
   return (
     <div className="App">
       <Header />
       <Content>
         <Switch>
           <Route path={ROUTE_PATHS.AUTH} component={AuthContainer} />
-          {/* <PrivateRoute path="/" exact component={Dashboard} authentificated={is_authorized} /> */}
           <Route path="/" exact component={LandingContainer} />
           <PrivateRoute path={ROUTE_PATHS.COMMON_LIST} component={CommonContainer} authentificated={true} />
           <Route component={NotFound} />
         </Switch>
       </Content>
-
       <Footer />
     </div>
   );
