1c2894cd187aee6a872ca34d12ec098a789a81ca
Alexander Ivanov <alexander2001ivanov@gmail.com>
2021-06-03 09:22:13 +0300

    Do not allow the payout to be double approved, or after executed
    Signed-off-by: Alexander Ivanov <alexander2001ivanov@gmail.com>

--- packages/core/src/services/payouts/command/approvePayoutCommand.ts
+++ packages/core/src/services/payouts/command/approvePayoutCommand.ts
@@ -1,9 +1,11 @@
-import { EventType, PayoutApprover, PayoutApproverResponse } from '@prisma/client';
+import { EventType, PayoutApprover, PayoutApproverResponse, PayoutStatus } from '@prisma/client';
 import * as z from 'zod';
+
+import { worker } from '@jobs';
 import { logger } from '@logger';
 import { prisma } from '@toolkits';
 import { eventService } from '@services';
-import { worker } from '@jobs';
+import { CommonError, NotFoundError } from '@errors';
 
 const schema = z.object({
   userId: z.string(),
@@ -14,6 +16,35 @@ const schema = z.object({
 
 export const approvePayoutCommand = async (command: z.infer<typeof schema>): Promise<PayoutApprover> => {
   schema.parse(command);
+
+  // Find the approver and the payout
+  const payout = await prisma.payout
+    .findUnique({
+      where: {
+        id: command.payoutId
+      },
+      include: {
+        approvers: {
+          where: {
+            userId: command.userId
+          }
+        }
+      }
+    });
+
+  if (!payout) {
+    throw new NotFoundError('approvePayoutCommand.payout.id', command.payoutId);
+  }
+
+  // Check if the user has already laid response
+  if (payout.approvers[0].outcome !== PayoutApproverResponse.Pending) {
+    throw new CommonError('Cannot approve one payout more than once');
+  }
+
+  // Check the status of the payout
+  if (payout.status !== PayoutStatus.PendingApproval) {
+    throw new CommonError('Cannot approve payout that is not pending approval');
+  }
 
   logger.info(`Acting on payout with outcome ${command.outcome}`);
 
