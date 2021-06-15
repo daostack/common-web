47970b8a341d70d65f5492417256f78fdfbda85c
Denis Stetskov <botozavreg@gmail.com>
2021-06-15 21:50:22 +0300

    570 fix

--- packages/web/src/shared/hooks/CalculateReadMoreLenght.tsx
+++ packages/web/src/shared/hooks/CalculateReadMoreLenght.tsx
@@ -2,9 +2,9 @@ function useCalculateReadMoreLength() {
   let length = 0;
   const windowWidth = window.innerWidth;
 
-  if (windowWidth >= 1240 || windowWidth <= 1366) {
+  if (windowWidth >= 1240 && windowWidth <= 1366) {
     length = 400;
-  } else if (windowWidth >= 1366 || windowWidth <= 1680) {
+  } else if (windowWidth >= 1366 && windowWidth <= 1680) {
     length = 620;
   } else {
     length = 760;
