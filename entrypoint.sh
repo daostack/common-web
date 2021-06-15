237b8f227c3dbdc640d282d9bd8cb998c49f7276
Alex Hodak <alexhodak@daostack.io>
2021-06-15 09:02:09 +0000

    Moved node server process init from direct to dumb-init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ $1 == "worker" ];
 then
-  cd ./packages/worker && yarn run start
+  cd ./packages/worker && dumb-init node -r dotenv/config dist/index.js
 elif [ $1 == "graphql" ];
 then
-  cd ./packages/graphql && yarn run start
+  cd ./packages/graphql && dumb-init node -r dotenv/config dist/index.js
 else
   echo "Argument not supported"
   exit 1
