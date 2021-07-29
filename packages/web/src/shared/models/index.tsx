e412eb11071d90256c904b18abdf87dc04f2903d
Denis Stetskov <botozavreg@gmail.com>
2021-07-29 10:39:10 +0300

    possible Roie lint fix

--- packages/web/src/shared/models/index.tsx
+++ packages/web/src/shared/models/index.tsx
@@ -1,6 +1,6 @@
 export * from "./User";
 export * from "./Common";
-export * from "./Discussion";
 export * from "./shared";
 export * from "./Proposals";
 export * from "./DiscussionMessage";
+export * from "./Discussion";
