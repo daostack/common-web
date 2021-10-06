ba33c72d76b56b07ef82652a853cb31e33bca6b2
Roie Natan <roie.natan@gmail.com>
2021-10-06 09:26:24 +0400

    logged-in user menu

--- packages/web/src/shared/components/Account/Account.tsx
+++ packages/web/src/shared/components/Account/Account.tsx
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  COMMON_APP_APP_STORE_LINK,
+  COMMON_APP_GOOGLE_PLAY_LINK,
+  MobileOperatingSystem,
+  ROUTE_PATHS,
+} from "../../constants";
+import { useOutsideClick } from "../../hooks";
 
 import { User } from "../../models";
+import { getMobileOperatingSystem } from "../../utils";
 
 import "./index.scss";
 
@@ -11,6 +19,15 @@ interface AccountProps {
 
 const Account = ({ user, logOut }: AccountProps) => {
   const [showMenu, setShowMenu] = useState(false);
+  const wrapperRef = useRef(null);
+  const { isOutside, setOusideValue } = useOutsideClick(wrapperRef);
+
+  useEffect(() => {
+    if (isOutside) {
+      setShowMenu(false);
+      setOusideValue();
+    }
+  }, [isOutside, setShowMenu, setOusideValue]);
 
   const userPic = user.photoURL
     ? user.photoURL
@@ -21,7 +38,19 @@ const Account = ({ user, logOut }: AccountProps) => {
       <img src={userPic} className="avatar" alt="user avatar" />
       <div>{user?.displayName || `${user.firstName} ${user.lastName}`}</div>
       {showMenu && (
-        <div className="menu-wrapper">
+        <div className="menu-wrapper" ref={wrapperRef}>
+          <div onClick={() => (window.location.href = ROUTE_PATHS.MY_COMMONS)}>My Commons</div>
+          <div
+            onClick={() =>
+              window.open(
+                getMobileOperatingSystem() === MobileOperatingSystem.iOS
+                  ? COMMON_APP_APP_STORE_LINK
+                  : COMMON_APP_GOOGLE_PLAY_LINK,
+              )
+            }
+          >
+            Download Common app
+          </div>
           <div onClick={() => logOut()}>Log out</div>
         </div>
       )}
