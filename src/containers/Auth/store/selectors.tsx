7b613e7daa356e166ffe6555fd6bb1705cfeac97
Denis Stetskov <botozavreg@gmail.com>
2021-11-06 16:34:06 +0200

    login rollback wip

--- src/containers/Auth/store/selectors.tsx
+++ src/containers/Auth/store/selectors.tsx
@@ -4,4 +4,9 @@ import { AppState } from "../../../shared/interfaces/State";
 
 const selectAuth = (state: AppState) => state.auth;
 
-export const authentificated = () => createSelector(selectAuth, (state) => state.authentificated);
+export const authentificated = () =>
+  createSelector(selectAuth, (state) => state.authentificated);
+export const selectUser = () =>
+  createSelector(selectAuth, (state) => state.user);
+export const selectIsNewUser = () =>
+  createSelector(selectAuth, (state) => state.isNewUser);
