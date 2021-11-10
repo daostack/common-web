4eeb70614fae1495c61b251517e6e6d3325f3078
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-10 12:19:41 +0300

    Create button component to create common

---
+++ src/containers/Common/components/CommonListContainer/CreateCommonButton/CreateCommonButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { isMobile } from "../../../../../shared/utils";
+import CloseIcon from "../../../../../shared/icons/close.icon";
+import "./index.scss";
+
+const BUTTON_TEXT = 'Create Common';
+
+export default function CreateCommonButton() {
+  const isMobileView = isMobile();
+
+  return (
+    <button className="create-common-button button-blue" aria-label={isMobileView ? BUTTON_TEXT : undefined}>
+      <CloseIcon
+        width={22}
+        height={22}
+        fill="currentColor"
+      />
+      <span className="create-common-button-text">
+        {BUTTON_TEXT}
+      </span>
+    </button>
+  );
+}
