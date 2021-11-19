9d6068dcbe1fc1190db4027ca569fa31bb0c1abc
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-19 12:47:24 +0300

    Update header logic for common creation modal

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/CreationSteps.tsx
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/CreationSteps.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 
-import { ModalHeaderContent } from "../../../../../../shared/components/Modal";
 import { GeneralInfo } from "./GeneralInfo";
-import { Progress } from "./Progress";
 import { CreationStep } from "./constants";
 
 interface CreationStepsProps {
@@ -22,6 +20,14 @@ export default function CreationSteps({ setTitle, setGoBackHandler, onFinish }: 
     setStep(step => step - 1);
   }, [step]);
 
+  const handleFinish = useCallback(() => {
+    if (step === CreationStep.Review) {
+      return;
+    }
+
+    setStep(step => step + 1);
+  }, [step]);
+
   useEffect(() => {
     setTitle("Create a Common");
   }, [setTitle]);
@@ -33,18 +39,11 @@ export default function CreationSteps({ setTitle, setGoBackHandler, onFinish }: 
   const content = useMemo(() => {
     switch (step) {
       case CreationStep.GeneralInfo:
-        return <GeneralInfo />;
+        return <GeneralInfo currentStep={step} onFinish={handleFinish} />;
       default:
         return null;
     }
   }, [step]);
 
-  return (
-    <>
-      <ModalHeaderContent>
-        <Progress creationStep={step} />
-      </ModalHeaderContent>
-      {content}
-    </>
-  );
+  return content;
 }
