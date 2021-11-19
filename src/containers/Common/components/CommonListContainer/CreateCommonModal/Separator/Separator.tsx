37fd4cce5683886df36ba923a975f0891a8988b1
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-19 12:04:53 +0300

    Create separator component

---
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/Separator/Separator.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+import "./index.scss";
+
+export default function Separator(): ReactElement {
+  return <div className="create-common-separator" />;
+}
