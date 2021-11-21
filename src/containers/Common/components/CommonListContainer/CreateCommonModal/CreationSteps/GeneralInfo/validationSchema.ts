c0a2a5b765ef675055573547415ddb07af86e1db
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-21 23:32:56 +0300

    Make links related fields optional

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/validationSchema.ts
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/validationSchema.ts
@@ -18,10 +18,8 @@ const schema = Yup.object().shape({
   links: Yup.array()
     .of(Yup.object().shape({
       title: Yup.string()
-        .max(MAX_LINK_TITLE_LENGTH, 'Entered title is too long')
-        .required('Please enter link title'),
-      link: Yup.string()
-        .required('Please enter a link'),
+        .max(MAX_LINK_TITLE_LENGTH, 'Entered title is too long'),
+      link: Yup.string(),
     }))
     .required('Please add at least 1 link')
     .min(1, 'Please add at least 1 link'),
