64ccce7cc60fbe71e567a1f08c426ab3f4a16be6
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-21 23:17:52 +0300

    Fix about field max length

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/constants.ts
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/constants.ts
@@ -1,4 +1,4 @@
 export const MAX_COMMON_NAME_LENGTH = 49;
 export const MAX_TAGLINE_LENGTH = 89;
-export const MAX_ABOUT_LENGTH = 89;
+export const MAX_ABOUT_LENGTH = 49;
 export const MAX_LINK_TITLE_LENGTH = 30;
