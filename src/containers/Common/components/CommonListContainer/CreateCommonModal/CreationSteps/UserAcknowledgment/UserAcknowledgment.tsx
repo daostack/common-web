807984eb5d0f98ede9348ff6c22f3ddb23e57d61
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-22 13:14:39 +0300

    Display causes link with icon

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/UserAcknowledgment/UserAcknowledgment.tsx
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/UserAcknowledgment/UserAcknowledgment.tsx
@@ -3,8 +3,10 @@ import { Formik, FormikConfig } from "formik";
 import { FormikProps } from "formik/dist/types";
 
 import { isMobile } from "../../../../../../../shared/utils";
+import { ButtonLink } from "../../../../../../../shared/components";
 import { ModalFooter, ModalHeaderContent } from "../../../../../../../shared/components/Modal";
 import { Form } from "../../../../../../../shared/components/Form/Formik";
+import ExplanationIcon from "../../../../../../../shared/icons/explanation.icon";
 import { Separator } from "../../Separator";
 import { Progress } from "../Progress";
 import { CheckedList } from "./CheckedList";
@@ -32,7 +34,9 @@ const LIST_ITEMS = [
   "The purpose of the Common is not in violation of any law, regulation, or 3rd party rights.",
   <>The Common will be raising funds for <strong>non-profit or charitable causes only.</strong> The common is not intended for commercial or for-profit purposes.</>,
   "All Commons and their members must comply with applicable financial and tax obligations.",
-  <>The Common will solely promote one or more of the following Causes.</>
+  <>
+    The Common will solely promote one or more of the following <ButtonLink className="create-common-user-acknowledgment__causes-link">Causes<ExplanationIcon className="create-common-user-acknowledgment__causes-icon" /></ButtonLink>.
+  </>,
 ];
 
 export default function UserAcknowledgment({ currentStep, onFinish }: UserAcknowledgmentProps): ReactElement {
