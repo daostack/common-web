c1421cd4192f139d84fdd763d824a2a07f1a80d4
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-22 15:42:29 +0300

    Add validation for correct link

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/validationSchema.ts
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/validationSchema.ts
@@ -5,26 +5,31 @@ import {
   MAX_TAGLINE_LENGTH,
   MAX_ABOUT_LENGTH,
   MAX_LINK_TITLE_LENGTH,
+  HTTPS_URL_REGEXP,
 } from "./constants";
 
 const schema = Yup.object().shape({
   commonName: Yup.string()
-    .max(MAX_COMMON_NAME_LENGTH, 'Entered common name is too long')
-    .required('Please enter a common name'),
+    .max(MAX_COMMON_NAME_LENGTH, "Entered common name is too long")
+    .required("Please enter a common name"),
   tagline: Yup.string()
-    .max(MAX_TAGLINE_LENGTH, 'Entered tagline is too long'),
+    .max(MAX_TAGLINE_LENGTH, "Entered tagline is too long"),
   about: Yup.string()
-    .max(MAX_ABOUT_LENGTH, 'Entered text is too long'),
+    .max(MAX_ABOUT_LENGTH, "Entered text is too long"),
   links: Yup.array()
     .of(Yup.object().shape({
       title: Yup.string()
-        .max(MAX_LINK_TITLE_LENGTH, 'Entered title is too long')
-        .required('Please enter link title'),
+        .max(MAX_LINK_TITLE_LENGTH, "Entered title is too long")
+        .required("Please enter link title"),
       link: Yup.string()
-        .required('Please enter a link'),
+        .matches(
+          HTTPS_URL_REGEXP,
+          "Please enter correct URL",
+        )
+        .required("Please enter a link"),
     }))
-    .required('Please add at least 1 link')
-    .min(1, 'Please add at least 1 link'),
+    .required("Please add at least 1 link")
+    .min(1, "Please add at least 1 link"),
 });
 
 export default schema;
