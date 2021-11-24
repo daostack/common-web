da7a236950f4bbd63f4db8261dbdb4421416377d
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-24 14:01:18 +0300

    Make craco version to always be the same

--- craco.config.js
+++ craco.config.js
@@ -10,7 +10,7 @@ module.exports = {
         // plugin does not take it from tsconfig
         baseUrl: ".",
         // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
-        tsConfigPath: "./tsconfig.paths.json"
+        tsConfigPath: "./tsconfig.paths.json",
       },
     },
   ],
