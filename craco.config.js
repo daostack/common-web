bc0b223b52ecdc8f2dd951e2ab40c7126eae9207
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-24 13:58:39 +0300

    Use craco-alias plugin not to duplicate aliases

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,17 @@
-const path = require("path");
+const CracoAlias = require("craco-alias");
 
 module.exports = {
-  webpack: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        // baseUrl SHOULD be specified
+        // plugin does not take it from tsconfig
+        baseUrl: ".",
+        // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
+        tsConfigPath: "./tsconfig.paths.json"
+      },
     },
-  },
+  ],
 };
