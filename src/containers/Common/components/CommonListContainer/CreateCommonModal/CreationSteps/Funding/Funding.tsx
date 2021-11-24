d41356bba6f1537161a8104db6795e6a9f9d947b
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-11-24 15:17:05 +0300

    Create basic funding creation step

---
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Funding/Funding.tsx
@@ -0,0 +1,74 @@
+import React, { useCallback, useRef, ReactElement } from "react";
+import { Formik, FormikConfig } from "formik";
+import { FormikProps } from "formik/dist/types";
+
+import { isMobile } from "../../../../../../../shared/utils";
+import { ModalFooter, ModalHeaderContent } from "../../../../../../../shared/components/Modal";
+import { Form } from "../../../../../../../shared/components/Form/Formik";
+import { Separator } from "../../Separator";
+import { Progress } from "../Progress";
+import "./index.scss";
+
+interface FundingProps {
+  currentStep: number;
+  onFinish: () => void;
+}
+
+interface FormValues {
+}
+
+const INITIAL_VALUES: FormValues = {
+};
+
+export default function Funding({ currentStep, onFinish }: FundingProps): ReactElement {
+  const formRef = useRef<FormikProps<FormValues>>(null);
+  const isMobileView = isMobile();
+
+  const handleContinueClick = useCallback(() => {
+    if (formRef.current) {
+      formRef.current.submitForm();
+    }
+  }, []);
+
+  const handleSubmit = useCallback<FormikConfig<FormValues>['onSubmit']>((values) => {
+    onFinish();
+  }, [onFinish]);
+
+  const progressEl = <Progress creationStep={currentStep} />;
+
+  return (
+    <>
+      {!isMobileView && (
+        <ModalHeaderContent>
+          {progressEl}
+        </ModalHeaderContent>
+      )}
+      <div className="create-common-funding">
+        {isMobileView && progressEl}
+        <Separator />
+        <Formik
+          initialValues={INITIAL_VALUES}
+          onSubmit={handleSubmit}
+          innerRef={formRef}
+          validateOnMount
+        >
+          {({ values, errors, touched, isValid }) => (
+            <Form>
+              <ModalFooter sticky={!isMobileView}>
+                <div className="create-common-funding__modal-footer">
+                  <button
+                    className="button-blue"
+                    onClick={handleContinueClick}
+                    disabled={!isValid}
+                  >
+                    Continue to Rules
+                  </button>
+                </div>
+              </ModalFooter>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
+}
