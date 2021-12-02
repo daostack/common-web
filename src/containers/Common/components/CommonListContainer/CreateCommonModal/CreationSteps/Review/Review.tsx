4b53949ded832d252083adc421fc73d7edf942ad
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-02 12:18:57 +0300

    Display links section in Review step

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Review/Review.tsx
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Review/Review.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback, ReactElement } from "react";
 
 import { isMobile } from "@/shared/utils";
+import { ButtonLink } from "@/shared/components";
 import { ModalFooter, ModalHeaderContent } from "@/shared/components/Modal";
+import LinkIcon from "@/shared/icons/link.icon";
 import { Separator } from "../../Separator";
 import { Progress } from "../Progress";
 import "./index.scss";
@@ -17,6 +19,10 @@ export default function Review({ currentStep, onFinish }: ReviewProps): ReactEle
   const tagline = "If you wanna save the Amazon, own it.";
   const minimumContribution = 10;
   const about = "We aim to ba a global non-profit initiative. Only small percentage of creative directors are women and we want to help change this through mentorship circles, portfolio reviews, talks & creative meetups.";
+  const links = [
+    { title: "Amazon Facebook group", link: "https://www.google.com" },
+    { title: "LinkedIn", link: "https://www.linkedin.com" },
+  ];
 
   const handleContinueClick = useCallback(() => {
     onFinish();
@@ -50,6 +56,19 @@ export default function Review({ currentStep, onFinish }: ReviewProps): ReactEle
             <p className="create-common-review__section-description">{about}</p>
           </div>
         )}
+        <div className="create-common-review__section">
+          <h5 className="create-common-review__section-title">Links</h5>
+          {links.map((link, index) => (
+            <ButtonLink
+              key={index}
+              className="create-common-review__link"
+              href={link.link}
+              target="_blank"
+            >
+              <LinkIcon className="create-common-review__link-icon" />{link.title}
+            </ButtonLink>
+          ))}
+        </div>
         <ModalFooter sticky>
           <div className="create-common-review__modal-footer">
             <button
