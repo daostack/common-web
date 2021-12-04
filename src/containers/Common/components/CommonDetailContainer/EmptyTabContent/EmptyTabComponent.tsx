19218a8d57a6e9279d0e82124546eb7bc7ebdfe0
Denis Stetskov <botozavreg@gmail.com>
2021-12-04 12:14:32 +0200

    my commons empty state added

--- src/containers/Common/components/CommonDetailContainer/EmptyTabContent/EmptyTabComponent.tsx
+++ src/containers/Common/components/CommonDetailContainer/EmptyTabContent/EmptyTabComponent.tsx
@@ -1,19 +1,27 @@
 import React, { useCallback } from "react";
-
+import { Link } from "react-router-dom";
 import { MembershipRequestModal } from "../MembershipRequestModal";
 import { useModal } from "../../../../../shared/hooks";
 import "./index.scss";
 import { Common } from "../../../../../shared/models";
+import { ROUTE_PATHS } from "../../../../../shared/constants";
 
 interface EmptyTabComponentProps {
   currentTab: string;
   message: string;
   title: string;
-  common: Common;
+  common?: Common;
 }
 
-export default function EmptyTabComponent({ currentTab, message, title, common }: EmptyTabComponentProps) {
-  const { isShowing: showJoinModal, onClose: onCloseJoinModal } = useModal(false);
+export default function EmptyTabComponent({
+  currentTab,
+  message,
+  title,
+  common,
+}: EmptyTabComponentProps) {
+  const { isShowing: showJoinModal, onClose: onCloseJoinModal } = useModal(
+    false
+  );
 
   const closeJoinModalHandler = useCallback(() => {
     onCloseJoinModal();
@@ -21,20 +29,40 @@ export default function EmptyTabComponent({ currentTab, message, title, common }
 
   return (
     <>
-      <MembershipRequestModal
-        isShowing={showJoinModal}
-        onClose={closeJoinModalHandler}
-        common={common}
-      />
+      {common && (
+        <MembershipRequestModal
+          isShowing={showJoinModal}
+          onClose={closeJoinModalHandler}
+          common={common}
+        />
+      )}
       <div className="empty-tab-component-wrapper">
         <div className="img-wrapper">
-          {currentTab === "proposals" && <img alt={currentTab} src="/icons/proposals-empty.svg" />}
-          {currentTab === "history" && <img alt={currentTab} src="/icons/proposals-empty.svg" />}
-          {currentTab === "discussions" && <img alt={currentTab} src="/icons/discussions-empty.svg" />}
+          {currentTab === "proposals" && (
+            <img alt={currentTab} src="/icons/proposals-empty.svg" />
+          )}
+          {currentTab === "history" && (
+            <img alt={currentTab} src="/icons/proposals-empty.svg" />
+          )}
+          {currentTab === "discussions" && (
+            <img alt={currentTab} src="/icons/discussions-empty.svg" />
+          )}
+          {currentTab === "my-commons" && (
+            <img
+              alt={currentTab}
+              src="/assets/images/membership-request-membership.svg"
+            />
+          )}
         </div>
         <div className="empty-tab-content-wrapper ">
           <div className="title">{title}</div>
           <div className="message">{message}</div>
+
+          {currentTab === "my-commons" && (
+            <Link to={`${ROUTE_PATHS.COMMON_LIST}`}>
+              <button className={`button-blue`}>Browse all Commons</button>
+            </Link>
+          )}
         </div>
       </div>
     </>
