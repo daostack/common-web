aaf5453c33e02f49a18cc6d1724cc78f95d6d5ad
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-07 16:35:06 +0300

    Add vertical variant to toggle button group

--- src/shared/components/Form/ToggleButtonGroup/ToggleButton/ToggleButton.tsx
+++ src/shared/components/Form/ToggleButtonGroup/ToggleButton/ToggleButton.tsx
@@ -1,12 +1,13 @@
 import React, { useCallback, FC } from "react";
 import classNames from "classnames";
 
-import { useToggleButtonGroupContext } from "../context";
+import { useToggleButtonGroupContext, ToggleButtonGroupVariant } from "../context";
 import "./index.scss";
 
 interface ToggleButtonStyles {
   default?: string;
   active?: string;
+  vertical?: string;
 }
 
 interface ToggleButtonProps {
@@ -16,7 +17,7 @@ interface ToggleButtonProps {
 
 const ToggleButton: FC<ToggleButtonProps> = (props) => {
   const { value, styles, children } = props;
-  const { currentValue, onChange } = useToggleButtonGroupContext();
+  const { currentValue, onChange, variant } = useToggleButtonGroupContext();
 
   const handleClick = useCallback(() => {
     onChange(value);
@@ -24,6 +25,7 @@ const ToggleButton: FC<ToggleButtonProps> = (props) => {
 
   const buttonClassName = classNames("custom-toggle-button", styles?.default, {
     [classNames("custom-toggle-button--active", styles?.active)]: value === currentValue,
+    [classNames("custom-toggle-button--vertical", styles?.vertical)]: variant === ToggleButtonGroupVariant.Vertical,
   });
 
   return (
