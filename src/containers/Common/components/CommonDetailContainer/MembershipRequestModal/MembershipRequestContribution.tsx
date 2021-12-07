d97891aab75457fc1e86b6798882f313fab27c02
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-07 16:42:54 +0300

    Add toggle buttons to Personal Contribution

--- src/containers/Common/components/CommonDetailContainer/MembershipRequestModal/MembershipRequestContribution.tsx
+++ src/containers/Common/components/CommonDetailContainer/MembershipRequestModal/MembershipRequestContribution.tsx
@@ -1,55 +1,61 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { ToggleButtonGroup, ToggleButton, ToggleButtonGroupVariant } from "../../../../../shared/components/Form/ToggleButtonGroup";
 import { formatPrice } from "../../../../../shared/utils";
 import "./index.scss";
 import { IStageProps } from "./MembershipRequestModal";
 
+const MIN_CALCULATION_AMOUNT = 2000;
+
 export default function MembershipRequestContribution(props: IStageProps) {
   const { userData, setUserData, common } = props;
-  const minFeeToJoin = common?.metadata.minFeeToJoin;
+  const [selectedContribution, setSelectedContribution] = useState<number | "other">(userData.contribution_amount || 0);
+  const minFeeToJoin = common?.metadata.minFeeToJoin || 0;
   const formattedMinFeeToJoin = formatPrice(minFeeToJoin);
+  const secondAmount = minFeeToJoin < MIN_CALCULATION_AMOUNT ? 2000 : (minFeeToJoin + 1000);
+  const thirdAmount = minFeeToJoin < MIN_CALCULATION_AMOUNT ? 5000 : (minFeeToJoin + 2000);
+
+  const handleChange = useCallback((value: unknown) => {
+    const convertedValue = Number(value);
+    setSelectedContribution(!Number.isNaN(convertedValue) ? convertedValue : "other");
+  }, []);
+
+  const toggleButtonStyles = { default: "membership-request-contribution__toggle-button" };
 
   return (
     <div className="membership-request-content membership-request-contribution">
       <div className="sub-title">Personal Contribution</div>
       <div className="sub-text">{`Select the amount you would like to contribute (${formattedMinFeeToJoin} min.)`}</div>
-      <div className="options-wrapper">
-        <button
-          className={`${userData.contribution_id === 0 ? "selected" : ""}`}
-          onClick={() =>
-            setUserData({
-              ...userData,
-              contribution_amount: minFeeToJoin,
-              contribution_id: 0,
-            })
-          }
+      <ToggleButtonGroup
+        className="membership-request-contribution__toggle-button-group"
+        value={selectedContribution}
+        onChange={handleChange}
+        variant={ToggleButtonGroupVariant.Vertical}
+      >
+        <ToggleButton
+          styles={toggleButtonStyles}
+          value={minFeeToJoin}
         >
-          {formattedMinFeeToJoin}
-        </button>
-        <button
-          className={`${userData.contribution_id === 1 ? "selected" : ""}`}
-          onClick={() =>
-            setUserData({
-              ...userData,
-              contribution_amount: 2000,
-              contribution_id: 1,
-            })
-          }
+          {formatPrice(minFeeToJoin)}
+        </ToggleButton>
+        <ToggleButton
+          styles={toggleButtonStyles}
+          value={secondAmount}
         >
-          {formatPrice(2000)}
-        </button>
-        <button
-          className={`${userData.contribution_id === 2 ? "selected" : ""}`}
-          onClick={() =>
-            setUserData({
-              ...userData,
-              contribution_amount: 5000,
-              contribution_id: 2,
-            })
-          }
+          {formatPrice(secondAmount)}
+        </ToggleButton>
+        <ToggleButton
+          styles={toggleButtonStyles}
+          value={thirdAmount}
         >
-          {formatPrice(5000)}
-        </button>{" "}
-      </div>
+          {formatPrice(thirdAmount)}
+        </ToggleButton>
+        <ToggleButton
+          styles={toggleButtonStyles}
+          value="other"
+        >
+          Other
+        </ToggleButton>
+      </ToggleButtonGroup>
       <button
         disabled={!userData.contribution_amount}
         className="button-blue"
