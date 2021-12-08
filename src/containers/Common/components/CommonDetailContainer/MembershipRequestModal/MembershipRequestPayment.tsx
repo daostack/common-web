81b47039ce1d561dd08f9dac5671aaa0d9cd9f97
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-08 14:23:09 +0300

    Update price formatting for payment stage

--- src/containers/Common/components/CommonDetailContainer/MembershipRequestModal/MembershipRequestPayment.tsx
+++ src/containers/Common/components/CommonDetailContainer/MembershipRequestModal/MembershipRequestPayment.tsx
@@ -70,7 +70,7 @@ export default function MembershipRequestPayment(props: IStageProps): ReactEleme
     <div className="membership-request-content membership-request-payment">
       <div className="sub-title">Payment Details</div>
       <div className="sub-text">
-        You are contributing <strong className="membership-request-payment__amount">{formatPrice(userData.contribution_amount)} ({contributionTypeText})</strong> to this Common.
+        You are contributing <strong className="membership-request-payment__amount">{formatPrice(userData.contribution_amount, false)} ({contributionTypeText})</strong> to this Common.
       </div>
       <label>Card Number</label>
       <input
