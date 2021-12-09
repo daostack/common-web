b06863c01cbbdde249e1b7ba141b49b5ac41709d
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-09 11:12:30 +0300

    Remove card details form

--- src/containers/Common/components/CommonDetailContainer/MembershipRequestModal/MembershipRequestPayment.tsx
+++ src/containers/Common/components/CommonDetailContainer/MembershipRequestModal/MembershipRequestPayment.tsx
@@ -26,17 +26,7 @@ export default function MembershipRequestPayment(props: IStageProps): ReactEleme
   const [card_number, setCardNumber] = useState(0); // 4007400000000007
   const [expiration_date, setExpirationDate] = useState("");
   const [cvv, setCvv] = useState(0);
-  const [showCCNumberError, setShowCCNumberError] = useState(false);
   const contributionTypeText = common?.metadata.contributionType === CommonContributionType.Monthly ? "monthly" : "one-time";
-
-  const onCCNumberChange = (value: string) => {
-    setCardNumber(Number(value));
-    if (!validateCreditCardProvider(value) || !luhnAlgo(value)) {
-      setShowCCNumberError(true);
-    } else {
-      setShowCCNumberError(false);
-    }
-  };
 
   const pay = async () => {
     try {
@@ -76,36 +66,6 @@ export default function MembershipRequestPayment(props: IStageProps): ReactEleme
       <div className="sub-title">{isMobileView ? "Billing Details" : "Payment Details"}</div>
       <div className="sub-text">
         You are contributing <strong className="membership-request-payment__amount">{formatPrice(userData.contribution_amount, false)} ({contributionTypeText})</strong> to this Common.
-      </div>
-      <label>Card Number</label>
-      <input
-        type="number"
-        value={card_number}
-        onChange={(e) => onCCNumberChange(e.target.value)}
-      />
-      {showCCNumberError && (
-        <span className="error">
-          Enter a valid credit card number. Only Visa is currently supported.
-        </span>
-      )}
-      <div className="expiration-cvv-wrapper">
-        <div>
-          <label>Expiration date</label>
-          <input
-            type="date"
-            min={getTodayDate()}
-            value={expiration_date}
-            onChange={(e) => setExpirationDate(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>CVV</label>
-          <input
-            type="number"
-            value={cvv}
-            onChange={(e) => setCvv(Number(e.target.value))}
-          />
-        </div>
       </div>
       <span className="membership-rejected-text">
         If your membership request will not be accepted, you will not be
