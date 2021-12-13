0cde5b0ad66a940cdf1585476cc5c5b1728775d7
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-13 11:05:00 +0300

    Add join the effort button to empty tab component

--- src/containers/Common/components/CommonDetailContainer/EmptyTabContent/EmptyTabComponent.tsx
+++ src/containers/Common/components/CommonDetailContainer/EmptyTabContent/EmptyTabComponent.tsx
@@ -1,10 +1,16 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { MembershipRequestModal } from "../MembershipRequestModal";
 import { useModal } from "../../../../../shared/hooks";
 import "./index.scss";
 import { Common } from "../../../../../shared/models";
 import { ROUTE_PATHS } from "../../../../../shared/constants";
+import { setIsLoginModalShowing } from "../../../../Auth/store/actions";
+import {
+  selectUser,
+  selectIsLoginModalShowing,
+} from "../../../../Auth/store/selectors";
 
 interface EmptyTabComponentProps {
   currentTab: string;
@@ -19,13 +25,39 @@ export default function EmptyTabComponent({
   title,
   common,
 }: EmptyTabComponentProps) {
-  const { isShowing: showJoinModal, onClose: onCloseJoinModal } = useModal(
-    false
-  );
+  const dispatch = useDispatch();
+  const [
+    shouldOpenJoinModalOnLoginClose,
+    setShouldOpenJoinModalOnLoginClose,
+  ] = useState(false);
+  const {
+    isShowing: showJoinModal,
+    onOpen: onJoinModalOpen,
+    onClose: onCloseJoinModal,
+  } = useModal(false);
+  const user = useSelector(selectUser());
+  const isLoginModalShowing = useSelector(selectIsLoginModalShowing());
+
+  const handleJoinModalOpen = useCallback(() => {
+    if (user) {
+      onJoinModalOpen();
+      return;
+    }
+
+    dispatch(setIsLoginModalShowing(true));
+    setShouldOpenJoinModalOnLoginClose(true);
+  }, [user, onJoinModalOpen, dispatch]);
 
   const closeJoinModalHandler = useCallback(() => {
     onCloseJoinModal();
   }, [onCloseJoinModal]);
+
+  useEffect(() => {
+    if (shouldOpenJoinModalOnLoginClose && !isLoginModalShowing) {
+      setShouldOpenJoinModalOnLoginClose(false);
+      onJoinModalOpen();
+    }
+  }, [shouldOpenJoinModalOnLoginClose, isLoginModalShowing, onJoinModalOpen]);
 
   return (
     <>
@@ -58,11 +90,19 @@ export default function EmptyTabComponent({
           <div className="title">{title}</div>
           <div className="message">{message}</div>
 
-          {currentTab === "my-commons" && (
-            <Link to={`${ROUTE_PATHS.COMMON_LIST}`}>
-              <button className={`button-blue`}>Browse all Commons</button>
-            </Link>
-          )}
+          <div className="empty-tab-content-wrapper__buttons-wrapper">
+            <button className="button-blue" onClick={handleJoinModalOpen}>
+              Join the effort
+            </button>
+            {currentTab === "my-commons" && (
+              <Link
+                className="empty-tab-content-wrapper__browse-commons-button"
+                to={`${ROUTE_PATHS.COMMON_LIST}`}
+              >
+                <button className={`button-blue`}>Browse all Commons</button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </>
