cc881313a0936c00c78ea49fbbd318dc8b7dbdf3
Roie Natan <roie.natan@gmail.com>
2021-12-14 12:38:44 -0500

    AddInvoices component init ; more updates

---
+++ src/containers/SubmitInvoices/components/AddInvoices/index.tsx
@@ -0,0 +1,1 @@
+export { default as AddInvoices } from "./AddInvoices";
--- src/containers/SubmitInvoices/containers/SubmitInvoicesContainer/SubmitInvoicesContainer.tsx
+++ src/containers/SubmitInvoices/containers/SubmitInvoicesContainer/SubmitInvoicesContainer.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../../Auth/store/selectors";
+import { AddInvoices } from "../../components/AddInvoices";
 import "./index.scss";
+
+enum Expense {
+  proposal = "proposal",
+}
 
 export default function SubmitInvoicesContainer() {
   const user = useSelector(selectUser());
+  const expense: Expense = Expense.proposal;
 
   return (
     <div className="submit-invoices-wrapper">
-      <img src="/icons/logo.svg" alt="logo" className="logo" />
-      <div>{`Hello ${user?.displayName}`}</div>
+      {/* <img src="/icons/logo.svg" alt="logo" className="logo" /> */}
+      <span>{`Hi ${user?.displayName}`}</span>
+      <span>{`Please add all of your invoices related to this ${expense}`}</span>
+      <AddInvoices />
     </div>
   )
 }
