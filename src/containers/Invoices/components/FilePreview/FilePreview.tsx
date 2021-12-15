ca1a4b466ff317d182ab65d110e9ac0ab6b7904e
Roie Natan <roie.natan@gmail.com>
2021-12-15 16:21:33 -0500

    more styles to file preview

--- src/containers/Invoices/components/FilePreview/FilePreview.tsx
+++ src/containers/Invoices/components/FilePreview/FilePreview.tsx
@@ -12,7 +12,13 @@ export default function FilePreview({ file }: IProps) {
     <div className="file-preview-wrapper">
       <div className="file-preview-overlay" />
       <div className="content">
-        <img src={previewFile} alt="preview" />
+        <div className="top">
+          <img className="delete-invoice" src="/icons/trash.svg" alt="trash" />
+        </div>
+        <img className="preview-image" src={previewFile} alt="preview" />
+        <div className="bottom">
+          <button className="button-blue">Add invoice amount</button>
+        </div>
       </div>
     </div>
   )
