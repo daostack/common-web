8bbcd926e2c6aa62a22da2bc2552f98729940f8c
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-17 12:40:20 +0300

    Add description card to invoice submission

--- src/containers/Invoices/containers/SubmitInvoicesContainer/SubmitInvoicesContainer.tsx
+++ src/containers/Invoices/containers/SubmitInvoicesContainer/SubmitInvoicesContainer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../../Auth/store/selectors";
 import { AddInvoices } from "../../components/AddInvoices";
+import ApprovedIcon from "../../../../shared/icons/approved.icon";
 import "./index.scss";
 
 enum Expense {
@@ -15,8 +16,20 @@ export default function SubmitInvoicesContainer() {
   return (
     <div className="submit-invoices-wrapper">
       {/* <img src="/icons/logo.svg" alt="logo" className="logo" /> */}
-      <span>{`Hi ${user?.displayName}`}</span>
+      <span>{`Hi ${user?.displayName},`}</span>
       <span>{`Please add all of your invoices related to this ${expense}`}</span>
+      <div className="submit-invoices-wrapper__description-wrapper">
+        <span className="submit-invoices-wrapper__description-header">
+          <ApprovedIcon className="submit-invoices-wrapper__description-approved-icon" />
+          Approved on Nov, 21 2021
+        </span>
+        <div className="submit-invoices-wrapper__description-content">
+          <span>Launch a facebook campaign to raise awareness about the amazon</span>
+          <span className="submit-invoices-wrapper__description-amount">
+            ₪950
+          </span>
+        </div>
+      </div>
       <AddInvoices />
     </div>
   )
