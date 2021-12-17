7ff481710cafa42f3f3a98f7fe6bbf9d5ae866de
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-17 14:04:34 +0300

    Add delete icon to invoice tile

--- src/shared/components/InvoiceTile/InvoiceTile.tsx
+++ src/shared/components/InvoiceTile/InvoiceTile.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
 import classNames from "classnames";
+import { ButtonIcon } from "../../../shared/components";
+import TrashIcon from "../../../shared/icons/trash.icon";
 import { formatPrice } from "../../../shared/utils";
 import "./index.scss";
 
@@ -7,16 +9,26 @@ interface InvoiceTileProps {
   className?: string;
   imageSrc: string;
   alt: string;
+  onDelete?: () => void;
   amount?: number | null;
 }
 
 const InvoiceTile: FC<InvoiceTileProps> = (props) => {
-  const { className, imageSrc, alt, amount = null } = props;
+  const { className, imageSrc, alt, onDelete, amount = null } = props;
 
   return (
     <div className={classNames("invoice-tile", className)}>
       <img className="invoice-tile__image" src={imageSrc} alt={alt} />
-      {amount !== null && <div className="invoice-tile__amount">Amount: {formatPrice(amount)}</div>}
+      {onDelete && (
+        <ButtonIcon className="invoice-tile__delete-button" onClick={onDelete}>
+          <TrashIcon className="invoice-tile__delete-icon" />
+        </ButtonIcon>
+      )}
+      {amount !== null && (
+        <div className="invoice-tile__amount">
+          Amount: {formatPrice(amount)}
+        </div>
+      )}
     </div>
   );
 };
