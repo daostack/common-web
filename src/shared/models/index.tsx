3fc08a74119a1ade94c79f9b86232d7af8de071e
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2021-12-23 11:00:06 +0300

    Remove Payment model

--- src/shared/models/index.tsx
+++ src/shared/models/index.tsx
@@ -2,7 +2,6 @@ export * from "./User";
 export * from "./Card";
 export * from "./Common";
 export * from "./shared";
-export * from "./Payment";
 export * from "./Proposals";
 export * from "./DiscussionMessage";
 export * from "./Discussion";
