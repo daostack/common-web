5eb9ff333e41137c3524c4a9b4d91f980e1183ff
Denis Stetskov <botozavreg@gmail.com>
2021-12-26 16:22:58 +0200

    add discussion layout added

--- src/containers/Common/components/CommonDetailContainer/AddDiscussionComponent/AddDiscussionComponent.tsx
+++ src/containers/Common/components/CommonDetailContainer/AddDiscussionComponent/AddDiscussionComponent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal } from "../../../../../shared/components";
 import { ModalProps } from "@/shared/interfaces";
 import { Common } from "@/shared/models";
+import "./index.scss";
 
 interface AddDiscussionComponentProps
   extends Pick<ModalProps, "isShowing" | "onClose"> {
@@ -14,7 +15,33 @@ const AddDiscussionComponent = ({
 }: AddDiscussionComponentProps) => {
   return (
     <Modal isShowing={isShowing} onClose={onClose}>
-      kek
+      <div className="add-discussion-wrapper">
+        <div className="add-discussion-title">New Post</div>
+        <div className="discussion-form-wrapper">
+          <div className="input-wrapper">
+            <label>
+              Post Title <span className="required">Required</span>
+            </label>
+            <div className="text-area-wrapper">
+              <textarea></textarea>
+            </div>
+          </div>
+          <div className="input-wrapper">
+            <label>
+              Message <span className="required">Required</span>
+            </label>
+            <div className="text-area-wrapper">
+              <textarea className="big"></textarea>
+            </div>
+          </div>
+
+          <div className="action-wrapper">
+            <button className="button-blue" disabled={true}>
+              Publish Post
+            </button>
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
