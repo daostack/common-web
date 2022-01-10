166010243055053e34c65aa9ef37b3ba0032687e
Roie Natan <roie.natan@gmail.com>
2022-01-10 17:52:24 -0500

    use firebase .doc() instead of .where()

--- src/containers/Invoices/api.tsx
+++ src/containers/Invoices/api.tsx
@@ -1,17 +1,17 @@
 import Api from "../../services/Api";
 import { ApiEndpoint } from "../../shared/constants";
 import { InvoicesSubmission, Proposal } from "../../shared/models";
-import { transformFirebaseDataList } from "../../shared/utils";
+import { transformFirebaseDataSingle } from "../../shared/utils";
 import firebase from "../../shared/utils/firebase";
 
 export async function fetchProposal(proposalId: string): Promise<Proposal> {
   const proposal = await firebase
     .firestore()
     .collection("proposals")
-    .where("id", "==", proposalId)
+    .doc(proposalId)
     .get();
-  const data = transformFirebaseDataList<Proposal>(proposal);
-  return data[0];
+  const data = transformFirebaseDataSingle<Proposal>(proposal);
+  return data;
 }
 
 export async function uploadInvoices(invoicesData: InvoicesSubmission): Promise<void> {
