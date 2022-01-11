1daa622ef67c4bc28b7c947769b4db345d195f80
Denis Stetskov <botozavreg@gmail.com>
2022-01-11 19:08:46 +0200

    add proposal layout added

--- src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddProposalComponent.tsx
+++ src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddProposalComponent.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
-import { Formik } from "formik";
-import * as Yup from "yup";
-import { TextField } from "@/shared/components/Form/Formik";
+import React, { useMemo, useState } from "react";
+
 import { Modal } from "../../../../../shared/components";
 import { ModalProps } from "@/shared/interfaces";
 
 import "./index.scss";
 import { Common } from "@/shared/models";
-import { formatPrice } from "@/shared/utils";
+import { AddProposalForm } from "./AddProposalForm";
+import { AddProposalConfirm } from "@/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddProposalConfirm";
 
 interface AddDiscussionComponentProps
   extends Pick<ModalProps, "isShowing" | "onClose"> {
@@ -15,110 +14,34 @@ interface AddDiscussionComponentProps
   common: Common;
 }
 
-const validationSchema = Yup.object({
-  message: Yup.string().required("Field required"),
-  title: Yup.string().required("Field required").max(49, "Title too long"),
-});
-
 const AddProposalComponent = ({
   isShowing,
   onClose,
   onProposalAdd,
   common,
 }: AddDiscussionComponentProps) => {
-  const [formValues] = useState({
-    title: "",
-    message: "",
-  });
+  const [proposalCreationStep, changeCreationProposalStep] = useState(
+    "confirm"
+  );
+
+  const renderProposalStep = useMemo(() => {
+    switch (proposalCreationStep) {
+      case "create":
+        return (
+          <AddProposalForm onProposalAdd={onProposalAdd} common={common} />
+        );
+      case "confirm":
+        return <AddProposalConfirm />;
+      default:
+        return (
+          <AddProposalForm onProposalAdd={onProposalAdd} common={common} />
+        );
+    }
+  }, [onProposalAdd, proposalCreationStep, common]);
+
   return (
     <Modal isShowing={isShowing} onClose={onClose}>
-      <Formik
-        validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          setSubmitting(false);
-          onProposalAdd(values);
-        }}
-        initialValues={formValues}
-        validateOnChange={true}
-        validateOnBlur={false}
-        validateOnMount={false}
-        isInitialValid={false}
-      >
-        {(formikProps) => (
-          <div className="add-proposal-wrapper">
-            <div className="common-title">{common.name}</div>
-            <div className="add-proposal-title">New proposal</div>
-            <div className="add-proposal-description">
-              Proposals allow you to make decisions as a group. If you choose to
-              request funding and the proposal is accepted, you will be
-              responsible to follow it through.
-            </div>
-            <div className="add-proposal-form-wrapper">
-              <TextField
-                id="title"
-                label="Title"
-                name={"title"}
-                placeholder={"Briefly describe your proposal"}
-                value={formikProps.values.title}
-                onChange={formikProps.handleChange}
-                onBlur={formikProps.handleBlur}
-                isRequired={true}
-              />
-              <div className="funding-request-wrapper">
-                <div className="funding-input-wrapper">
-                  <TextField
-                    id="funding"
-                    label="Funding amount requested"
-                    name={"funding"}
-                    placeholder={"₪0"}
-                    value={formikProps.values.title}
-                    onChange={formikProps.handleChange}
-                    onBlur={formikProps.handleBlur}
-                  />
-                </div>
-                <div className="funding-description">
-                  Leave as ₪0 if no funds are requested. <br /> Common balance:
-                  {formatPrice(common.balance)}
-                </div>
-              </div>
-              <div className="proposal-description-wrapper">
-                <TextField
-                  id="description"
-                  label="Description"
-                  name={"description"}
-                  placeholder={
-                    "What exactly do you plan to do and how? How does it align with the Common's agenda and goals?"
-                  }
-                  value={formikProps.values.title}
-                  onChange={formikProps.handleChange}
-                  onBlur={formikProps.handleBlur}
-                  isTextarea={true}
-                />
-              </div>
-              <div className="add-additional-information">
-                <div className="link">Add link</div>
-                <div className="link">Add image</div>
-              </div>
-              <div className="proposal-note-wrapper">
-                <div className="note-title">Please note:</div>
-                <div className="note-text">
-                  Other proposals might be accepted and make the balance
-                  insufficient for the amount requested.
-                </div>
-              </div>
-              <div className="action-wrapper">
-                <button
-                  className="button-blue"
-                  disabled={!formikProps.isValid}
-                  onClick={formikProps.submitForm}
-                >
-                  Create proposal
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
-      </Formik>
+      {renderProposalStep}
     </Modal>
   );
 };
---
+++ src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddProposalConfirm.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const AddProposalConfirm = () => {
+  return (
+    <div className="app-proposal-confirm-wrapper">
+      <img
+        src="/icons/add-proposal/illustrations-full-page-funds.svg"
+        alt="confirm"
+      />
+      <div className="add-proposal-confirm-title">Use of Funds</div>
+      <div className="add-proposal-confirm-description">
+        If your proposal is accepted, the funds requested would be allocated to
+        you for <br /> the proposed purpose and you will be responsible to
+        ensure that the funding is <br /> used adequately. All Common members
+        are volunteers and can not be paid by the <br /> Common for their
+        services.
+      </div>
+      <div className="add-proposal-confirm-checkboxes">
+        <div className="checkboxes-title">You may only use the funds to:</div>
+
+        <div className="confirm-checkboxes">
+          <div className="checkbox">
+            <img src="/icons/check.png" />
+            <span>
+              Pay properly registered businesses, with a valid tax withholding
+              exemption, for their products or services.
+            </span>
+          </div>
+          <div className="checkbox">
+            <img src="/icons/check.png" />
+            <span>Contribute to registered non-profits.</span>
+          </div>
+        </div>
+        <div className="confirm-notes-wrapper">
+          After claiming the funds, you will be required to submit invoices
+          and/or receipts for <br /> each expense sponsored by the Common.
+        </div>
+      </div>
+      <div className="action-wrapper">
+        <button className="button-blue">
+          I agree with the above statement
+        </button>
+      </div>
+    </div>
+  );
+};
