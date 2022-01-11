5d73501e675199120947789e0c757dd5edd631a2
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-01-11 13:10:41 +0300

    Create Linkify component

--- src/shared/components/Image/Image.tsx
+++ src/shared/components/Image/Image.tsx
@@ -8,11 +8,12 @@ import React, {
 } from "react";
 
 interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+  alt: string;
   placeholderElement?: ReactNode;
 }
 
 const Image: FC<ImageProps> = (props) => {
-  const { onError, placeholderElement = null, ...restProps } = props;
+  const { alt, onError, placeholderElement = null, ...restProps } = props;
   const [hasError, setHasError] = useState(false);
 
   const handleError = useCallback<ReactEventHandler<HTMLImageElement>>(
@@ -29,7 +30,7 @@ const Image: FC<ImageProps> = (props) => {
   return hasError && placeholderElement ? (
     <>{placeholderElement}</>
   ) : (
-    <img {...restProps} onError={handleError} />
+    <img {...restProps} alt={alt} onError={handleError} />
   );
 };
 
