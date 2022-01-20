b73d78491d1dfd21f6e9bcb577dcc5bfad8f4725
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-01-20 15:28:08 +0300

    Add private routes to trustee routes

--- src/containers/Trustee/containers/TrusteeContainer/TrusteeContainer.tsx
+++ src/containers/Trustee/containers/TrusteeContainer/TrusteeContainer.tsx
@@ -1,11 +1,24 @@
 import React, { FC } from "react";
 import { Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { ROUTE_PATHS } from "../../../../shared/constants";
+import { UserRole } from "../../../../shared/models";
+import PrivateRoute from "../../../App/PrivateRoute";
+import { authentificated, selectUser } from "../../../Auth/store/selectors";
 import { AuthenticationContainer } from "../AuthenticationContainer";
 import { InvoiceAcceptanceContainer } from "../InvoiceAcceptanceContainer";
 import { InvoicesAcceptanceContainer } from "../InvoicesAcceptanceContainer";
 
 const TrusteeContainer: FC = () => {
+  const isAuthenticated = useSelector(authentificated());
+  const user = useSelector(selectUser());
+  const generalPrivateProps = {
+    authenticated: isAuthenticated,
+    userRoles: user?.roles,
+    anyMandatoryRoles: [UserRole.Trustee],
+    unauthenticatedRedirectPath: ROUTE_PATHS.TRUSTEE_AUTH,
+  };
+
   return (
     <>
       <Route
@@ -13,15 +26,17 @@ const TrusteeContainer: FC = () => {
         exact
         component={AuthenticationContainer}
       />
-      <Route
+      <PrivateRoute
         path={ROUTE_PATHS.TRUSTEE_INVOICE}
         exact
         component={InvoiceAcceptanceContainer}
+        {...generalPrivateProps}
       />
-      <Route
+      <PrivateRoute
         path={ROUTE_PATHS.TRUSTEE_INVOICES}
         exact
         component={InvoicesAcceptanceContainer}
+        {...generalPrivateProps}
       />
     </>
   );
