a93326864ca19b1962a75dab35dc14e29fa84eb1
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-02-02 16:22:09 +0300

    Remove slider related code from review step

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Review/Review.tsx
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Review/Review.tsx
@@ -1,18 +1,8 @@
-import React, {
-  useCallback,
-  useRef,
-  useState,
-  ReactElement,
-  ReactNode,
-} from "react";
+import React, { useCallback, ReactElement, ReactNode } from "react";
 import { useSelector } from "react-redux";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperClass from "swiper/types/swiper-class";
 import { Button } from "@/shared/components";
 import { ModalHeaderContent } from "@/shared/components/Modal";
 import { ScreenSize } from "@/shared/constants";
-import LeftArrowIcon from "@/shared/icons/leftArrow.icon";
-import RightArrowIcon from "@/shared/icons/rightArrow.icon";
 import {
   CommonContributionType,
   CommonLink,
@@ -25,7 +15,6 @@ import { Progress } from "../Progress";
 import { LinkList } from "./LinkList";
 import { MainCommonInfo } from "./MainCommonInfo";
 import { RuleList } from "./RuleList";
-import { SelectFile } from "./SelectFile";
 import "./index.scss";
 
 interface ReviewProps {
@@ -39,13 +28,6 @@ export default function Review({
 }: ReviewProps): ReactElement {
   const screenSize = useSelector(getScreenSize());
   const isMobileView = screenSize === ScreenSize.Mobile;
-  const swiperRef = useRef<SwiperClass>();
-  const [slideNumber, setSlideNumber] = useState(1);
-  const defaultSliderImgs = [
-    "/assets/images/review-bg-image.jpg",
-    "/assets/images/review-bg-image.jpg",
-  ];
-  const coverImageTitle = "Select or upload cover image";
   const commonName = "Amazon Network";
   const tagline = "If you wanna save the Amazon, own it.";
   const minFeeToJoin = 1000;
@@ -77,31 +59,12 @@ export default function Review({
         "We're all in this together to create a nurturing environment. Let's teat everyone with respect. Healthy debates are natural, but kindness is required.",
     },
   ];
-  const handleSwiper = useCallback(
-    (swiper: SwiperClass) => {
-      swiperRef.current = swiper;
-    },
-    [swiperRef]
-  );
 
   const handleContinueClick = useCallback(() => {
     onFinish();
   }, [onFinish]);
 
   const progressEl = <Progress creationStep={currentStep} />;
-
-  const handlePrevSlide = () => {
-    if (slideNumber > 1) {
-      setSlideNumber((prevState) => prevState - 1);
-      swiperRef.current?.slidePrev();
-    }
-  };
-  const handleNextSlide = () => {
-    if (slideNumber < defaultSliderImgs.length) {
-      setSlideNumber((prevState) => prevState + 1);
-      swiperRef.current?.slideNext();
-    }
-  };
 
   const getMinimumContributionText = (): ReactNode => {
     let text: ReactNode =
@@ -129,49 +92,6 @@ export default function Review({
       {!isMobileView && <ModalHeaderContent>{progressEl}</ModalHeaderContent>}
       <div className="create-common-review">
         {isMobileView && progressEl}
-        <div className="create-common-review__slider">
-          <Swiper
-            navigation
-            onSwiper={handleSwiper}
-            allowTouchMove={isMobileView ? true : false}
-          >
-            {defaultSliderImgs.map((slide, index) => (
-              <SwiperSlide key={index}>
-                <figure className="create-common-review__slide">
-                  <img
-                    className="create-common-review__slide-img"
-                    src={slide}
-                    alt={slide}
-                  />
-                </figure>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          <SelectFile className="create-common-review__select-file" />
-          <div className="create-common-review__title-and-arrows">
-            <span onClick={handlePrevSlide}>
-              <LeftArrowIcon
-                className={
-                  slideNumber > 1
-                    ? "create-common-review__arrow-icon"
-                    : "create-common-review__arrow-icon-disable"
-                }
-              />
-            </span>
-            <span className="create-common-review__cover-image-title">
-              {coverImageTitle}
-            </span>
-            <span onClick={handleNextSlide}>
-              <RightArrowIcon
-                className={
-                  slideNumber === defaultSliderImgs.length
-                    ? "create-common-review__arrow-icon-disable"
-                    : "create-common-review__arrow-icon"
-                }
-              />
-            </span>
-          </div>
-        </div>
         <MainCommonInfo
           commonName={commonName}
           tagline={tagline}
--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Review/SelectFile/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as SelectFile } from "./SelectFile";
