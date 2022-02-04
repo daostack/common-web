d2f110732b7655edb7b778d89f673118d0f9c10a
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-02-04 11:39:27 +0300

    Add terms of use link

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/UserAcknowledgment/UserAcknowledgment.tsx
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/UserAcknowledgment/UserAcknowledgment.tsx
@@ -4,6 +4,7 @@ import { isMobile } from "@/shared/utils";
 import { ButtonLink, Separator } from "@/shared/components";
 import { Checkbox } from "@/shared/components/Form";
 import { ModalFooter, ModalHeaderContent } from "@/shared/components/Modal";
+import { TERMS_OF_USE_URL } from "@/shared/constants";
 import ExplanationIcon from "@/shared/icons/explanation.icon";
 import { Progress } from "../Progress";
 import { CheckedList } from "./CheckedList";
@@ -14,48 +15,66 @@ interface UserAcknowledgmentProps {
   onFinish: () => void;
 }
 
-const CAUSES_TEXT = "Education, Religion, Culture, Science, Health, Welfare, Sports, Fighting corruption, Protecting democracy, Employment, and Human rights.";
+const CAUSES_TEXT =
+  "Education, Religion, Culture, Science, Health, Welfare, Sports, Fighting corruption, Protecting democracy, Employment, and Human rights.";
 
-export default function UserAcknowledgment({ currentStep, onFinish }: UserAcknowledgmentProps): ReactElement {
+export default function UserAcknowledgment({
+  currentStep,
+  onFinish,
+}: UserAcknowledgmentProps): ReactElement {
   const [showCausesBox, setShowCausesBox] = useState(false);
   const [areTermsConfirmed, setAreTermsConfirmed] = useState(false);
   const isMobileView = isMobile();
 
   const toggleCausesBoxShowing = useCallback(() => {
-    setShowCausesBox(shouldShow => !shouldShow);
+    setShowCausesBox((shouldShow) => !shouldShow);
   }, []);
 
   const handleTermsChange = useCallback(() => {
-    setAreTermsConfirmed(checked => !checked);
+    setAreTermsConfirmed((checked) => !checked);
   }, []);
 
-  const listItems = useMemo(() => [
-    "The purpose of the Common is not in violation of any law, regulation, or 3rd party rights.",
-    <>The Common will be raising funds for <strong>non-profit or charitable causes only.</strong> The common is not intended for commercial or for-profit purposes.</>,
-    "All Commons and their members must comply with applicable financial and tax obligations.",
-    <>
-      The Common will solely promote one or more of the following <ButtonLink className="create-common-user-acknowledgment__causes-link" onClick={toggleCausesBoxShowing}>Causes<ExplanationIcon className="create-common-user-acknowledgment__causes-icon" /></ButtonLink>.
-    </>,
-  ], [toggleCausesBoxShowing]);
+  const listItems = useMemo(
+    () => [
+      "The purpose of the Common is not in violation of any law, regulation, or 3rd party rights.",
+      <>
+        The Common will be raising funds for{" "}
+        <strong>non-profit or charitable causes only.</strong> The common is not
+        intended for commercial or for-profit purposes.
+      </>,
+      "All Commons and their members must comply with applicable financial and tax obligations.",
+      <>
+        The Common will solely promote one or more of the following{" "}
+        <ButtonLink
+          className="create-common-user-acknowledgment__causes-link"
+          onClick={toggleCausesBoxShowing}
+        >
+          Causes
+          <ExplanationIcon className="create-common-user-acknowledgment__causes-icon" />
+        </ButtonLink>
+        .
+      </>,
+    ],
+    [toggleCausesBoxShowing]
+  );
 
-  const headerEl = useMemo(() => (
-    <>
-      <img
-        className="create-common-user-acknowledgment__header-image"
-        src="/assets/images/common-creation-user-acknowledgment.svg"
-        alt="User Acknowledgment"
-      />
-      <Progress creationStep={currentStep} />
-    </>
-  ), [currentStep]);
+  const headerEl = useMemo(
+    () => (
+      <>
+        <img
+          className="create-common-user-acknowledgment__header-image"
+          src="/assets/images/common-creation-user-acknowledgment.svg"
+          alt="User Acknowledgment"
+        />
+        <Progress creationStep={currentStep} />
+      </>
+    ),
+    [currentStep]
+  );
 
   return (
     <>
-      {!isMobileView && (
-        <ModalHeaderContent>
-          {headerEl}
-        </ModalHeaderContent>
-      )}
+      {!isMobileView && <ModalHeaderContent>{headerEl}</ModalHeaderContent>}
       <div className="create-common-user-acknowledgment">
         {isMobileView && headerEl}
         <Separator className="create-common-user-acknowledgment__separator" />
@@ -69,7 +88,15 @@ export default function UserAcknowledgment({ currentStep, onFinish }: UserAcknow
           </div>
         )}
         <span className="create-common-user-acknowledgment__terms-text">
-          For more details and information refer to the <ButtonLink className="create-common-user-acknowledgment__terms-link">Terms of Use.</ButtonLink>
+          For more details and information refer to the{" "}
+          <ButtonLink
+            className="create-common-user-acknowledgment__terms-link"
+            href={TERMS_OF_USE_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Terms of Use.
+          </ButtonLink>
         </span>
         <Checkbox
           className="create-common-user-acknowledgment__terms-checkbox"
