31dcf39dfb0d18e462d96eb6c5f93c360ddc7bfc
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-02-04 12:18:12 +0300

    Use general button component in funding step

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Funding/Funding.tsx
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/Funding/Funding.tsx
@@ -1,12 +1,19 @@
 import React, { useCallback, useRef, ReactElement, ReactNode } from "react";
+import { useSelector } from "react-redux";
 import { Formik, FormikConfig } from "formik";
 import { FormikProps } from "formik/dist/types";
-
-import { isMobile } from "@/shared/utils";
-import { Separator } from "@/shared/components";
+import { Button, Separator } from "@/shared/components";
 import { ModalFooter, ModalHeaderContent } from "@/shared/components/Modal";
-import { Checkbox, CurrencyInput, Form, ToggleButtonGroup, ToggleButton } from "@/shared/components/Form/Formik";
+import {
+  Checkbox,
+  CurrencyInput,
+  Form,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@/shared/components/Form/Formik";
+import { ScreenSize } from "@/shared/constants";
 import { CommonContributionType } from "@/shared/models";
+import { getScreenSize } from "@/shared/store/selectors";
 import { IntermediateCreateCommonPayload } from "../../../../../interfaces";
 import { Progress } from "../Progress";
 import { MIN_CONTRIBUTION_VALUE } from "./constants";
@@ -33,28 +40,51 @@ const getInitialValues = (
   isCommonJoinFree: data.zeroContribution || false,
 });
 
-const getCurrencyInputLabel = (contributionType: CommonContributionType, isMobileView: boolean): ReactNode => {
-  const contributionText = contributionType === CommonContributionType.OneTime ? "one-time" : "monthly";
-  const additionalText = isMobileView && contributionType === CommonContributionType.Monthly
-    ? ` (min. $${MIN_CONTRIBUTION_VALUE})`
-    : "";
+const getCurrencyInputLabel = (
+  contributionType: CommonContributionType,
+  isMobileView: boolean
+): ReactNode => {
+  const contributionText =
+    contributionType === CommonContributionType.OneTime
+      ? "one-time"
+      : "monthly";
+  const additionalText =
+    isMobileView && contributionType === CommonContributionType.Monthly
+      ? ` (min. $${MIN_CONTRIBUTION_VALUE})`
+      : "";
 
-  return <>Minimum <strong>{contributionText}</strong> contribution{additionalText}</>;
+  return (
+    <>
+      Minimum <strong>{contributionText}</strong> contribution{additionalText}
+    </>
+  );
 };
 
-const getCurrencyInputDescription = (contributionType: CommonContributionType, isMobileView: boolean): string => {
-  const descriptionPieces = ["Set the minimum amount that new members will have to contribute in order to join this Common."];
+const getCurrencyInputDescription = (
+  contributionType: CommonContributionType,
+  isMobileView: boolean
+): string => {
+  const descriptionPieces = [
+    "Set the minimum amount that new members will have to contribute in order to join this Common.",
+  ];
 
   if (!isMobileView || contributionType !== CommonContributionType.Monthly) {
-    descriptionPieces.push(`The minimum contribution allowed by credit card is $${MIN_CONTRIBUTION_VALUE}.`);
+    descriptionPieces.push(
+      `The minimum contribution allowed by credit card is $${MIN_CONTRIBUTION_VALUE}.`
+    );
   }
 
   return descriptionPieces.join(" ");
 };
 
-export default function Funding({ currentStep, onFinish, creationData }: FundingProps): ReactElement {
+export default function Funding({
+  currentStep,
+  onFinish,
+  creationData,
+}: FundingProps): ReactElement {
   const formRef = useRef<FormikProps<FormValues>>(null);
-  const isMobileView = isMobile();
+  const screenSize = useSelector(getScreenSize());
+  const isMobileView = screenSize === ScreenSize.Mobile;
 
   const handleContinueClick = useCallback(() => {
     if (formRef.current) {
@@ -62,23 +92,22 @@ export default function Funding({ currentStep, onFinish, creationData }: Funding
     }
   }, []);
 
-  const handleSubmit = useCallback<FormikConfig<FormValues>['onSubmit']>((values) => {
-    onFinish({
-      contributionType: values.contributionType,
-      contributionAmount: values.minimumContribution || 0,
-      zeroContribution: values.isCommonJoinFree,
-    });
-  }, [onFinish]);
+  const handleSubmit = useCallback<FormikConfig<FormValues>["onSubmit"]>(
+    (values) => {
+      onFinish({
+        contributionType: values.contributionType,
+        contributionAmount: values.minimumContribution || 0,
+        zeroContribution: values.isCommonJoinFree,
+      });
+    },
+    [onFinish]
+  );
 
   const progressEl = <Progress creationStep={currentStep} />;
 
   return (
     <>
-      {!isMobileView && (
-        <ModalHeaderContent>
-          {progressEl}
-        </ModalHeaderContent>
-      )}
+      {!isMobileView && <ModalHeaderContent>{progressEl}</ModalHeaderContent>}
       <div className="create-common-funding">
         {isMobileView && progressEl}
         <Separator className="create-common-funding__separator" />
@@ -109,12 +138,16 @@ export default function Funding({ currentStep, onFinish, creationData }: Funding
                 id="minimumContribution"
                 name="minimumContribution"
                 label={getCurrencyInputLabel(contributionType, isMobileView)}
-                description={getCurrencyInputDescription(contributionType, isMobileView)}
+                description={getCurrencyInputDescription(
+                  contributionType,
+                  isMobileView
+                )}
                 placeholder={`$${MIN_CONTRIBUTION_VALUE}`}
                 allowDecimals={false}
                 styles={{
                   label: "create-common-funding__field-label",
-                  description: "create-common-funding__currency-input-description",
+                  description:
+                    "create-common-funding__currency-input-description",
                   error: "create-common-funding__currency-input-error",
                 }}
               />
@@ -131,14 +164,14 @@ export default function Funding({ currentStep, onFinish, creationData }: Funding
               )}
               <ModalFooter sticky>
                 <div className="create-common-funding__modal-footer">
-                  <button
+                  <Button
                     key="funding-continue"
-                    className="button-blue"
                     onClick={handleContinueClick}
+                    shouldUseFullWidth={isMobileView}
                     disabled={!isValid}
                   >
                     Continue to Rules
-                  </button>
+                  </Button>
                 </div>
               </ModalFooter>
             </Form>
