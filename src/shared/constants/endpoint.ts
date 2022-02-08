b6f929ab794e0cf10e126b0de5a3baa95116d0fd
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-02-08 14:29:09 +0300

    Update reports download endpoint url

--- src/shared/constants/endpoint.ts
+++ src/shared/constants/endpoint.ts
@@ -2,5 +2,5 @@ export const ApiEndpoint = {
   CreateRequestToJoin: "/proposals/create/join",
   UploadInvoices: "/payments/payout-docs/add",
   ApproveOrDeclineProposal: "/payments/payout-docs/trustee-decision",
-  GetReports: "/reports",
+  GetReportsArchive: "/reports/archive",
 };
--- src/shared/utils/getReportsDownloadURL.ts
+++ src/shared/utils/getReportsDownloadURL.ts
@@ -2,4 +2,4 @@ import config from "../../config";
 import { ApiEndpoint } from "../constants/endpoint";
 
 export const getReportsDownloadURL = (): string =>
-  `${config.cloudFunctionUrl}${ApiEndpoint.GetReports}`;
+  `${config.cloudFunctionUrl}${ApiEndpoint.GetReportsArchive}`;
