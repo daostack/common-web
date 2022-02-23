038638e4b62cf078ee0e82f30c999b43287c16a2
Roie Natan <roie.natan@gmail.com>
2022-02-23 07:54:31 -0500

    add return type to sendEmail function

--- src/shared/utils/sendEmail.ts
+++ src/shared/utils/sendEmail.ts
@@ -3,6 +3,6 @@ import Api from "../../services/Api";
 import { ApiEndpoint } from "../constants";
 import { SendEmail } from "../interfaces/SendEmail";
 
-export async function sendEmail(requestData: SendEmail) {
+export async function sendEmail(requestData: SendEmail): Promise<void> {
   await Api.post(ApiEndpoint.SendEmail, requestData);
 }
