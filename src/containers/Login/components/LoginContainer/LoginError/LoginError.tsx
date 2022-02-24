52c4e2d403afddb4716f6532ac057b7fe04e739f
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-02-24 15:42:17 +0300

    Refactor login error component

--- src/containers/Login/components/LoginContainer/LoginError/LoginError.tsx
+++ src/containers/Login/components/LoginContainer/LoginError/LoginError.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import "../../../containers/LoginContainer/index.scss";
+import React, { FC } from "react";
+import classNames from "classnames";
 import "./index.scss";
 
-const LoginError = () => {
-  return (
-    <div className="login-error">
-      There was an error logging you in. Please try again or try connecting a
-      different account.
-    </div>
-  );
-};
+interface LoginErrorProps {
+  className?: string;
+}
+
+const LoginError: FC<LoginErrorProps> = ({ className }) => (
+  <span className={classNames("login-error", className)}>
+    There was an error logging you in. Please try again or try connecting a
+    different account.
+  </span>
+);
 
 export default LoginError;
