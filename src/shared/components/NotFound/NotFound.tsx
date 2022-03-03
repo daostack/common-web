388009fe8e9090448420f07d32f5d7ad14fd884b
Roie Natan <roie.natan@gmail.com>
2022-03-02 22:04:55 -0600

    updates to 404 page

--- src/shared/components/NotFound/NotFound.tsx
+++ src/shared/components/NotFound/NotFound.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from "react";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { ROUTE_PATHS } from "@/shared/constants";
+import Button from "../Button/Button";
 import "./index.scss";
 
-const NotFound: FC = () => (
-  <div className="page-not-found">
-    <span className="page-not-found__code">404</span>
-    <h3 className="page-not-found__message">This page could not be found.</h3>
-  </div>
-);
-
-export default NotFound;
+export default function NotFound() {
+  const history = useHistory();
+  
+  return (
+    <div className="page-not-found">
+      <img src="/assets/images/404.jpg" alt="404" />
+      <h3 className="page-not-found__message">Woops, the page you’re looking for isn’t there</h3>
+      <Button onClick={() => history.push(ROUTE_PATHS.HOME)}>Back to Home</Button>
+    </div>
+  )
+}
