23e69bf2215aca8c0f6d8065b6ab3b5b0ad663fa
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-03-17 10:41:13 +0400

    Block entering of new characters in input if max length is set

--- src/shared/components/Form/Input/Input.tsx
+++ src/shared/components/Form/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FC } from "react";
+import React, { useState, ChangeEventHandler, FC } from "react";
 import classNames from "classnames";
 
 import { ErrorText } from "../ErrorText";
@@ -14,11 +14,11 @@ interface InputStyles {
   error?: string;
 }
 
-type InputProps = JSX.IntrinsicElements['input'] & { isTextarea?: false };
-type TextareaProps = JSX.IntrinsicElements['textarea'] & { isTextarea: true };
+type InputProps = JSX.IntrinsicElements["input"] & { isTextarea?: false };
+type TextareaProps = JSX.IntrinsicElements["textarea"] & { isTextarea: true };
 type FilterExtraPropsFunction = {
-  (props: InputProps): JSX.IntrinsicElements['input'];
-  (props: TextareaProps): JSX.IntrinsicElements['textarea'];
+  (props: InputProps): JSX.IntrinsicElements["input"];
+  (props: TextareaProps): JSX.IntrinsicElements["textarea"];
 };
 export type FullInputProps = (InputProps | TextareaProps) & {
   name: string;
@@ -32,33 +32,84 @@ export type FullInputProps = (InputProps | TextareaProps) & {
   styles?: InputStyles;
 };
 
-const filterExtraProps: FilterExtraPropsFunction = <T extends (InputProps | TextareaProps)>(props: T) => {
+const filterExtraProps: FilterExtraPropsFunction = <
+  T extends InputProps | TextareaProps
+>(
+  props: T
+) => {
   const { isTextarea, ...restProps } = props;
 
   return restProps;
 };
 
+const isChangeAllowed = (value: string, maxLength?: number): boolean =>
+  !maxLength || value.length <= maxLength;
+
 const Input: FC<FullInputProps> = (props) => {
-  const { className, label, description, hint, maxLength, shouldDisplayCount, error, styles, ...restProps } = props;
-  const [inputLengthRef, setInputLengthRef] = useState<HTMLSpanElement | null>(null);
+  const {
+    className,
+    label,
+    description,
+    hint,
+    maxLength,
+    shouldDisplayCount,
+    error,
+    styles,
+    ...restProps
+  } = props;
+  const [inputLengthRef, setInputLengthRef] = useState<HTMLSpanElement | null>(
+    null
+  );
   const id = restProps.id || restProps.name;
-  const shouldDisplayCountToUse = shouldDisplayCount ?? Boolean(maxLength && maxLength > 0);
-  const inputStyles = shouldDisplayCountToUse && inputLengthRef
-    ? { paddingRight: inputLengthRef.clientWidth + 14 }
-    : undefined;
-  const inputClassName = classNames("custom-input__input", styles?.input?.default, {
-    "custom-input__input--error": error,
-  });
+  const shouldDisplayCountToUse =
+    shouldDisplayCount ?? Boolean(maxLength && maxLength > 0);
+  const inputStyles =
+    shouldDisplayCountToUse && inputLengthRef
+      ? { paddingRight: inputLengthRef.clientWidth + 14 }
+      : undefined;
+  const inputClassName = classNames(
+    "custom-input__input",
+    styles?.input?.default,
+    {
+      "custom-input__input--error": error,
+    }
+  );
   const generalInputProps = {
     id,
     className: inputClassName,
     style: inputStyles,
   };
 
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (
+      props.onChange &&
+      !props.isTextarea &&
+      isChangeAllowed(event.target.value, maxLength)
+    ) {
+      props.onChange(event);
+    }
+  };
+  const handleTextareaChange: ChangeEventHandler<HTMLTextAreaElement> = (
+    event
+  ) => {
+    if (
+      props.onChange &&
+      props.isTextarea &&
+      isChangeAllowed(event.target.value, maxLength)
+    ) {
+      props.onChange(event);
+    }
+  };
+
   return (
     <div className={classNames("custom-input", className)}>
       {(label || hint) && (
-        <div className={classNames("custom-input__label-wrapper", styles?.labelWrapper)}>
+        <div
+          className={classNames(
+            "custom-input__label-wrapper",
+            styles?.labelWrapper
+          )}
+        >
           {label && (
             <label
               htmlFor={id}
@@ -71,7 +122,12 @@ const Input: FC<FullInputProps> = (props) => {
         </div>
       )}
       {description && (
-        <p className={classNames("custom-input__description", styles?.description)}>
+        <p
+          className={classNames(
+            "custom-input__description",
+            styles?.description
+          )}
+        >
           {description}
         </p>
       )}
@@ -80,24 +136,26 @@ const Input: FC<FullInputProps> = (props) => {
           <input
             {...filterExtraProps(restProps)}
             {...generalInputProps}
+            onChange={handleInputChange}
           />
         )}
         {restProps.isTextarea && (
           <textarea
             {...filterExtraProps(restProps)}
             {...generalInputProps}
+            onChange={handleTextareaChange}
           />
         )}
         {shouldDisplayCountToUse && (
-          <span
-            className="custom-input__input-length"
-            ref={setInputLengthRef}
-          >
-            {typeof restProps.value === 'string' ? restProps.value.length : 0}/{maxLength}
+          <span className="custom-input__input-length" ref={setInputLengthRef}>
+            {typeof restProps.value === "string" ? restProps.value.length : 0}/
+            {maxLength}
           </span>
         )}
       </div>
-      {Boolean(error) && <ErrorText className={styles?.error}>{error}</ErrorText>}
+      {Boolean(error) && (
+        <ErrorText className={styles?.error}>{error}</ErrorText>
+      )}
     </div>
   );
 };
