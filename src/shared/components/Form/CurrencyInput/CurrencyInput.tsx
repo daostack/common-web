b3a6468cfc35ca3a4c08287915296cf23dfca132
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-03-31 15:08:16 +0400

    reformat currency input component

--- src/shared/components/Form/CurrencyInput/CurrencyInput.tsx
+++ src/shared/components/Form/CurrencyInput/CurrencyInput.tsx
@@ -1,10 +1,16 @@
 import React, { FC, ReactNode } from "react";
 import classNames from "classnames";
-import BaseCurrencyInput, { CurrencyInputProps as BaseCurrencyInputProps } from "react-currency-input-field";
-import { IntlConfig } from "react-currency-input-field/dist/components/CurrencyInputProps";
-
+import BaseCurrencyInput, {
+  CurrencyInputProps as BaseCurrencyInputProps,
+} from "react-currency-input-field";
 import { ErrorText } from "../ErrorText";
+import { DEFAULT_INTL_CONFIG } from "./constants";
 import "./index.scss";
+
+enum CurrencyInputVariant {
+  Default,
+  Middle,
+}
 
 interface CurrencyInputStyles {
   label?: string;
@@ -21,6 +27,7 @@ export interface CurrencyInputProps extends BaseCurrencyInputProps {
   description?: string;
   hint?: string;
   error?: string;
+  variant?: CurrencyInputVariant;
   styles?: CurrencyInputStyles;
 }
 
@@ -29,14 +36,33 @@ const DEFAULT_INTL_CONFIG: IntlConfig = {
 };
 
 const CurrencyInput: FC<CurrencyInputProps> = (props) => {
-  const { className, label, description, hint, error, styles, allowNegativeValue, intlConfig, prefix, ...restProps } = props;
+  const {
+    className,
+    label,
+    description,
+    hint,
+    error,
+    styles,
+    allowNegativeValue,
+    intlConfig,
+    prefix,
+    variant = CurrencyInputVariant.Default,
+    ...restProps
+  } = props;
   const id = restProps.id || restProps.name;
-  const labelWrapperClassName = classNames("custom-currency-input__label-wrapper", {
-    "custom-currency-input__label-wrapper--with-description": description,
-  });
-  const inputClassName = classNames("custom-currency-input__input", styles?.input?.default, {
-    "custom-currency-input__input--error": error,
-  });
+  const labelWrapperClassName = classNames(
+    "custom-currency-input__label-wrapper",
+    {
+      "custom-currency-input__label-wrapper--with-description": description,
+    }
+  );
+  const inputClassName = classNames(
+    "custom-currency-input__input",
+    styles?.input?.default,
+    {
+      "custom-currency-input__input--error": error,
+    }
+  );
 
   return (
     <div className={classNames("custom-currency-input", className)}>
@@ -45,7 +71,10 @@ const CurrencyInput: FC<CurrencyInputProps> = (props) => {
           {label && (
             <label
               htmlFor={id}
-              className={classNames("custom-currency-input__label", styles?.label)}
+              className={classNames(
+                "custom-currency-input__label",
+                styles?.label
+              )}
             >
               {label}
             </label>
@@ -54,7 +83,12 @@ const CurrencyInput: FC<CurrencyInputProps> = (props) => {
         </div>
       )}
       {description && (
-        <p className={classNames("custom-currency-input__description", styles?.description)}>
+        <p
+          className={classNames(
+            "custom-currency-input__description",
+            styles?.description
+          )}
+        >
           {description}
         </p>
       )}
@@ -68,7 +102,9 @@ const CurrencyInput: FC<CurrencyInputProps> = (props) => {
           prefix={prefix ?? "₪"}
         />
       </div>
-      {Boolean(error) && <ErrorText className={styles?.error}>{error}</ErrorText>}
+      {Boolean(error) && (
+        <ErrorText className={styles?.error}>{error}</ErrorText>
+      )}
     </div>
   );
 };
