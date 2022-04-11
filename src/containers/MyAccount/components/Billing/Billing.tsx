fa4c3ff080dee64f0004f0fedd1a87199bfb23c8
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-04-11 14:42:19 +0400

    create basic billing my account page

---
+++ src/containers/MyAccount/components/Billing/Billing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+export default function Billing() {
+  return (
+    <div className="route-content my-account-billing">
+      <header className="my-account-billing__header">
+        <h2 className="route-title">Billing</h2>
+      </header>
+    </div>
+  );
+}
