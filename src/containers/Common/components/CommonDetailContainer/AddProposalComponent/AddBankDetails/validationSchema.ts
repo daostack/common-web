a561b0fc3e8da10800cc61b0c4b12c26999c90e3
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-04-12 15:39:21 +0400

    add validation for gender

--- src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddBankDetails/validationSchema.ts
+++ src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddBankDetails/validationSchema.ts
@@ -1,16 +1,31 @@
 import * as Yup from "yup";
 import { NUMBERS_ONLY_REGEXP } from "@/shared/constants";
+import { Gender, GENDER_OPTIONS } from "@/shared/models/Gender";
 
 const schema = Yup.object().shape({
-  idNumber: Yup.string().required("Please enter your ID number").matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
-  gender: Yup.number().required("Please choose gender"),
-  phoneNumber: Yup.string().required("Please etner a phone number").matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
-  email: Yup.string().required("Please enter your email").email("Please enter a valid email address"),
+  idNumber: Yup.string()
+    .required("Please enter your ID number")
+    .matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
+  gender: Yup.number()
+    .oneOf(GENDER_OPTIONS.map((option) => option.value as Gender))
+    .required("Please choose gender"),
+  phoneNumber: Yup.string()
+    .required("Please etner a phone number")
+    .matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
+  email: Yup.string()
+    .required("Please enter your email")
+    .email("Please enter a valid email address"),
   bankCode: Yup.number().required("Please choose a bank"),
-  branchNumber: Yup.string().required("Please enter the branch number").matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
-  accountNumber: Yup.string().required("Please enter the account number").matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
+  branchNumber: Yup.string()
+    .required("Please enter the branch number")
+    .matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
+  accountNumber: Yup.string()
+    .required("Please enter the account number")
+    .matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
   address: Yup.string().required("Please enter your address"),
-  streetNumber: Yup.string().required("Please enter the street number").matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
+  streetNumber: Yup.string()
+    .required("Please enter the street number")
+    .matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
   city: Yup.string().required("Please enter your city"),
 });
 
