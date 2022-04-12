079da3fbf1006598dd058e9d29b1da4e2af16301
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-04-12 16:06:12 +0400

    update phone number validation

--- src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddBankDetails/validationSchema.ts
+++ src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddBankDetails/validationSchema.ts
@@ -1,4 +1,5 @@
 import * as Yup from "yup";
+import { isPossiblePhoneNumber } from "react-phone-number-input";
 import { NUMBERS_ONLY_REGEXP } from "@/shared/constants";
 import { Gender, GENDER_OPTIONS } from "@/shared/models/Gender";
 
@@ -10,8 +11,12 @@ const schema = Yup.object().shape({
     .oneOf(GENDER_OPTIONS.map((option) => option.value as Gender))
     .required("Please choose gender"),
   phoneNumber: Yup.string()
-    .required("Please etner a phone number")
-    .matches(NUMBERS_ONLY_REGEXP, "Only numbers allowed"),
+    .required("Please enter a phone number")
+    .test(
+      "phone-number",
+      "Please enter correct phone number",
+      (value) => value && isPossiblePhoneNumber(value)
+    ),
   email: Yup.string()
     .required("Please enter your email")
     .email("Please enter a valid email address"),
