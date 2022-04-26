2ffcb6b290c12c60f66dbddb1e1c8a7dc609db9c
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-04-26 09:48:22 +0400

    resolve warnings in the code

--- src/containers/MyAccount/components/Billing/Contributions/Contributions.tsx
+++ src/containers/MyAccount/components/Billing/Contributions/Contributions.tsx
@@ -1,16 +1,6 @@
 import React, { FC } from "react";
-import { useSelector } from "react-redux";
-import { Modal, PaymentMethod } from "@/shared/components";
-import { ScreenSize } from "@/shared/constants";
-import { ChangePaymentMethodState } from "@/shared/hooks/useCases";
-import QuestionOutlineIcon from "@/shared/icons/questionOutline.icon";
-import { ModalType } from "@/shared/interfaces";
 import { Payment, Subscription } from "@/shared/models";
-import { getScreenSize } from "@/shared/store/selectors";
-import { AddingCard } from "../AddingCard";
 import { ContributionList } from "../ContributionList";
-import { ChangePaymentMethod } from "../ChangePaymentMethod";
-import { PaymentMethodUpdateSuccess } from "../PaymentMethodUpdateSuccess";
 import "./index.scss";
 
 interface ContributionsProps {
