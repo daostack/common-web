729b966a6b9c84bdaf339f08db8239e36a631ef9
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-04-29 15:11:55 +0400

    create jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/public/"],
+  testEnvironment: "jsdom",
+};
