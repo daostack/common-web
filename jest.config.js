8e88f17dbd44f23410f904373c6e6cc80028325a
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-05-13 14:57:04 +0300

    update config

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,17 @@ module.exports = {
   transform: {
     "^.+\\.[jt]sx?$": "ts-jest",
   },
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect",
+    "<rootDir>/src/shared/utils/tests/setupTests.ts",
+  ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
+    "^.+\\.(gif|ttf|eot|svg|png)$":
+      "<rootDir>/src/shared/utils/tests/mockFile.ts",
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   testPathIgnorePatterns: ["/node_modules/", "/public/"],
   testEnvironment: "jsdom",
-  setupFiles: ["<rootDir>/src/shared/utils/tests/setupTests.ts"],
 };
