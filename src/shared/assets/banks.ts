c8f3ed76ec85be2db8d71a0a5e28a3490234b526
Roie Natan <roie.natan@gmail.com>
2022-05-14 16:29:41 +0400

    banks list updated

--- src/shared/assets/banks.ts
+++ src/shared/assets/banks.ts
@@ -15,4 +15,44 @@ export const BANKS_OPTIONS = [
     value: 11,
     name: 'Israel Discount Bank',
   },
+  {
+    value: 4,
+    name: 'Bank Yahav'
+  },
+  {
+    value: 9,
+    name: 'Bank Hadoar'
+  },
+  {
+    value: 13,
+    name: 'Union Bank'
+  },
+  {
+    value: 14,
+    name: 'Bank Otsar Ha-Hayal'
+  },
+  {
+    value: 17,
+    name: 'Mercantile'
+  },
+  {
+    value: 26,
+    name: 'UBank'
+  },
+  {
+    value: 31,
+    name: 'First International Bank of Israel'
+  },
+  {
+    value: 46,
+    name: 'Bank Massad'
+  },
+  {
+    value: 54,
+    name: 'Bank of Jerusalem'
+  },
+  {
+    value: 99,
+    name: 'Bank of Israel'
+  }
 ];
