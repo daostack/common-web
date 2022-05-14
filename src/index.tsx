67d6dfcc8ef08169791d0c209d9f85ea40de19b9
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-05-14 13:51:58 +0300

    create app wrapper with routing and store set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { history, store } from "@/shared/appConfig";
+import AppWrapper from "@/containers/App/AppWrapper";
 import "./index.scss";
 import App from "./containers/App/App";
 
-if (process.env.REACT_APP_ENV === "dev") {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const whyDidYouRender = require("@welldone-software/why-did-you-render");
-  whyDidYouRender(React, { trackHooks: true });
-}
-
 ReactDOM.render(
-  <Router history={history}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>,
+  <AppWrapper>
+    <App />
+  </AppWrapper>,
   document.getElementById("root")
 );
