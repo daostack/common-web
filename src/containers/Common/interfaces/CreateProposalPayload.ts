0e4180af2c456817d10b1db2d5c81a5428b83536
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-05-23 10:39:19 +0300

    create type for proposal creation

---
+++ src/containers/Common/interfaces/CreateProposalPayload.ts
@@ -0,0 +1,19 @@
+import { ProposalsTypes } from "@/shared/constants";
+import {
+  FundsAllocation,
+  MemberAdmittance,
+} from "@/shared/models/governance/proposals";
+
+interface CreateFundsAllocation {
+  type: ProposalsTypes.FUNDS_ALLOCATION;
+  args: Omit<FundsAllocation["data"]["args"], "proposerId">;
+}
+
+interface CreateMemberAdmittance {
+  type: ProposalsTypes.MEMBER_ADMITTANCE;
+  args: Omit<MemberAdmittance["data"]["args"], "proposerId">;
+}
+
+export type CreateProposalPayload =
+  | CreateFundsAllocation
+  | CreateMemberAdmittance;
