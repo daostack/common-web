79494bfe8a37fb1c4420ce170e8b778ab04d1458
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-06-24 12:28:01 +0300

    make contact us form component dumb

--- src/containers/Landing/components/ContactUsContainer/ContactUsForm/ContactUsForm.tsx
+++ src/containers/Landing/components/ContactUsContainer/ContactUsForm/ContactUsForm.tsx
@@ -1,17 +1,19 @@
-import React, { FC, useCallback, useState } from "react";
-import { useDispatch } from "react-redux";
-import { Formik, FormikConfig } from "formik";
-import { sendEmail } from "@/containers/Landing/store/actions";
+import React, { FC } from "react";
+import { Formik } from "formik";
 import { Button, Loader } from "@/shared/components";
 import { ErrorText } from "@/shared/components/Form";
 import { Form, TextField } from "@/shared/components/Form/Formik";
 import { SUPPORT_EMAIL } from "@/shared/constants";
-import { useNotification } from "@/shared/hooks";
-import { EmailType } from "@/shared/interfaces/SendEmail";
 import validationSchema from "./validationSchema";
 import "./index.scss";
 
-interface FormValues {
+interface ContactUsFormProps {
+  onSubmit: (values: ContactUsFormValues) => void;
+  isLoading?: boolean;
+  errorText?: string;
+}
+
+export interface ContactUsFormValues {
   name: string;
   commonTitle: string;
   description: string;
@@ -20,7 +22,7 @@ interface FormValues {
   phoneNumber: string;
 }
 
-const INITIAL_VALUES: FormValues = {
+const INITIAL_VALUES: ContactUsFormValues = {
   name: "",
   commonTitle: "",
   description: "",
@@ -29,162 +31,122 @@ const INITIAL_VALUES: FormValues = {
   phoneNumber: "",
 };
 
-const ContactUsForm: FC = () => {
-  const dispatch = useDispatch();
-  const [errorText, setErrorText] = useState("");
-  const { notify } = useNotification();
-
-  const handleSubmit = useCallback<FormikConfig<FormValues>["onSubmit"]>(
-    (values, { setSubmitting }) => {
-      setSubmitting(true);
-      setErrorText("");
-
-      const description = [
-        values.description,
-        `Common title: ${values.commonTitle}`,
-        `Residence: ${values.residence}`,
-        `Phone number: ${values.phoneNumber}`,
-      ].join("\n");
-
-      dispatch(
-        sendEmail.request({
-          payload: {
-            senderEmail: values.email,
-            senderName: values.name,
-            type: EmailType.ContactUsAdmin,
-            text: description,
-          },
-          callback: (error) => {
-            if (error) {
-              setErrorText(error?.message ?? "Something went wrong");
-            } else {
-              notify("The form is successfully sent!");
-            }
-
-            setSubmitting(false);
-          },
-        })
-      );
-    },
-    [dispatch, notify]
-  );
+const ContactUsForm: FC<ContactUsFormProps> = (props) => {
+  const { onSubmit, isLoading = false, errorText } = props;
 
   return (
     <Formik
       initialValues={INITIAL_VALUES}
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
       validationSchema={validationSchema}
       validateOnMount
     >
-      {({ isSubmitting }) => (
-        <Form className="contact-us-contact-us-form">
-          <TextField
-            className="contact-us-contact-us-form__text-field"
-            id="userName"
-            name="name"
-            label="Your name"
-            styles={{
-              label: "contact-us-contact-us-form__label",
-              input: {
-                default: "contact-us-contact-us-form__input",
-              },
-            }}
-          />
-          <TextField
-            className="contact-us-contact-us-form__text-field"
-            id="commonTitle"
-            name="commonTitle"
-            label="What is the title of the Common you want to launch?"
-            styles={{
-              label: "contact-us-contact-us-form__label",
-              input: {
-                default: "contact-us-contact-us-form__input",
-              },
-            }}
-          />
-          <TextField
-            className="contact-us-contact-us-form__text-field"
-            id="description"
-            name="description"
-            label="Please tell us a bit about the initiative and the people behind it"
-            styles={{
-              label: "contact-us-contact-us-form__label",
-              input: {
-                default:
-                  "contact-us-contact-us-form__input contact-us-contact-us-form__textarea",
-              },
-            }}
-            isTextarea
-          />
-          <TextField
-            className="contact-us-contact-us-form__text-field"
-            id="residence"
-            name="residence"
-            label="Where are you from?"
-            styles={{
-              label: "contact-us-contact-us-form__label",
-              input: {
-                default: "contact-us-contact-us-form__input",
-              },
-            }}
-          />
-          <TextField
-            className="contact-us-contact-us-form__text-field"
-            id="email"
-            name="email"
-            label="Email"
-            placeholder="example@email.com"
-            styles={{
-              label: "contact-us-contact-us-form__label",
-              input: {
-                default: "contact-us-contact-us-form__input",
-              },
-            }}
-          />
-          <TextField
-            className="contact-us-contact-us-form__text-field"
-            id="phoneNumber"
-            name="phoneNumber"
-            label="Phone number"
-            placeholder="+972-"
-            styles={{
-              label: "contact-us-contact-us-form__label",
-              input: {
-                default: "contact-us-contact-us-form__input",
-              },
-            }}
-          />
-          {isSubmitting && (
-            <div className="contact-us-contact-us-form__loader">
-              <Loader />
-            </div>
-          )}
-          {!isSubmitting && (
-            <Button
-              className="contact-us-contact-us-form__submit-button"
-              type="submit"
-              disabled={isSubmitting}
-              shouldUseFullWidth
-            >
-              Send
-            </Button>
-          )}
-          {errorText && (
-            <ErrorText className="contact-us-contact-us-form__error">
-              {errorText}
-            </ErrorText>
-          )}
-          <span className="contact-us-contact-us-form__support-email-wrapper">
-            Contact us at:{" "}
-            <a
-              className="contact-us-contact-us-form__support-email"
-              href={`mailto:${SUPPORT_EMAIL}`}
-            >
-              {SUPPORT_EMAIL}
-            </a>
-          </span>
-        </Form>
-      )}
+      <Form className="contact-us-contact-us-form">
+        <TextField
+          className="contact-us-contact-us-form__text-field"
+          id="userName"
+          name="name"
+          label="Your name"
+          styles={{
+            label: "contact-us-contact-us-form__label",
+            input: {
+              default: "contact-us-contact-us-form__input",
+            },
+          }}
+        />
+        <TextField
+          className="contact-us-contact-us-form__text-field"
+          id="commonTitle"
+          name="commonTitle"
+          label="What is the title of the Common you want to launch?"
+          styles={{
+            label: "contact-us-contact-us-form__label",
+            input: {
+              default: "contact-us-contact-us-form__input",
+            },
+          }}
+        />
+        <TextField
+          className="contact-us-contact-us-form__text-field"
+          id="description"
+          name="description"
+          label="Please tell us a bit about the initiative and the people behind it"
+          styles={{
+            label: "contact-us-contact-us-form__label",
+            input: {
+              default:
+                "contact-us-contact-us-form__input contact-us-contact-us-form__textarea",
+            },
+          }}
+          isTextarea
+        />
+        <TextField
+          className="contact-us-contact-us-form__text-field"
+          id="residence"
+          name="residence"
+          label="Where are you from?"
+          styles={{
+            label: "contact-us-contact-us-form__label",
+            input: {
+              default: "contact-us-contact-us-form__input",
+            },
+          }}
+        />
+        <TextField
+          className="contact-us-contact-us-form__text-field"
+          id="email"
+          name="email"
+          label="Email"
+          placeholder="example@email.com"
+          styles={{
+            label: "contact-us-contact-us-form__label",
+            input: {
+              default: "contact-us-contact-us-form__input",
+            },
+          }}
+        />
+        <TextField
+          className="contact-us-contact-us-form__text-field"
+          id="phoneNumber"
+          name="phoneNumber"
+          label="Phone number"
+          placeholder="+972-"
+          styles={{
+            label: "contact-us-contact-us-form__label",
+            input: {
+              default: "contact-us-contact-us-form__input",
+            },
+          }}
+        />
+        {isLoading && (
+          <div className="contact-us-contact-us-form__loader">
+            <Loader />
+          </div>
+        )}
+        {!isLoading && (
+          <Button
+            className="contact-us-contact-us-form__submit-button"
+            type="submit"
+            shouldUseFullWidth
+          >
+            Send
+          </Button>
+        )}
+        {errorText && (
+          <ErrorText className="contact-us-contact-us-form__error">
+            {errorText}
+          </ErrorText>
+        )}
+        <span className="contact-us-contact-us-form__support-email-wrapper">
+          Contact us at:{" "}
+          <a
+            className="contact-us-contact-us-form__support-email"
+            href={`mailto:${SUPPORT_EMAIL}`}
+          >
+            {SUPPORT_EMAIL}
+          </a>
+        </span>
+      </Form>
     </Formik>
   );
 };
