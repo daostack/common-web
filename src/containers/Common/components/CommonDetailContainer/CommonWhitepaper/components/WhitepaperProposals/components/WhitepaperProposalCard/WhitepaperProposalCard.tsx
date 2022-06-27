4eece88227b3bd07861ff7bb459687dba5397cd5
Roie Natan <roie.natan@gmail.com>
2022-06-27 18:16:05 +0400

    use ProposalsTypes enum

--- src/containers/Common/components/CommonDetailContainer/CommonWhitepaper/components/WhitepaperProposals/components/WhitepaperProposalCard/WhitepaperProposalCard.tsx
+++ src/containers/Common/components/CommonDetailContainer/CommonWhitepaper/components/WhitepaperProposals/components/WhitepaperProposalCard/WhitepaperProposalCard.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames";
 import { startCase, lowerCase } from "lodash";
 import { Proposals } from "@/shared/models/governance/proposals";
 import { Proposal } from "@/shared/models";
+import { ProposalsTypes } from "@/shared/constants";
 import { calculateVoters } from "../../../../utils";
 import "./index.scss";
 
@@ -14,14 +15,14 @@ interface IProps {
 }
 
 // TODO: temporary until we have a better way to handle this
-const PROPOSALS_TYPE_1 = ["FUNDS_ALLOCATION", "MEMBER_ADMITTANCE"];
-//const PROPOSALS_TYPE_2 = ["ASSIGN_CIRCLE", "REMOVE_CIRCLE"];
+const PROPOSALS_TYPE_1 = [ProposalsTypes.FUNDS_ALLOCATION, ProposalsTypes.MEMBER_ADMITTANCE];
+//const PROPOSALS_TYPE_2 = [ProposalsTypes.ASSIGN_CIRCLE, ProposalsTypes.REMOVE_CIRCLE];
 
 export default function WhitepaperProposalCard({ circles, proposalType, proposalData }: IProps) {
   const [toggle, setToggle] = useState(false);
 
   // TODO: for now we take the first index for "CIRCLE" proposal. Need to check this.
-  const data = PROPOSALS_TYPE_1.includes(proposalType) ? proposalData as Proposal : proposalData[1] as Proposal;
+  const data = PROPOSALS_TYPE_1.includes(proposalType as ProposalsTypes) ? proposalData as Proposal : proposalData[1] as Proposal;
 
   const voters = calculateVoters(circles, data.global.weights)?.map((voter, index) => {
     return <span className="whitepaper-proposal-card__voter" key={index}>{voter}</span>
