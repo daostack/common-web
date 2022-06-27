e6f09b3d64a015cea1a5cf19311d91e3decc4f38
SobolevYuri <yura.sobolev.91@gmail.com>
2022-06-27 19:24:44 +0200

    CW-528 Saving of create proposal intermediate progress

--- src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddProposalComponent.tsx
+++ src/containers/Common/components/CommonDetailContainer/AddProposalComponent/AddProposalComponent.tsx
@@ -147,21 +147,25 @@ export const AddProposalComponent = ({
   const renderProposalStep = useMemo(() => {
     switch (proposalCreationStep) {
       case AddProposalSteps.CREATE:
-        return (
-          <AddProposalForm
-            common={common}
-            saveProposalState={saveProposalState}
-            addBankDetails={addBankDetails}
-          />
-        );
       case AddProposalSteps.BANK_DETAILS:
         return (
-          <AddBankDetails
-            onBankDetails={onBankDetails}
-            onBankDetailsAfterError={setInitialBankAccountDetails}
-            title="Add Bank Account"
-            initialBankAccountDetails={initialBankAccountDetails}
-          />
+          <>
+            <AddProposalForm
+              common={common}
+              saveProposalState={saveProposalState}
+              addBankDetails={addBankDetails}
+              hidden={proposalCreationStep === AddProposalSteps.BANK_DETAILS}
+            />
+            {
+              (proposalCreationStep === AddProposalSteps.BANK_DETAILS)
+              && <AddBankDetails
+                onBankDetails={onBankDetails}
+                onBankDetailsAfterError={setInitialBankAccountDetails}
+                title="Add Bank Account"
+                initialBankAccountDetails={initialBankAccountDetails}
+              />
+            }
+          </>
         );
       case AddProposalSteps.CONFIRM:
         return (
