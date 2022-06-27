a7fc452d9fc9542454de5224c1d4e9b2ff7e45b4
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-06-27 15:54:20 +0300

    create general error class

--- src/shared/constants/errorCode.ts
+++ src/shared/constants/errorCode.ts
@@ -1,3 +1,7 @@
 export enum ErrorCode {
+  // Front-end error codes
+  CUserDoesNotExist = "CUserDoesNotExist",
+
+  // Back-end error codes
   SellerRejected = "External.PayMeError.SellerRejected",
 }
--- src/shared/utils/index.tsx
+++ src/shared/utils/index.tsx
@@ -1,3 +1,4 @@
+export * from "./error";
 export * from "./shared";
 export { default as tokenHandler } from "./tokenHandler";
 export * from "./download";
