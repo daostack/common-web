4a2378f5e234a3f4cded63f9e103809c5b3134ff
watchtower314 <moore3.14159@gmail.com>
2022-07-14 18:06:05 +0300

    added bank account button

---
+++ src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/Configuration/Configuration.tsx
@@ -0,0 +1,136 @@
+import React, { FC, useEffect, useMemo, useRef, useState, useCallback } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "@/containers/Auth/store/selectors";
+import {
+  Button,
+  ModalFooter,
+  Separator,
+} from "@/shared/components";
+import { ScreenSize } from "@/shared/constants";
+import DollarIcon from "@/shared/icons/dollar.icon";
+import { Circle, CommonMemberWithUserInfo, Governance } from "@/shared/models";
+import { getScreenSize } from "@/shared/store/selectors";
+import { getUserName } from "@/shared/utils";
+import { generateCirclesBinaryNumber } from "../../../CommonWhitepaper/utils";
+import { StageName } from "../../StageName";
+import { MemberInfo } from "../MemberInfo";
+import { FundsAllocationData } from "../types";
+import { Formik, FormikConfig } from "formik";
+import { FormikProps } from "formik/dist/types";
+import { Form, TextField, LinksArray } from "@/shared/components/Form/Formik";
+import "./index.scss";
+
+interface ConfigurationProps {
+  governance: Governance;
+  //commonMembers: CommonMemberWithUserInfo[];
+  initialData: FundsAllocationData | null;
+  onFinish: (data: FundsAllocationData) => void;
+}
+
+interface FormValues {
+  title: string;
+  description: string;
+  goalOfPayment: string;
+}
+
+const Configuration: FC<ConfigurationProps> = (props) => {
+  const { governance, initialData, onFinish } = props;
+  const isInitialCircleUpdate = useRef(true);
+  const user = useSelector(selectUser());
+  const screenSize = useSelector(getScreenSize());
+  const isMobileView = screenSize === ScreenSize.Mobile;
+  const formRef = useRef<FormikProps<FormValues>>(null);
+
+  const getInitialValues = (): FormValues => ({
+    title: "",
+    description: "",
+    goalOfPayment: "",
+  });
+
+  const handleSubmit = useCallback<FormikConfig<FormValues>["onSubmit"]>(
+    (values) => {
+      onFinish({
+        title: values.title,
+        description: values.description,
+        goalOfPayment: values.goalOfPayment
+      });
+    },
+    [onFinish]
+  );
+
+  const handleContinueClick = useCallback(() => {
+    if (formRef.current) {
+      formRef.current.submitForm();
+      console.log('formRef.current', formRef.current.values)
+      onFinish(formRef.current.values)
+    }
+  }, []);
+
+  return (
+    <div className="funds-allocation-configuration">
+      <StageName
+        className="funds-allocation-configuration__stage-name"
+        name="Funds Allocation"
+        backgroundColor="light-yellow"
+        icon={
+          <DollarIcon className="funds-allocation-configuration__avatar-icon" />
+        }
+      />
+      <Separator className="funds-allocation-configuration__separator" />
+      <div className="funds-allocation-configuration__form">
+        <Formik
+          initialValues={getInitialValues()}
+          onSubmit={handleSubmit}
+          innerRef={formRef}
+          //validationSchema={validationSchema}
+          validateOnMount
+        >
+          {({ values, errors, touched, isValid }) => (
+            <Form>
+              <TextField
+                className="create-funds-allocation__text-field"
+                id="title"
+                name="title"
+                label="Title"
+                placeholder="Briefly describe your proposal"
+                maxLength={60}
+                isRequired
+              />
+              <TextField
+                className="create-funds-allocation__text-field"
+                id="description"
+                name="description"
+                label="Description"
+                placeholder="What exactly do you plan to do and how? How does it align with the Common's agenda and goals"
+                isRequired
+              />
+              <TextField
+                className="create-funds-allocation__text-field"
+                id="goalOfPayment"
+                name="goalOfPayment"
+                label="Goal of Payment"
+                placeholder="What exactly do you plan to do with the funding?"
+                rows={isMobileView ? 4 : 3}
+                isTextarea
+                isRequired
+              />
+              <ModalFooter sticky={!isMobileView}>
+                <div className="funds-allocation-configuration__modal-footer">
+                  <Button
+                    onClick={handleContinueClick}
+                    shouldUseFullWidth={isMobileView}
+                    disabled={!isValid}
+                  >
+                    Continue
+                  </Button>
+                </div>
+              </ModalFooter>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default Configuration;
---
+++ src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/FundsAllocationStage.tsx
@@ -0,0 +1,206 @@
+import React, { FC, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "@/containers/Auth/store/selectors";
+import { useCommonMembers } from "@/containers/Common/hooks";
+import { CreateProposal } from "@/containers/Common/interfaces";
+import { createFundingProposal } from "@/containers/Common/store/actions";
+import { Loader, Modal } from "@/shared/components";
+import { ProposalsTypes, ScreenSize } from "@/shared/constants";
+import { ModalType } from "@/shared/interfaces";
+import { Common, Governance } from "@/shared/models";
+import { getScreenSize } from "@/shared/store/selectors";
+import { getUserName } from "@/shared/utils";
+import { useCreateProposalContext } from "../context";
+import { Configuration } from "./Configuration";
+import { Confirmation } from "./Confirmation";
+import { FundDetails } from "./FundDetails";
+import { Success } from "./Success";
+import { FundsAllocationStep } from "./constants";
+import { FundsAllocationData } from "./types";
+import "./index.scss";
+
+interface FundsAllocationStageProps {
+  common: Common;
+  governance: Governance;
+  onFinish: (shouldViewProposal?: boolean) => void;
+  onGoBack: () => void;
+}
+
+const FundsAllocationStage: FC<FundsAllocationStageProps> = (props) => {
+  const { common, governance, onFinish, onGoBack } = props;
+  const dispatch = useDispatch();
+  const [fundsAllocationData, setfundsAllocationData] =
+    useState<FundsAllocationData | null>(null);
+  const [step, setStep] = useState(FundsAllocationStep.Configuration);
+  const [isProposalCreating, setIsProposalCreating] = useState(false);
+  const {
+    setTitle,
+    setOnGoBack,
+    setShouldShowClosePrompt,
+    setShouldBeOnFullHeight,
+  } = useCreateProposalContext();
+  const {
+    fetched: areCommonMembersFetched,
+    data: commonMembers,
+    fetchCommonMembers,
+  } = useCommonMembers();
+  const user = useSelector(selectUser());
+  const screenSize = useSelector(getScreenSize());
+  const isMobileView = screenSize === ScreenSize.Mobile;
+  const isConfigurationStep = step === FundsAllocationStep.Configuration;
+  const isSuccessStep = step === FundsAllocationStep.Success;
+  const shouldShowModalTitle = isMobileView || isConfigurationStep;
+  const isLoading = !areCommonMembersFetched || isProposalCreating;
+
+  const handleConfigurationFinish = (data: FundsAllocationData) => {
+    setfundsAllocationData(data);
+    setStep(FundsAllocationStep.Funds);
+    console.log('set step to confirm')
+  };
+
+  const handleConfirm = () => {
+    if (!fundsAllocationData || !user) {
+      return;
+    }
+
+    setIsProposalCreating(true);
+    const payload: Omit<
+      CreateProposal[ProposalsTypes.FUNDS_ALLOCATION]["data"],
+      "type"
+    > = {
+      args: {
+        //commonId: common.id,
+        // TODO: Use here name of common member
+        title: fundsAllocationData.title,
+        description: fundsAllocationData.description, //`Request from ${getUserName(user)}`,
+        //proposerId: user.id,
+        //goalOfPayment: fundsAllocationData.goalOfPayment,
+        //images: [],
+        //links: [],
+        //files: [],
+        //circleId: '',
+        //userId: '' //fundsAllocationData.commonMember.userId
+      },
+    };
+
+    dispatch(
+      createFundingProposal.request({
+        payload,
+        callback: (error, data) => {
+          if (!error && data) {
+            setStep(FundsAllocationStep.Success);
+            setIsProposalCreating(false);
+          }
+        },
+      })
+    );
+  };
+
+  const handleConfirmationCancel = () => {
+    setStep(FundsAllocationStep.Configuration);
+  };
+
+  const handleBackToCommon = () => {
+    onFinish(false);
+  };
+
+  const handleViewProposal = () => {
+    onFinish(true);
+  };
+
+  useEffect(() => {
+    fetchCommonMembers(governance.commonId);
+  }, [fetchCommonMembers, governance.commonId]);
+
+  useEffect(() => {
+    setTitle(shouldShowModalTitle ? "Create New Proposal" : "");
+  }, [setTitle, shouldShowModalTitle]);
+
+  useEffect(() => {
+    setOnGoBack(
+      shouldShowModalTitle && !isProposalCreating ? onGoBack : undefined
+    );
+  }, [setOnGoBack, onGoBack, shouldShowModalTitle, isProposalCreating]);
+
+  useEffect(() => {
+    setShouldShowClosePrompt(!isSuccessStep);
+  }, [setShouldShowClosePrompt, isSuccessStep]);
+
+  useEffect(() => {
+    setShouldBeOnFullHeight(isConfigurationStep || isLoading);
+  }, [setShouldBeOnFullHeight, isConfigurationStep, isLoading]);
+
+  const renderConfirmationStep = () => <></>
+    /*fundsAllocationData && (
+      <Confirmation
+        circle={fundsAllocationData.circle}
+        commonMember={fundsAllocationData.commonMember}
+        onSubmit={handleConfirm}
+        onCancel={handleConfirmationCancel}
+      />
+    );*/
+
+
+
+  const renderSuccessStep = () => (
+    <Success
+      onBackToCommon={handleBackToCommon}
+      onViewProposal={handleViewProposal}
+    />
+  );
+
+  return (
+    <div className="funds-allocation-creation-stage">
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <>
+          {(isConfigurationStep || isMobileView) && (
+            <Configuration
+              governance={governance}
+              //commonMembers={commonMembers}
+              initialData={fundsAllocationData}
+              onFinish={handleConfigurationFinish}
+            />
+          )}
+
+          {(step === FundsAllocationStep.Funds || isMobileView) &&
+              <FundDetails
+              governance={governance}
+              //commonMembers={commonMembers}
+              initialData={fundsAllocationData}
+              onFinish={handleConfigurationFinish}
+            />
+           }
+          {step === FundsAllocationStep.Confirmation &&
+            (isMobileView ? (
+              <Modal
+                isShowing
+                onClose={handleConfirmationCancel}
+                type={ModalType.MobilePopUp}
+                hideCloseButton
+              >
+                {renderConfirmationStep()}
+              </Modal>
+            ) : (
+              renderConfirmationStep()
+            ))}
+          {isSuccessStep &&
+            (isMobileView ? (
+              <Modal
+                isShowing
+                onClose={handleBackToCommon}
+                type={ModalType.MobilePopUp}
+                hideCloseButton
+              >
+                {renderSuccessStep()}
+              </Modal>
+            ) : (
+              renderSuccessStep()
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FundsAllocationStage;
