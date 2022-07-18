bf188a9d2e57e8be4940227c6bd3f456f80b7820
watchtower314 <moore3.14159@gmail.com>
2022-07-18 18:40:51 +0300

    fix mismatch classnames

--- src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/Success/Success.tsx
+++ src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/Success/Success.tsx
@@ -16,20 +16,20 @@ const Success: FC<SuccessProps> = (props) => {
   const isMobileView = screenSize === ScreenSize.Mobile;
 
   return (
-    <div className="assign-circle-success">
+    <div className="funds-allocation-success">
       <img
-        className="assign-circle-success__image"
+        className="funds-allocation-success__image"
         src="/assets/images/illustrations-full-page-launch.svg"
         alt="Avatar"
       />
-      <h4 className="assign-circle-success__title">Your proposal is live!</h4>
-      <p className="assign-circle-success__description">
+      <h4 className="funds-allocation-success__title">Your proposal is live!</h4>
+      <p className="funds-allocation-success__description">
         The Common members will now discuss the proposal and vote to accept or
         reject it. You will be notified when the voting ends.
       </p>
-      <div className="assign-circle-success__buttons-wrapper">
+      <div className="funds-allocation-success__buttons-wrapper">
         <Button
-          className="assign-circle-success__back-button"
+          className="funds-allocation-success__back-button"
           onClick={onBackToCommon}
           variant={
             isMobileView
