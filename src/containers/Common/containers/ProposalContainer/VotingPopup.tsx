13732467acbbb69c3e1d130c202300865fadb242
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-07-25 21:17:20 +0200

    remove votes apply waiting from proposal page

--- src/containers/Common/containers/ProposalContainer/VotingPopup.tsx
+++ src/containers/Common/containers/ProposalContainer/VotingPopup.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState, useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { Proposal, Vote, VoteOutcome, } from "@/shared/models";
-import { Modal, Loader } from "@/shared/components";
+import { Proposal, Vote, VoteOutcome } from "@/shared/models";
+import { Modal, Loader, Button } from "@/shared/components";
 import { createVote } from "@/containers/Common/store/actions";
 
 interface VotingPopupProps {
@@ -11,50 +11,111 @@ interface VotingPopupProps {
   onClose: () => void;
 }
 
-export const VotingPopup: FC<VotingPopupProps> = (
-  {
-    proposal,
-    setVote,
-    isShowing,
-    onClose,
-  }
-) => {
+export const VotingPopup: FC<VotingPopupProps> = ({
+  proposal,
+  setVote,
+  isShowing,
+  onClose,
+}) => {
   const dispatch = useDispatch();
   const [voting, setVoting] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
-  const handleCreateVote = useCallback((voteOutcome: VoteOutcome) => {
-    setVoting(true);
+  const handleCreateVote = useCallback(
+    (voteOutcome: VoteOutcome) => {
+      setVoting(true);
 
-    dispatch(createVote.request({
-      payload: {
-        proposalVotes: proposal.votes,
-        votePayload: {
-          proposalId: proposal.id,
-          outcome: voteOutcome,
-        },
-      },
-      callback: (error, vote) => {
-        setVoting(false);
+      dispatch(
+        createVote.request({
+          payload: {
+            proposalVotes: proposal.votes,
+            votePayload: {
+              proposalId: proposal.id,
+              outcome: voteOutcome,
+            },
+            shouldWaitForVoteToBeApplied: false,
+          },
+          callback: (error, vote) => {
+            setVoting(false);
 
-        if (error || !vote) {
-          console.error(error);
-          return;
-        }
+            if (error || !vote) {
+              console.error(error);
+              return;
+            }
 
-        setVote(vote);
-
-        onClose();
-      }
-    }))
-  }, [
-      dispatch,
-      onClose,
-      proposal.id,
-      proposal.votes,
-      setVote,
-      setVoting
-    ]
+            setVote(vote);
+            setIsSuccess(true);
+          },
+        })
+      );
+    },
+    [dispatch, onClose, proposal.id, proposal.votes, setVote, setVoting]
   );
+
+  const renderContent = () => {
+    if (isSuccess) {
+      return (
+        <div className="voting-popup__success-details-wrapper">
+          <img
+            className="voting-popup__approved-illustration"
+            src="/icons/preview-approved.svg"
+            alt="popup-pic"
+          />
+          <p>Your vote was submitted! It will take some time to apply.</p>
+          <Button
+            className="voting-popup__success-button"
+            onClick={onClose}
+            shouldUseFullWidth
+          >
+            OK
+          </Button>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <img
+          src="/icons/votes/popup-illustration.svg"
+          alt="popup-pic"
+          className="voting-popup__illustration"
+        />
+        <div className="voting-popup__title">Please vote</div>
+        {voting ? (
+          <div className="voting-popup__loader-wrapper">
+            <Loader />
+          </div>
+        ) : (
+          <ul className="voting-popup__vote-options-container">
+            <li
+              key="approve"
+              className="voting-popup__vote-options-item"
+              onClick={() => handleCreateVote(VoteOutcome.Approved)}
+            >
+              <img src="/icons/votes/approved-old.svg" alt="vote type symbol" />
+            </li>
+            <li
+              key="abstain"
+              className="voting-popup__vote-options-item"
+              onClick={() => handleCreateVote(VoteOutcome.Abstained)}
+            >
+              <img
+                src="/icons/votes/abstained-old.svg"
+                alt="vote type symbol"
+              />
+            </li>
+            <li
+              key="reject"
+              className="voting-popup__vote-options-item"
+              onClick={() => handleCreateVote(VoteOutcome.Rejected)}
+            >
+              <img src="/icons/votes/rejected-old.svg" alt="vote type symbol" />
+            </li>
+          </ul>
+        )}
+      </>
+    );
+  };
 
   return (
     <Modal
@@ -62,54 +123,7 @@ export const VotingPopup: FC<VotingPopupProps> = (
       onClose={voting ? () => {} : onClose}
       className="voting-popup__modal-wrapper"
     >
-      <div className="voting-popup">
-        <img
-          src="/icons/votes/popup-illustration.svg"
-          alt="popup-pic"
-          className="voting-popup__illustration"
-        />
-        <div className="voting-popup__title">
-          Please vote
-        </div>
-        {
-          voting
-            ? <div className="voting-popup__loader-wrapper">
-              <Loader />
-            </div>
-            : <ul className="voting-popup__vote-options-container">
-              <li
-                key="approve"
-                className="voting-popup__vote-options-item"
-                onClick={() => handleCreateVote(VoteOutcome.Approved)}
-              >
-                <img
-                  src="/icons/votes/approved-old.svg"
-                  alt="vote type symbol"
-                />
-              </li>
-              <li
-                key="abstain"
-                className="voting-popup__vote-options-item"
-                onClick={() => handleCreateVote(VoteOutcome.Abstained)}
-              >
-                <img
-                  src="/icons/votes/abstained-old.svg"
-                  alt="vote type symbol"
-                />
-              </li>
-              <li
-                key="reject"
-                className="voting-popup__vote-options-item"
-                onClick={() => handleCreateVote(VoteOutcome.Rejected)}
-              >
-                <img
-                  src="/icons/votes/rejected-old.svg"
-                  alt="vote type symbol"
-                />
-              </li>
-            </ul>
-        }
-      </div>
+      <div className="voting-popup">{renderContent()}</div>
     </Modal>
-  )
+  );
 };
