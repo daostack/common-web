96bf6c1ad0adcc00a2a4758029f8ee4229b0fffb
a <a@me.localdomain>
2022-07-27 13:18:21 +0300

    use prefix util

--- src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/FundDetails/validationSchema.ts
+++ src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/FundDetails/validationSchema.ts
@@ -1,8 +1,9 @@
 import * as yup from 'yup';
+import { formatPrice } from '../../../../../../../shared/utils';
 
 export const validationSchema = yup.object({
     commonBalance: yup.number(),
     amount: yup.number().transform(value => (isNaN(value) ? undefined : value)).max(yup.ref('commonBalance'),
-        ({ max }) => `The amount requested cannot be greater than the Common balance (₪${max}).`),
+        ({ max }) => `The amount requested cannot be greater than the Common balance (${formatPrice(max, { shouldRemovePrefixFromZero: false })}).`),
     bankAccountDetails: yup.object().required(),
 });
