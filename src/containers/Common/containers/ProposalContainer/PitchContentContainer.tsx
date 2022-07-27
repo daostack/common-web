9e9fb64f7c99613289ad06d462d23bd82899789a
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-07-27 10:20:35 +0200

    display description of proposal with multiple amount of lines

--- src/containers/Common/containers/ProposalContainer/PitchContentContainer.tsx
+++ src/containers/Common/containers/ProposalContainer/PitchContentContainer.tsx
@@ -9,37 +9,45 @@ interface PitchContentContainerProps {
 
 export const PitchContentContainer: FC<PitchContentContainerProps> = ({
   proposal,
-}) => (
-  <div className="pitch-content__wrapper">
-    <p className="pitch-content__description">
-      {proposal.data.args.description}
-    </p>
-    {proposal.data.args.links.length > 0 && (
-      <ul className="pitch-content__links">
-        {proposal.data.args.links.map((link) => (
-          <li key={link.title} className="pitch-content__links-item">
-            <ButtonLink
-              className="pitch-content__links-item-button"
-              href={link.value}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkIcon className="pitch-content__links-item-icon" />
-              {link.title}
-            </ButtonLink>
-          </li>
+}) => {
+  const descriptionParts = proposal.data.args.description.split("\n");
+
+  return (
+    <div className="pitch-content__wrapper">
+      <div className="pitch-content__description-wrapper">
+        {descriptionParts.map((text, index) => (
+          <p className="pitch-content__description" key={index}>
+            {text}
+          </p>
         ))}
-      </ul>
-    )}
-    {proposal.data.args.images.length > 0 && (
-      <ul className="pitch-content__images">
-        {proposal.data.args.images.map((image) => (
-          <li key={image.title} className="pitch-content__images-item">
-            <img src={image.value} alt={image.title} />
-            <span className="title">{image.title}</span>
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
-);
+      </div>
+      {proposal.data.args.links.length > 0 && (
+        <ul className="pitch-content__links">
+          {proposal.data.args.links.map((link) => (
+            <li key={link.title} className="pitch-content__links-item">
+              <ButtonLink
+                className="pitch-content__links-item-button"
+                href={link.value}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkIcon className="pitch-content__links-item-icon" />
+                {link.title}
+              </ButtonLink>
+            </li>
+          ))}
+        </ul>
+      )}
+      {proposal.data.args.images.length > 0 && (
+        <ul className="pitch-content__images">
+          {proposal.data.args.images.map((image) => (
+            <li key={image.title} className="pitch-content__images-item">
+              <img src={image.value} alt={image.title} />
+              <span className="title">{image.title}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
