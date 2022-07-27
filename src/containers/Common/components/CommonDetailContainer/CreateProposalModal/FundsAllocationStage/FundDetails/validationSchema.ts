de109790661b9c522aabb7bfa1018a51d7ec2448
a <a@me.localdomain>
2022-07-27 12:33:46 +0300

    added the value to error

--- src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/FundDetails/validationSchema.ts
+++ src/containers/Common/components/CommonDetailContainer/CreateProposalModal/FundsAllocationStage/FundDetails/validationSchema.ts
@@ -2,6 +2,7 @@ import * as yup from 'yup';
 
 export const validationSchema = yup.object({
     commonBalance: yup.number(),
-    amount: yup.number().transform(value => (isNaN(value) ? undefined : value)).max(yup.ref('commonBalance'), 'The amount requested cannot be greater than the Common balance.'),
-    bankAccountDetails: yup.object().required(), 
+    amount: yup.number().transform(value => (isNaN(value) ? undefined : value)).max(yup.ref('commonBalance'),
+        ({ max }) => `The amount requested cannot be greater than the Common balance (₪${max}).`),
+    bankAccountDetails: yup.object().required(),
 });
