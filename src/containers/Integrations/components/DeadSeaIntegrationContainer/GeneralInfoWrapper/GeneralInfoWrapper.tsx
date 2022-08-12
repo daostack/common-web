1d3a0911542dadc56da5aff9edbb26819c539f88
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-08-12 14:46:15 +0200

    add description to general info

--- src/containers/Integrations/components/DeadSeaIntegrationContainer/GeneralInfoWrapper/GeneralInfoWrapper.tsx
+++ src/containers/Integrations/components/DeadSeaIntegrationContainer/GeneralInfoWrapper/GeneralInfoWrapper.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from "react";
+import classNames from "classnames";
 import { ButtonLink } from "@/shared/components";
 import LongLeftArrowIcon from "@/shared/icons/longLeftArrow.icon";
 import "./index.scss";
 
 interface GeneralInfoWrapperProps {
+  description?: string;
   onGoBack?: (() => void) | null;
 }
 
 const GeneralInfoWrapper: FC<GeneralInfoWrapperProps> = (props) => {
-  const { onGoBack, children } = props;
+  const { description, onGoBack, children } = props;
 
   return (
     <div className="dead-sea-general-info-wrapper">
@@ -23,9 +25,22 @@ const GeneralInfoWrapper: FC<GeneralInfoWrapperProps> = (props) => {
       ) : (
         <span className="dead-sea-general-info-wrapper__action">Join the</span>
       )}
-      <h1 className="dead-sea-general-info-wrapper__title">
+      <h1
+        className={classNames("dead-sea-general-info-wrapper__title", {
+          "dead-sea-general-info-wrapper__bottom-margin": !description,
+        })}
+      >
         Dead Sea Guardians
       </h1>
+      {description && (
+        <p
+          className={
+            "dead-sea-general-info-wrapper__description dead-sea-general-info-wrapper__bottom-margin"
+          }
+        >
+          {description}
+        </p>
+      )}
       {children}
     </div>
   );
--- src/containers/Integrations/components/DeadSeaIntegrationContainer/InitialStep/InitialStep.tsx
+++ src/containers/Integrations/components/DeadSeaIntegrationContainer/InitialStep/InitialStep.tsx
@@ -7,11 +7,14 @@ interface InitialStepProps {
   onFinish: (amount: number) => void;
 }
 
+const DESCRIPTION =
+  "The Dead Sea is one of nature’s wonders and the lowest place on earth. Due to diversion of water and over exploitation of minerals the Dead Sea is declining 1.2 meters every year and has already lost over 30% of its surface area. If we don't act now, what will be left of the Sea, in our lifetime, will be just a puddle.";
+
 const InitialStep: FC<InitialStepProps> = (props) => {
   const { amount, onFinish } = props;
 
   return (
-    <GeneralInfoWrapper>
+    <GeneralInfoWrapper description={DESCRIPTION}>
       <AmountSelection amount={amount} onAmountChange={onFinish} />
     </GeneralInfoWrapper>
   );
