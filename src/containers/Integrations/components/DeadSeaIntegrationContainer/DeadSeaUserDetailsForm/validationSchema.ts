b446f7ebd848b9c776752217518e91d2b01c4c9c
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-08-15 13:50:33 +0200

    remove name regexp check in the dead sea form validation

--- src/containers/Integrations/components/DeadSeaIntegrationContainer/DeadSeaUserDetailsForm/validationSchema.ts
+++ src/containers/Integrations/components/DeadSeaIntegrationContainer/DeadSeaUserDetailsForm/validationSchema.ts
@@ -1,16 +1,11 @@
 import * as Yup from "yup";
 import { countryList } from "@/shared/assets/countries";
-import { NAME_REGEXP } from "@/shared/constants";
 
 const AVAILABLE_COUNTRIES = countryList.map((country) => country.value);
 
 const schema = Yup.object().shape({
-  firstName: Yup.string()
-    .matches(NAME_REGEXP, "Only alphabets are allowed for this field")
-    .required("Please enter your first name"),
-  lastName: Yup.string()
-    .matches(NAME_REGEXP, "Only alphabets are allowed for this field")
-    .required("Please enter your last name"),
+  firstName: Yup.string().required("Please enter your first name"),
+  lastName: Yup.string().required("Please enter your last name"),
   email: Yup.string()
     .email("Please enter a valid email address")
     .required("Please enter your email"),
