8b0815ac20dd3457451ed3ca30d60a5fe3695203
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-08-16 17:36:17 +0200

    hide some more proposal types from circles tab

--- src/containers/Common/components/CommonDetailContainer/CommonWhitepaper/components/WhitepaperMembers/helpers.ts
+++ src/containers/Common/components/CommonDetailContainer/CommonWhitepaper/components/WhitepaperMembers/helpers.ts
@@ -28,6 +28,12 @@ export const checkShouldRemoveAction = (
     GovernanceActions.RECEIVE_REPORT_DISCUSSION_NOTIFICATION,
     GovernanceActions.RECEIVE_REPORT_MESSAGE_NOTIFICATION,
     GovernanceActions.RECEIVE_REPORT_PROPOSAL_NOTIFICATION,
+    GovernanceActions.REPORT_DISCUSSION,
+    GovernanceActions.REPORT_PROPOSAL,
+    GovernanceActions.HIDE_OR_UNHIDE_DISCUSSION,
+    GovernanceActions.HIDE_OR_UNHIDE_PROPOSAL,
+    GovernanceActions.DELETE_DISCUSSION,
+    GovernanceActions.DELETE_MESSAGE,
   ].includes(action);
 
 export const getTextForAction = (action: keyof AllowedActions): string => {
@@ -38,22 +44,10 @@ export const getTextForAction = (action: keyof AllowedActions): string => {
       return "Create a discussion";
     case GovernanceActions.CREATE_MESSAGE:
       return "Write a message";
-    case GovernanceActions.REPORT_DISCUSSION:
-      return "Report a discussion";
     case GovernanceActions.REPORT_MESSAGE:
       return "Report a message";
-    case GovernanceActions.REPORT_PROPOSAL:
-      return "Report a proposal";
-    case GovernanceActions.HIDE_OR_UNHIDE_DISCUSSION:
-      return "Hide a discussion";
     case GovernanceActions.HIDE_OR_UNHIDE_MESSAGE:
       return "Hide a message";
-    case GovernanceActions.HIDE_OR_UNHIDE_PROPOSAL:
-      return "Hide a proposal";
-    case GovernanceActions.DELETE_DISCUSSION:
-      return "Delete a discussion";
-    case GovernanceActions.DELETE_MESSAGE:
-      return "Delete a message";
     case GovernanceActions.RECEIVE_FUNDS:
       return "Receive funds";
     default:
