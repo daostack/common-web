04fc7b8e5c96f637a0d156a7f8f5c71d3459cc48
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-08-22 15:40:24 +0200

    create separate component for main info from general info step

--- src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/GeneralInfo.tsx
+++ src/containers/Common/components/CommonListContainer/CreateCommonModal/CreationSteps/GeneralInfo/GeneralInfo.tsx
@@ -1,21 +1,13 @@
-import React, { useCallback, useRef, ReactElement } from "react";
+import React, { ReactElement } from "react";
 import { useSelector } from "react-redux";
-import { Formik, FormikConfig } from "formik";
-import { FormikProps } from "formik/dist/types";
-import { Button, Separator } from "@/shared/components";
-import { ModalFooter, ModalHeaderContent } from "@/shared/components/Modal";
-import { Form, TextField, LinksArray } from "@/shared/components/Form/Formik";
-import { ScreenSize, MAX_LINK_TITLE_LENGTH } from "@/shared/constants";
-import { CommonLink } from "@/shared/models";
+import { Separator } from "@/shared/components";
+import { ModalHeaderContent } from "@/shared/components/Modal";
+import { ScreenSize } from "@/shared/constants";
 import { getScreenSize } from "@/shared/store/selectors";
 import { IntermediateCreateCommonPayload } from "../../../../../interfaces";
 import { Progress } from "../Progress";
-import {
-  MAX_COMMON_NAME_LENGTH,
-  MAX_TAGLINE_LENGTH,
-  MAX_ABOUT_LENGTH,
-} from "./constants";
-import validationSchema from "./validationSchema";
+import { MainInfo } from "./MainInfo";
+import { MainInfoValues } from "./types";
 import "./index.scss";
 
 interface GeneralInfoProps {
@@ -25,47 +17,21 @@ interface GeneralInfoProps {
   creationData: IntermediateCreateCommonPayload;
 }
 
-interface FormValues {
-  commonName: string;
-  tagline: string;
-  about: string;
-  links: CommonLink[];
-}
-
-const getInitialValues = (
-  data: IntermediateCreateCommonPayload
-): FormValues => ({
-  commonName: data.name,
-  tagline: data.byline || "",
-  about: data.description || "",
-  links: data.links?.length ? data.links : [{ title: "", value: "" }],
-});
-
 export default function GeneralInfo(props: GeneralInfoProps): ReactElement {
   const { currentStep, isSubCommonCreation, onFinish, creationData } = props;
-  const formRef = useRef<FormikProps<FormValues>>(null);
   const screenSize = useSelector(getScreenSize());
   const isMobileView = screenSize === ScreenSize.Mobile;
 
-  const handleContinueClick = useCallback(() => {
-    if (formRef.current) {
-      formRef.current.submitForm();
-    }
-  }, []);
+  const handleMainInfoSubmit = (values: MainInfoValues) => {
+    const links = values.links.filter((link) => link.title && link.value);
 
-  const handleSubmit = useCallback<FormikConfig<FormValues>["onSubmit"]>(
-    (values) => {
-      const links = values.links.filter((link) => link.title && link.value);
-
-      onFinish({
-        name: values.commonName,
-        byline: values.tagline,
-        description: values.about,
-        links,
-      });
-    },
-    [onFinish]
-  );
+    onFinish({
+      name: values.commonName,
+      byline: values.tagline,
+      description: values.about,
+      links,
+    });
+  };
 
   const progressEl = (
     <Progress
@@ -80,66 +46,7 @@ export default function GeneralInfo(props: GeneralInfoProps): ReactElement {
       <div className="create-common-general-info">
         {isMobileView && progressEl}
         <Separator className="create-common-general-info__separator" />
-        <Formik
-          initialValues={getInitialValues(creationData)}
-          onSubmit={handleSubmit}
-          innerRef={formRef}
-          validationSchema={validationSchema}
-          validateOnMount
-        >
-          {({ values, errors, touched, isValid }) => (
-            <Form>
-              <TextField
-                className="create-common-general-info__text-field"
-                id="commonName"
-                name="commonName"
-                label="Common name"
-                placeholder="Save the Amazon"
-                maxLength={MAX_COMMON_NAME_LENGTH}
-                isRequired
-              />
-              <TextField
-                className="create-common-general-info__text-field"
-                id="tagline"
-                name="tagline"
-                label="Tagline"
-                placeholder="What is the ultimate goal of the Common?"
-                maxLength={MAX_TAGLINE_LENGTH}
-              />
-              <TextField
-                className="create-common-general-info__text-field"
-                id="about"
-                name="about"
-                label="About"
-                placeholder="Describe your cause and let others know why they should join you. What makes you passionate about it? What does success look like?"
-                maxLength={MAX_ABOUT_LENGTH}
-                rows={isMobileView ? 4 : 3}
-                isTextarea
-                isRequired
-              />
-              <LinksArray
-                name="links"
-                values={values.links}
-                errors={errors.links}
-                touched={touched.links}
-                maxTitleLength={MAX_LINK_TITLE_LENGTH}
-                className="create-common-general-info__text-field"
-                itemClassName="create-common-general-info__links-array-item"
-              />
-              <ModalFooter sticky={!isMobileView}>
-                <div className="create-common-general-info__modal-footer">
-                  <Button
-                    onClick={handleContinueClick}
-                    shouldUseFullWidth={isMobileView}
-                    disabled={!isValid}
-                  >
-                    Continue to Acknowledgment
-                  </Button>
-                </div>
-              </ModalFooter>
-            </Form>
-          )}
-        </Formik>
+        <MainInfo onFinish={handleMainInfoSubmit} creationData={creationData} />
       </div>
     </>
   );
