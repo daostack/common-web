02d7fba4f4d6b9b82432478cd5040170ec1b7085
Pavel Meyer <meyerpavel1207@gmail.com>
2022-09-13 12:00:12 +0300

    CM-820 Refactoring

--- src/shared/utils/circles.ts
+++ src/shared/utils/circles.ts
@@ -1,9 +1,9 @@
 import { Circle } from "../models";
 
-export const getCirclesNames = (circles: Circle[] | null, circleVisibility?: string[]): string => {
-  if(!circles || !circleVisibility) {
+export const getCirclesNames = (circles: Circle[] | null, circleIds?: string[]): string => {
+  if(!circles || !circleIds) {
     return '';
   }
   
-  return (circles ?? [])?.filter(({id}) => circleVisibility?.includes(id)).map(({name}) => name).join(', ')
+  return (circles ?? [])?.filter(({id}) => circleIds?.includes(id)).map(({name}) => name).join(', ')
 };
