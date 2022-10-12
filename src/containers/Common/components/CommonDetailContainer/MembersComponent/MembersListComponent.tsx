0a3ebd3702cc006290c47fb39a9a051ff102cc20
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-12 18:57:54 +0300

    use util for member name

--- src/containers/Common/components/CommonDetailContainer/MembersComponent/MembersListComponent.tsx
+++ src/containers/Common/components/CommonDetailContainer/MembersComponent/MembersListComponent.tsx
@@ -1,43 +1,45 @@
 import React, { FC, memo } from "react";
 import { useSelector } from "react-redux";
 import { CommonMemberWithUserInfo } from "@/shared/models";
-import { getCirclesWithHighestTier } from "@/shared/utils";
+import { getCirclesWithHighestTier, getUserName } from "@/shared/utils";
 import { selectGovernance } from "../../../store/selectors";
 import CommonMember from "./CommonMemberComponent";
 
 interface MembersListComponentProps {
-    members: CommonMemberWithUserInfo[];
-    commonId: string;
+  members: CommonMemberWithUserInfo[];
+  commonId: string;
 }
 
 const MembersList: FC<MembersListComponentProps> = ({ members, commonId }) => {
-    const governance = useSelector(selectGovernance());
-    const governanceCircles = Object.values(governance?.circles || {});
+  const governance = useSelector(selectGovernance());
+  const governanceCircles = Object.values(governance?.circles || {});
 
-    return <ul className="members__section__members-list">
-        {members.map(member => {
-            const memberCircleIds = Object.values(member.circles.map);
-            const memberCircles = governanceCircles.filter((circle) =>
-              memberCircleIds.includes(circle.id)
-            );
-            const circlesString = getCirclesWithHighestTier(memberCircles)
-              .map((circle) => circle.name)
-              .join(", ");
-            const memberName = `${member.user.firstName} ${member.user.lastName}`;
+  return (
+    <ul className="members__section__members-list">
+      {members.map((member) => {
+        const memberCircleIds = Object.values(member.circles.map);
+        const memberCircles = governanceCircles.filter((circle) =>
+          memberCircleIds.includes(circle.id)
+        );
+        const circlesString = getCirclesWithHighestTier(memberCircles)
+          .map((circle) => circle.name)
+          .join(", ");
+        const memberName = getUserName(member.user);
 
-            return (
-              <CommonMember
-                key={member.id}
-                circles={circlesString}
-                memberName={memberName}
-                avatar={member.user.photoURL}
-                joinedAt={member.joinedAt}
-                member={member}
-                commonId={commonId}
-              />
-            );
-        })}
+        return (
+          <CommonMember
+            key={member.id}
+            circles={circlesString}
+            memberName={memberName}
+            avatar={member.user.photoURL}
+            joinedAt={member.joinedAt}
+            member={member}
+            commonId={commonId}
+          />
+        );
+      })}
     </ul>
-}
+  );
+};
 
 export default memo(MembersList);
