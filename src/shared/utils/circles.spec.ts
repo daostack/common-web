9a623cc63e302453e60951c30eddf30a4bfb0f18
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-13 19:19:32 +0300

    fix util for getting circles with hier tier

--- src/shared/utils/circles.spec.ts
+++ src/shared/utils/circles.spec.ts
@@ -6,36 +6,70 @@ import {
 } from "./circles";
 
 describe("circlesHierarchy", () => {
-  const CIRCLES: Pick<Circle, "id" | "name" | "hierarchy">[] = [
+  const CIRCLES_WITHOUT_EXCLUSION: Pick<Circle, "id" | "name" | "hierarchy">[] = [
+    { id: "circle-0", name: "Circle 0", hierarchy: { tier: 10, exclusions: [] } },
     { id: "circle-1", name: "Circle 1", hierarchy: null },
     { id: "circle-2", name: "Circle 2", hierarchy: { tier: 0, exclusions: [] } },
     { id: "circle-3", name: "Circle 3", hierarchy: null },
     { id: "circle-4", name: "Circle 4", hierarchy: { tier: 20, exclusions: [] } },
     { id: "circle-5", name: "Circle 5", hierarchy: { tier: 10, exclusions: [] } },
+    { id: "circle-6", name: "Circle 6", hierarchy: null },
+    { id: "circle-7", name: "Circle 7", hierarchy: { tier: 0, exclusions: [] } },
+  ];
+  const CIRCLES_WITH_EXCLUSION: Pick<Circle, "id" | "name" | "hierarchy">[] = [
+    { id: "circle-0", name: "Circle 0", hierarchy: { tier: 10, exclusions: [] } },
+    { id: "circle-1", name: "Circle 1", hierarchy: null },
+    { id: "circle-2", name: "Circle 2", hierarchy: { tier: 0, exclusions: [] } },
+    { id: "circle-3", name: "Circle 3", hierarchy: null },
+    { id: "circle-4", name: "Circle 4", hierarchy: { tier: 20, exclusions: [] } },
+    { id: "circle-5", name: "Circle 5", hierarchy: { tier: 10, exclusions: [20] } },
+    { id: "circle-6", name: "Circle 6", hierarchy: null },
+    { id: "circle-7", name: "Circle 7", hierarchy: { tier: 0, exclusions: [] } },
+    { id: "circle-8", name: "Circle 8", hierarchy: { tier: 20, exclusions: [] } },
   ];
 
   describe("getCirclesWithHighestTier", () => {
-    it("should return correct circles", () => {
-      const expectedResult = [CIRCLES[0], CIRCLES[2], CIRCLES[3]];
+    it("should return correct circles when without exclusion", () => {
+      const expectedResult = [
+        CIRCLES_WITHOUT_EXCLUSION[1],
+        CIRCLES_WITHOUT_EXCLUSION[3],
+        CIRCLES_WITHOUT_EXCLUSION[6],
+        CIRCLES_WITHOUT_EXCLUSION[4],
+      ];
 
-      expect(getCirclesWithHighestTier(CIRCLES)).toEqual(expectedResult);
+      expect(getCirclesWithHighestTier(CIRCLES_WITHOUT_EXCLUSION)).toEqual(expectedResult);
+    });
+
+    it("should return correct circles when with exclusions", () => {
+      const expectedResult = [
+        CIRCLES_WITH_EXCLUSION[1],
+        CIRCLES_WITH_EXCLUSION[3],
+        CIRCLES_WITH_EXCLUSION[6],
+        CIRCLES_WITH_EXCLUSION[5],
+        CIRCLES_WITH_EXCLUSION[4],
+        CIRCLES_WITH_EXCLUSION[8],
+      ];
+
+      expect(getCirclesWithHighestTier(CIRCLES_WITH_EXCLUSION)).toEqual(
+        expectedResult
+      );
     });
   });
 
   describe("getCirclesWithLowestTier", () => {
     it("should return correct circles", () => {
-      const expectedResult = [CIRCLES[0], CIRCLES[2], CIRCLES[1]];
+      const expectedResult = [CIRCLES_WITHOUT_EXCLUSION[0], CIRCLES_WITHOUT_EXCLUSION[2], CIRCLES_WITHOUT_EXCLUSION[1]];
 
-      expect(getCirclesWithLowestTier(CIRCLES)).toEqual(expectedResult);
+      expect(getCirclesWithLowestTier(CIRCLES_WITHOUT_EXCLUSION)).toEqual(expectedResult);
     });
   });
 
   describe("addCirclesWithHigherTier", () => {
     it("should return correct circles", () => {
-      const currentCircles = [CIRCLES[2], CIRCLES[4]];
-      const expectedResult = [CIRCLES[2], CIRCLES[3], CIRCLES[4]];
+      const currentCircles = [CIRCLES_WITHOUT_EXCLUSION[2], CIRCLES_WITHOUT_EXCLUSION[4]];
+      const expectedResult = [CIRCLES_WITHOUT_EXCLUSION[2], CIRCLES_WITHOUT_EXCLUSION[3], CIRCLES_WITHOUT_EXCLUSION[4]];
 
-      expect(addCirclesWithHigherTier(currentCircles, CIRCLES)).toEqual(
+      expect(addCirclesWithHigherTier(currentCircles, CIRCLES_WITHOUT_EXCLUSION)).toEqual(
         expectedResult
       );
     });
--- src/shared/utils/circles.ts
+++ src/shared/utils/circles.ts
@@ -1,3 +1,4 @@
+import uniqBy from "lodash/uniqBy";
 import { Circle } from "../models";
 
 export const getFilteredByIdCircles = (
@@ -14,31 +15,88 @@ export const getFilteredByIdCircles = (
   return circles.filter(({ id }) => circleIds.includes(id));
 };
 
-export const getCirclesWithHighestTier = <T extends Pick<Circle, "hierarchy">>(
+const getCirclesPerTier = <T extends Pick<Circle, "id" | "hierarchy">>(
+  circles: T[]
+): Record<number, T[]> =>
+  circles.reduce<Record<number, T[]>>((acc, circle) => {
+    if (!circle.hierarchy) {
+      return acc;
+    }
+
+    const currentItem = acc[circle.hierarchy.tier] || [];
+
+    return {
+      ...acc,
+      [circle.hierarchy.tier]: currentItem.concat(circle),
+    };
+  }, {});
+
+const getWhereCircleAreExcluded = <T extends Pick<Circle, "hierarchy">>(
+  allCircles: T[],
+  circlesToCheck: T[]
+): T[] =>
+  allCircles.filter((circle) =>
+    circlesToCheck.some(
+      (circleToCheck) =>
+        typeof circleToCheck.hierarchy?.tier === "number" &&
+        circle.hierarchy?.exclusions.includes(circleToCheck.hierarchy.tier)
+    )
+  );
+
+const sortCirclesByTierAscending = <T extends Pick<Circle, "hierarchy">>(
+  circles: T[]
+): T[] =>
+  [...circles].sort((prevCircle, nextCircle) => {
+    if (!nextCircle.hierarchy) {
+      return 1;
+    }
+    if (!prevCircle.hierarchy) {
+      return -1;
+    }
+
+    return prevCircle.hierarchy.tier - nextCircle.hierarchy.tier;
+  });
+
+export const getCirclesWithHighestTier = <
+  T extends Pick<Circle, "id" | "hierarchy">
+>(
   circles: T[]
 ): T[] => {
   const finalCircles = circles.filter((circle) => !circle.hierarchy);
   const circlesWithHierarchy = circles.filter((circle) =>
     Boolean(circle.hierarchy)
   );
-  const circleWithHighestTier = circlesWithHierarchy.sort(
-    (prevCircle, nextCircle) => {
-      if (!nextCircle.hierarchy) {
-        return -1;
-      }
-      if (!prevCircle.hierarchy) {
-        return 1;
-      }
 
-      return nextCircle.hierarchy.tier - prevCircle.hierarchy.tier;
-    }
-  )[0];
-
-  if (circleWithHighestTier) {
-    finalCircles.push(circleWithHighestTier);
+  if (circlesWithHierarchy.length === 0) {
+    return finalCircles;
   }
 
-  return finalCircles;
+  const circlesPerTier = getCirclesPerTier(circlesWithHierarchy);
+  const [, highestCircles] =
+    Object.entries(circlesPerTier).sort(([rawPrevTier], [rawNextTier]) => {
+      const prevTier = Number(rawPrevTier);
+      const nextTier = Number(rawNextTier);
+
+      return nextTier - prevTier;
+    })[0] || [];
+
+  if (!highestCircles) {
+    return finalCircles;
+  }
+
+  let circlesToCheck: T[] = highestCircles;
+
+  while (circlesToCheck.length > 0) {
+    finalCircles.push(...circlesToCheck);
+    circlesToCheck = getWhereCircleAreExcluded(
+      circlesWithHierarchy,
+      circlesToCheck
+    );
+  }
+
+  const uniqueCircles = uniqBy(finalCircles, (circle) => circle.id);
+
+  return sortCirclesByTierAscending(uniqueCircles);
 };
 
 export const getCirclesWithLowestTier = <T extends Pick<Circle, "hierarchy">>(
