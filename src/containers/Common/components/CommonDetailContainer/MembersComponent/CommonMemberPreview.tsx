27fd011cd9c32620ceee32028a010ff71287c64b
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-19 15:32:59 +0300

    remove links to commons in common member preview

--- src/containers/Common/components/CommonDetailContainer/MembersComponent/CommonMemberPreview.tsx
+++ src/containers/Common/components/CommonDetailContainer/MembersComponent/CommonMemberPreview.tsx
@@ -1,14 +1,16 @@
 import React, { FC, useState, useEffect, useCallback, useMemo } from "react";
-import { UserAvatar, Modal, Loader} from "@/shared/components";
-import { CommonMemberPreviewInfo, CommonMemberWithUserInfo } from "@/shared/models";
+import { UserAvatar, Modal, Loader } from "@/shared/components";
+import {
+  CommonMemberPreviewInfo,
+  CommonMemberWithUserInfo,
+} from "@/shared/models";
 import { getCommonMemberInfo } from "@/containers/Common/store/api";
 import { getCountryNameFromCode } from "@/shared/assets/countries";
-import { ROUTE_PATHS } from "@/shared/constants";
 import {
   getCirclesWithHighestTier,
   getFilteredByIdCircles,
 } from "@/shared/utils/circles";
-import "./common-member-preview.scss"
+import "./common-member-preview.scss";
 
 interface CommonMemberPreview {
   member: CommonMemberWithUserInfo;
@@ -32,9 +34,11 @@ export const CommonMemberPreview: FC<CommonMemberPreview> = (props) => {
     isShowing = false,
     onClose,
     country,
-    about
+    about,
   } = props;
-  const [previewInfo, setPreviewInfo] = useState<CommonMemberPreviewInfo>({} as CommonMemberPreviewInfo);
+  const [previewInfo, setPreviewInfo] = useState<CommonMemberPreviewInfo>(
+    {} as CommonMemberPreviewInfo
+  );
   const [isLoading, setLoading] = useState(true);
   const commonsInfo = useMemo(() => {
     if (!previewInfo.commons) {
@@ -54,23 +58,25 @@ export const CommonMemberPreview: FC<CommonMemberPreview> = (props) => {
 
       return { ...common, userCircleNames };
     });
-  },[previewInfo]);
+  }, [previewInfo]);
 
   useEffect(() => {
-    if(isShowing && member?.userId) {
+    if (isShowing && member?.userId) {
       (async () => {
-        const commonMemberPreviewInfo = await getCommonMemberInfo(member.userId, commonId);
+        const commonMemberPreviewInfo = await getCommonMemberInfo(
+          member.userId,
+          commonId
+        );
         setPreviewInfo(commonMemberPreviewInfo);
         setLoading(false);
       })();
     }
-  },[isShowing, member])
+  }, [isShowing, member]);
 
   const GeneralUserInfo = useCallback(() => {
-    if(isLoading) {
-      return <Loader />
+    if (isLoading) {
+      return <Loader />;
     }
-
 
     return (
       <>
@@ -82,40 +88,45 @@ export const CommonMemberPreview: FC<CommonMemberPreview> = (props) => {
         )}
         {previewInfo.introToCommon && (
           <>
-            <p className="common-member-preview__section-title">Membership intro</p>
-            <div className="common-member-preview__info">{previewInfo.introToCommon}</div>
+            <p className="common-member-preview__section-title">
+              Membership intro
+            </p>
+            <div className="common-member-preview__info">
+              {previewInfo.introToCommon}
+            </div>
           </>
         )}
-        <p className="common-member-preview__section-title">Member of the following Commons</p>
+        <p className="common-member-preview__section-title">
+          Member of the following Commons
+        </p>
         {commonsInfo.map((item) => (
           <div key={item.id} className="common-member-preview__info">
-            <a
-              href={ROUTE_PATHS.COMMON_DETAIL.replace(':id', item.id)}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="common-member-preview__common-name-link"
-            >
-              {item.name}
-            </a>
-            {" "}
-            -
-            {" "}
-            {item.userCircleNames}
+            {item.name} - {item.userCircleNames}
           </div>
         ))}
       </>
-    )
-  },[isLoading, about, previewInfo])
-
+    );
+  }, [isLoading, about, previewInfo]);
 
   return (
-    <Modal isShowing={isShowing} onClose={onClose} className="common-member-preview">
+    <Modal
+      isShowing={isShowing}
+      onClose={onClose}
+      className="common-member-preview"
+    >
       <div className="common-member-preview__container">
         <div className="common-member-preview__container__header">
-          <UserAvatar photoURL={avatar} className="common-member-preview__container__header__avatar" />
+          <UserAvatar
+            photoURL={avatar}
+            className="common-member-preview__container__header__avatar"
+          />
           <div className="common-member-preview__container__header__user-info">
-            <div className="common-member-preview__container__header__user-info__name">{memberName}</div>
-            <div className="common-member-preview__container__header__user-info__subtitle">{circles}</div>
+            <div className="common-member-preview__container__header__user-info__name">
+              {memberName}
+            </div>
+            <div className="common-member-preview__container__header__user-info__subtitle">
+              {circles}
+            </div>
             {country && (
               <div className="common-member-preview__container__header__user-info__subtitle common-member-preview__container__header__user-info__country">
                 {getCountryNameFromCode(country)}
@@ -126,5 +137,5 @@ export const CommonMemberPreview: FC<CommonMemberPreview> = (props) => {
         <GeneralUserInfo />
       </div>
     </Modal>
-  )
-}
+  );
+};
