b09dc5bab960fb1f0fbbbd8baf0d8a6346412a5f
Pavel Meyer <meyerpavel1207@gmail.com>
2022-10-20 12:19:29 +0300

    CM-872 Added Edited flag and refactoring

--- src/containers/Common/components/CommonDetailContainer/ChatComponent/ChatMessage.tsx
+++ src/containers/Common/components/CommonDetailContainer/ChatComponent/ChatMessage.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import classNames from "classnames";
 import { Linkify, ElementDropdown, UserAvatar } from "@/shared/components";
 import { DiscussionMessage, User } from "@/shared/models";
 import { getUserName } from "@/shared/utils";
 import { DynamicLinkType, Orientation, ChatType, ENTITY_TYPES } from "@/shared/constants";
-import EditMessageInput from './EditMessageInput';
+import EditMessageInput from "./EditMessageInput";
 
 interface ChatMessageProps {
   discussionMessage: DiscussionMessage;
@@ -24,19 +24,25 @@ const getDynamicLinkByChatType = (chatType: ChatType): DynamicLinkType => {
   }
 };
 
-export default function ChatMessage(
-  {
-    discussionMessage,
-    chatType,
-    highlighted = false,
-    className,
-    onMessageDropdownOpen,
-    user,
-  }: ChatMessageProps
-) {
+export default function ChatMessage({
+  discussionMessage,
+  chatType,
+  highlighted = false,
+  className,
+  onMessageDropdownOpen,
+  user,
+}: ChatMessageProps) {
   const [isEditMode, setEditMode] = useState(false);
-  const [message, setMessage] = useState(discussionMessage.text);
-  const mDate = new Date(discussionMessage.createdAt.seconds * 1000);
+  const createdAtDate = new Date(discussionMessage.createdAt.seconds * 1000);
+  const updateAtdDate = new Date(discussionMessage.updatedAt.seconds * 1000);
+
+  const isEdited = useMemo(() => {
+    if (updateAtdDate > createdAtDate) {
+      return true;
+    }
+
+    return false;
+  }, [createdAtDate, updateAtdDate]);
 
   const ReplyMessageContainer = useCallback(() => {
     if (!discussionMessage.parentMessage) {
@@ -45,20 +51,14 @@ export default function ChatMessage(
 
     return (
       <div className="reply-message-container">
-        <div className="message-name">
-          {discussionMessage.parentMessage?.ownerName}
-        </div>
+        <div className="message-name">{discussionMessage.parentMessage?.ownerName}</div>
         <Linkify>{discussionMessage.parentMessage.text}</Linkify>
       </div>
-    )
-  }, [discussionMessage.parentMessage])
-
+    );
+  }, [discussionMessage.parentMessage]);
 
   return (
-    <li
-      id={discussionMessage.id}
-      className={classNames("message-wrapper", className, { highlighted })}
-    >
+    <li id={discussionMessage.id} className={classNames("message-wrapper", className, { highlighted })}>
       <div className="message">
         <div className="icon-wrapper">
           <UserAvatar
@@ -68,22 +68,32 @@ export default function ChatMessage(
           />
         </div>
         {isEditMode ? (
-         <EditMessageInput discussionMessage={discussionMessage} onClose={() => setEditMode(false)}/>
-        ) : 
-        (
+          <EditMessageInput discussionMessage={discussionMessage} onClose={() => setEditMode(false)} />
+        ) : (
           <div className="message-text">
             <ReplyMessageContainer />
-            <div className="message-name">
-              {getUserName(discussionMessage.owner)}
-            </div>
+            <div className="message-name">{getUserName(discussionMessage.owner)}</div>
             <div className="message-content">
               <Linkify>{discussionMessage.text}</Linkify>
-              <div className="time-wrapper">
-                {mDate.toLocaleTimeString([], {
-                  hour12: false,
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
+              <div className="time-wrapper-container">
+                {isEdited && (
+                  <div className="time-wrapper edited-time-wrapper ">
+                    (Edited{" "}
+                    {updateAtdDate.toLocaleTimeString([], {
+                      hour12: false,
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                    )
+                  </div>
+                )}
+                <div className="time-wrapper creation-time-wrapper">
+                  {createdAtDate.toLocaleTimeString([], {
+                    hour12: false,
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </div>
               </div>
             </div>
             <ElementDropdown
