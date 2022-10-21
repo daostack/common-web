bf2cc89c78b21324269493525fd0408dde6f7c42
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-21 16:07:40 +0300

    fix circles by id filtering

--- src/shared/utils/circles.ts
+++ src/shared/utils/circles.ts
@@ -3,13 +3,10 @@ import { Circle } from "../models";
 
 export const getFilteredByIdCircles = (
   circles: Circle[] | null,
-  circleIds?: string[]
+  circleIds: string[] = []
 ): Circle[] => {
-  if (!circles || circles.length === 0) {
+  if (!circles || circles.length === 0 || circleIds.length === 0) {
     return [];
-  }
-  if (!circleIds || circleIds.length === 0) {
-    return [...circles];
   }
 
   return circles.filter(({ id }) => circleIds.includes(id));
