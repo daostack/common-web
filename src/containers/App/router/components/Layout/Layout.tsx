ff0e4bcdd5f1129d234ba93d63f91c73e6cf774a
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-28 19:16:50 +0300

    move router layout to separate folder

--- src/containers/App/router/components/Layout/Layout.tsx
+++ src/containers/App/router/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Route, Switch } from "react-router-dom";
-import { LayoutConfigurationWithRouteProps } from "../types";
+import { LayoutConfigurationWithRouteProps } from "../../types";
 
 const Layout: FC<LayoutConfigurationWithRouteProps> = (props) => {
   const { component: LayoutComponent, routes, ...restProps } = props;
