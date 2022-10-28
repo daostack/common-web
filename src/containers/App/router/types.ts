a811240b7667a3b84732829abe17998df8eca69b
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-28 14:58:03 +0300

    create different route type configurations

--- src/containers/App/router/types.ts
+++ src/containers/App/router/types.ts
@@ -2,12 +2,36 @@ import { ComponentType } from "react";
 import { RouteProps } from "react-router";
 import { ROUTE_PATHS } from "src/shared/constants";
 
-export interface AppRoute extends Pick<RouteProps, "component" | "exact"> {
-  path: ROUTE_PATHS;
-  isPrivate?: boolean;
+enum RouteType {
+  Public,
+  Private,
+  OnlyPublic,
 }
+
+interface GeneralRouteConfiguration
+  extends Pick<RouteProps, "component" | "exact"> {
+  path: ROUTE_PATHS;
+}
+
+export interface PublicRouteConfiguration extends GeneralRouteConfiguration {
+  type?: RouteType.Public;
+}
+
+export interface PrivateRouteConfiguration extends GeneralRouteConfiguration {
+  type: RouteType.Private;
+}
+
+export interface OnlyPublicRouteConfiguration
+  extends GeneralRouteConfiguration {
+  type: RouteType.OnlyPublic;
+}
+
+type Route =
+  | PublicRouteConfiguration
+  | PrivateRouteConfiguration
+  | OnlyPublicRouteConfiguration;
 
 export interface LayoutConfiguration {
   component: ComponentType;
-  routes: AppRoute[];
+  routes: Route[];
 }
