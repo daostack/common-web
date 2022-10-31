613ac2d560dc801f9ba19f96a2a52789191a3c8d
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-31 16:53:42 +0300

    create webview login handler

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,56 +1,19 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
 import {
   BackgroundNotificationModal,
   TutorialModal,
 } from "@/shared/components";
 import { NotificationProvider } from "@/shared/components/Notification";
-import {
-  ROUTE_PATHS,
-  SMALL_SCREEN_BREAKPOINT,
-  ScreenSize,
-  WebviewActions,
-} from "@/shared/constants";
-import { FirebaseCredentials } from "@/shared/interfaces/FirebaseCredentials";
+import { SMALL_SCREEN_BREAKPOINT, ScreenSize } from "@/shared/constants";
 import { changeScreenSize } from "@/shared/store/actions";
 import { selectTutorialModalState } from "@/shared/store/selectors";
-import { parseJson } from "@/shared/utils/json";
-import { webviewLogin } from "../Auth/store/actions";
-import { TextDirectionHandler } from "./handlers";
+import { TextDirectionHandler, WebViewLoginHandler } from "./handlers";
 import { Router } from "./router";
 
 const App = () => {
   const dispatch = useDispatch();
   const tutorialModalState = useSelector(selectTutorialModalState());
-  const history = useHistory();
-
-  useEffect(() => {
-    window.addEventListener("message", (event) => {
-      const data = parseJson(event.data) as FirebaseCredentials;
-      if (!data?.providerId) {
-        return;
-      }
-
-      try {
-        dispatch(
-          webviewLogin.request({
-            payload: data,
-            callback: (isLoggedIn) => {
-              if (isLoggedIn) {
-                window.ReactNativeWebView.postMessage(
-                  WebviewActions.loginSuccess,
-                );
-                history.push(ROUTE_PATHS.MY_COMMONS);
-              }
-            },
-          }),
-        );
-      } catch (err) {
-        window.ReactNativeWebView.postMessage(WebviewActions.loginError);
-      }
-    });
-  }, []);
 
   useEffect(() => {
     const screenSize = window.matchMedia(
@@ -86,6 +49,7 @@ const App = () => {
       <TutorialModal isShowing={tutorialModalState.isShowing} />
       <NotificationProvider>
         <TextDirectionHandler />
+        <WebViewLoginHandler />
         <Router />
       </NotificationProvider>
     </>
---
+++ src/containers/App/handlers/WebViewLoginHandler/WebViewLoginHandler.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+import { webviewLogin } from "@/containers/Auth/store/actions";
+import { ROUTE_PATHS, WebviewActions } from "@/shared/constants";
+import { FirebaseCredentials } from "@/shared/interfaces/FirebaseCredentials";
+import { parseJson } from "@/shared/utils/json";
+
+const WebViewLoginHandler: FC = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      const data = parseJson(event.data) as FirebaseCredentials;
+      if (!data?.providerId) {
+        return;
+      }
+
+      try {
+        dispatch(
+          webviewLogin.request({
+            payload: data,
+            callback: (isLoggedIn) => {
+              if (isLoggedIn) {
+                window.ReactNativeWebView.postMessage(
+                  WebviewActions.loginSuccess,
+                );
+                history.push(ROUTE_PATHS.MY_COMMONS);
+              }
+            },
+          }),
+        );
+      } catch (err) {
+        window.ReactNativeWebView.postMessage(WebviewActions.loginError);
+      }
+    });
+  }, []);
+
+  return null;
+};
+
+export default WebViewLoginHandler;
