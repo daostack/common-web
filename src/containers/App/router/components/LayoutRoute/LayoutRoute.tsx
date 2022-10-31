e4c912eaf7a0ffe4c87627593fdbe73e4fe7f34a
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-10-31 14:23:34 +0300

    wrap layout in the appropriate component

--- src/containers/App/router/components/LayoutRoute/LayoutRoute.tsx
+++ src/containers/App/router/components/LayoutRoute/LayoutRoute.tsx
@@ -6,16 +6,26 @@ import {
   authentificated,
   selectUserRoles,
 } from "@/containers/Auth/store/selectors";
+import { matchRoute } from "@/shared/utils";
 import { renderRouteContent } from "./helpers";
 
 interface PrivateRouteProps extends RouteProps {
-  routeConfiguration: RouteConfiguration;
+  routeConfigurations: RouteConfiguration[];
 }
 
 const LayoutRoute: FC<PrivateRouteProps> = (props) => {
-  const { routeConfiguration, component, children, ...restProps } = props;
+  const { routeConfigurations, component, children, ...restProps } = props;
   const userRoles = useSelector(selectUserRoles());
   const authenticated = useSelector(authentificated());
+  const pathname = restProps.location?.pathname || "";
+  const routeConfiguration = routeConfigurations.find((configuration) =>
+    matchRoute(pathname, configuration.path, { exact: configuration.exact }),
+  );
+
+  if (!routeConfiguration) {
+    console.error(`There is no specified route for path: "${pathname}"`);
+    return null;
+  }
 
   return (
     <Route
