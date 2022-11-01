db805b024bfd9144d22021c949a77c4ba21d753f
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-01 12:26:04 +0300

    remove welldone-software dependency

--- src/containers/App/AppWrapper.tsx
+++ src/containers/App/AppWrapper.tsx
@@ -1,14 +1,7 @@
 import React, { FC } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/shared/appConfig";
-import { Environment, REACT_APP_ENV } from "@/shared/constants";
 import "@/shared/utils/yup";
-
-if ([Environment.Local, Environment.Dev].includes(REACT_APP_ENV)) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const whyDidYouRender = require("@welldone-software/why-did-you-render");
-  whyDidYouRender(React, { trackHooks: true });
-}
 
 const AppWrapper: FC = ({ children }) => (
   <Provider store={store}>{children}</Provider>
