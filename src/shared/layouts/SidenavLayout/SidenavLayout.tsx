d7529879c011f3fd584e661ed5e567ecd6644095
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-10 17:10:32 +0300

    add main styling logic for sidenav layout desktop view

--- src/shared/layouts/SidenavLayout/SidenavLayout.tsx
+++ src/shared/layouts/SidenavLayout/SidenavLayout.tsx
@@ -7,9 +7,9 @@ const SidenavLayout: FC = (props) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.contentContainer}>
-        <aside id="sidenav-open" className={styles.sidenav}>
-          <div className={styles.sidenavContent}>
+      <aside id="sidenav-open" className={styles.aside}>
+        <div className={styles.sidenavContent}>
+          <div className={styles.customContent}>
             {Array(amountOfBlocks)
               .fill(null)
               .map((item, index) => (
@@ -29,110 +29,89 @@ const SidenavLayout: FC = (props) => {
               Add block
             </button>
           </div>
-
+        </div>
+        <a
+          href="#"
+          id="sidenav-close"
+          className={styles.sidenavClose}
+          title="Close Menu"
+          aria-label="Close Menu"
+        />
+      </aside>
+      <main className={styles.main}>
+        <header>
           <a
-            href="#"
-            id="sidenav-close"
-            title="Close Menu"
-            aria-label="Close Menu"
-          />
-        </aside>
-        <main className={styles.main}>
-          <header>
-            <a
-              href="#sidenav-open"
-              id="sidenav-button"
-              className="hamburger"
-              title="Open Menu"
-              aria-label="Open Menu"
-            >
-              Open Sidenav
-            </a>
-            <h1>Site Title</h1>
-          </header>
+            href="#sidenav-open"
+            id="sidenav-button"
+            className="hamburger"
+            title="Open Menu"
+            aria-label="Open Menu"
+          >
+            Open Sidenav
+          </a>
+          <h1>Site Title</h1>
+        </header>
 
-          <article>
-            <h2>Totam Header</h2>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-
-            <h3>Subhead Totam Odit</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-
-            <h3>Subhead</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-
-            <h3>Subhead</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-
-            <h3>Subhead</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
-              rerum, amet odio explicabo voluptas eos cum libero, ex esse quasi
-              optio incidunt soluta eligendi labore error corrupti! Dolore,
-              cupiditate porro.
-            </p>
-          </article>
-        </main>
-      </div>
-      <footer>
-        Footer1 <br />
-        Footer1 <br />
-        Footer1 <br />
-        Footer1 <br />
-        Footer1 <br />
-        Footer1 <br />
-        Footer1 <br />
-        Footer1 <br />
+        <article>
+          <h2>Totam Header</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+            consectetur, necessitatibus velit officia ut impedit veritatis
+            temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
+            necessitatibus voluptatem nihil doloribus! Enim.
+          </p>
+        </article>
+      </main>
+      <footer className={styles.footer}>
+        Footer1Footer1Footer1Footer1Footer1
       </footer>
     </div>
   );
