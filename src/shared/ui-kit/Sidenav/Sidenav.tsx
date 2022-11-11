faffe8ed8eaf76aac0be2e629d3ba578a0f78746
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-11 18:23:25 +0300

    block page scroll when sidenav is open on mobile view

--- src/shared/ui-kit/Sidenav/Sidenav.tsx
+++ src/shared/ui-kit/Sidenav/Sidenav.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from "react";
+import { useLocation } from "react-router";
 import { SIDENAV_ID } from "@/shared/constants";
+import { useLockedBody } from "@/shared/hooks";
+import { useIsTabletView } from "@/shared/hooks/viewport";
 import styles from "./Sidenav.module.scss";
 
 const Sidenav: FC = (props) => {
   const { children } = props;
+  const location = useLocation();
+  const isTabletView = useIsTabletView();
+  const isSidenavOpen = !isTabletView || location.hash === `#${SIDENAV_ID}`;
+  const shouldLockBodyScroll = isTabletView && isSidenavOpen;
+  useLockedBody(shouldLockBodyScroll);
 
   return (
     <aside id={SIDENAV_ID} className={styles.sidenav}>
