1d0efd07db19e644fd260961ab30e73eba1ce5cf
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-14 18:18:29 +0300

    create component for menu item

--- src/shared/layouts/SidenavLayout/components/SidenavContent/components/UserInfo/components/MenuItems/MenuItems.tsx
+++ src/shared/layouts/SidenavLayout/components/SidenavContent/components/UserInfo/components/MenuItems/MenuItems.tsx
@@ -1,37 +1,35 @@
 import React, { FC } from "react";
-import { NavLink } from "react-router-dom";
-import classNames from "classnames";
 import { Menu } from "@headlessui/react";
 import { ROUTE_PATHS } from "@/shared/constants";
+import { MenuItem } from "./components";
+import { Item, ItemType } from "./types";
 import styles from "./MenuItems.module.scss";
 
 const MenuItems: FC = () => {
+  const items: Item[] = [
+    {
+      key: "my-account",
+      text: "My Account",
+      to: ROUTE_PATHS.MY_ACCOUNT_PROFILE,
+    },
+    {
+      key: "log-out",
+      type: ItemType.Button,
+      text: "Log out",
+      onClick: () => {
+        console.log("Log out");
+      },
+    },
+  ];
+
   return (
     <Menu.Items as={React.Fragment}>
       <ul className={styles.itemsWrapper}>
-        <Menu.Item>
-          {({ active }) => (
-            <NavLink
-              className={classNames(styles.item, {
-                [styles.itemActive]: active,
-              })}
-              to={ROUTE_PATHS.MY_ACCOUNT_PROFILE}
-            >
-              My Account
-            </NavLink>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <button
-              className={classNames(styles.item, {
-                [styles.itemActive]: active,
-              })}
-            >
-              Log out
-            </button>
-          )}
-        </Menu.Item>
+        {items.map((item) => (
+          <Menu.Item key={item.key}>
+            {({ active }) => <MenuItem item={item} active={active} />}
+          </Menu.Item>
+        ))}
       </ul>
     </Menu.Items>
   );
---
+++ src/shared/layouts/SidenavLayout/components/SidenavContent/components/UserInfo/components/MenuItems/components/MenuItem/MenuItem.tsx
@@ -0,0 +1,57 @@
+import React, {
+  forwardRef,
+  ForwardRefRenderFunction,
+  MouseEventHandler,
+  RefObject,
+} from "react";
+import { NavLink } from "react-router-dom";
+import classNames from "classnames";
+import { Item, ItemType } from "../../types";
+import styles from "./MenuItem.module.scss";
+
+interface MenuItemProps {
+  item: Item;
+  active: boolean;
+  // This is passed by Menu.Item and we should call it if we override onClick
+  onClick?: MouseEventHandler<HTMLAnchorElement | HTMLButtonElement>;
+}
+const MenuItem: ForwardRefRenderFunction<{}, MenuItemProps> = (props, ref) => {
+  const { item, active, ...restProps } = props;
+  const content = item.text;
+  const className = classNames(styles.item, {
+    [styles.itemActive]: active,
+  });
+
+  switch (item.type) {
+    case ItemType.Button:
+      return (
+        <button
+          ref={ref as RefObject<HTMLButtonElement>}
+          {...restProps}
+          className={className}
+          onClick={(...args) => {
+            item.onClick();
+
+            if (restProps.onClick) {
+              restProps.onClick(...args);
+            }
+          }}
+        >
+          {content}
+        </button>
+      );
+    default:
+      return (
+        <NavLink
+          ref={ref as RefObject<HTMLAnchorElement>}
+          {...restProps}
+          className={className}
+          to={item.to}
+        >
+          {content}
+        </NavLink>
+      );
+  }
+};
+
+export default forwardRef(MenuItem);
