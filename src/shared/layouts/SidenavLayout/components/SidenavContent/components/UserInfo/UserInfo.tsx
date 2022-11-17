f71e20bc4507ad6e29200c70aa07d5e818d3bdaa
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-17 17:46:30 +0300

    add different contents to user info component

--- src/shared/layouts/SidenavLayout/components/SidenavContent/components/UserInfo/UserInfo.tsx
+++ src/shared/layouts/SidenavLayout/components/SidenavContent/components/UserInfo/UserInfo.tsx
@@ -1,18 +1,23 @@
 import React, { FC } from "react";
-import { AuthenticatedContent } from "./components";
+import { AuthenticatedContent, UnauthenticatedContent } from "./components";
 import styles from "./UserInfo.module.scss";
 
 interface UserInfoProps {
   avatarURL?: string;
   userName?: string;
+  isAuthenticated?: boolean;
 }
 
 const UserInfo: FC<UserInfoProps> = (props) => {
-  const { avatarURL, userName } = props;
+  const { avatarURL, userName, isAuthenticated = false } = props;
 
   return (
     <div className={styles.container}>
-      <AuthenticatedContent avatarURL={avatarURL} userName={userName} />
+      {isAuthenticated ? (
+        <AuthenticatedContent avatarURL={avatarURL} userName={userName} />
+      ) : (
+        <UnauthenticatedContent />
+      )}
     </div>
   );
 };
