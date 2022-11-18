0cad28b15e651eddffc34c61f628dbda65292b5d
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-18 17:52:52 +0300

    update lock body scroll hook
    Fixed issue that it didn't work on iOS

--- src/shared/layouts/SidenavLayout/SidenavLayout.tsx
+++ src/shared/layouts/SidenavLayout/SidenavLayout.tsx
@@ -1,13 +1,22 @@
 import React, { FC } from "react";
+import { useLocation } from "react-router";
+import { SIDENAV_ID } from "@/shared/constants";
+import { useLockedBody } from "@/shared/hooks";
+import { useIsTabletView } from "@/shared/hooks/viewport";
 import { Footer, Sidenav } from "@/shared/ui-kit";
 import { SidenavContent } from "./components";
 import styles from "./SidenavLayout.module.scss";
 
 const SidenavLayout: FC = (props) => {
   const { children } = props;
+  const location = useLocation();
+  const isTabletView = useIsTabletView();
+  const isSidenavOpen = !isTabletView || location.hash === `#${SIDENAV_ID}`;
+  const shouldLockBodyScroll = isTabletView && isSidenavOpen;
+  const { setTargetEl } = useLockedBody(shouldLockBodyScroll);
 
   return (
-    <div className={styles.container}>
+    <div ref={setTargetEl} className={styles.container}>
       <Sidenav>
         <SidenavContent className={styles.sidenavContent} />
       </Sidenav>
--- src/shared/ui-kit/Sidenav/Sidenav.tsx
+++ src/shared/ui-kit/Sidenav/Sidenav.tsx
@@ -1,18 +1,10 @@
 import React, { FC, KeyboardEvent } from "react";
-import { useLocation } from "react-router";
 import { SIDENAV_ID } from "@/shared/constants";
 import { KeyboardKeys } from "@/shared/constants/keyboardKeys";
-import { useLockedBody } from "@/shared/hooks";
-import { useIsTabletView } from "@/shared/hooks/viewport";
 import styles from "./Sidenav.module.scss";
 
 const Sidenav: FC = (props) => {
   const { children } = props;
-  const location = useLocation();
-  const isTabletView = useIsTabletView();
-  const isSidenavOpen = !isTabletView || location.hash === `#${SIDENAV_ID}`;
-  const shouldLockBodyScroll = isTabletView && isSidenavOpen;
-  useLockedBody(shouldLockBodyScroll);
 
   const closeSidenav = () => {
     window.location.hash = "";
