778df6fe5c1b7b17bab291daebe3bf94a234edc2
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-21 16:17:42 +0100

    create basic new common page

--- src/pages/App/router/configuration/sidenavLayout.tsx
+++ src/pages/App/router/configuration/sidenavLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { ROUTE_PATHS, SIDENAV_ID } from "@/shared/constants";
+import { Common as CommonPage } from "@/pages/common";
+import { ROUTE_PATHS } from "@/shared/constants";
 import { SidenavLayout } from "@/shared/layouts";
 import { LayoutConfiguration } from "../types";
 
@@ -7,82 +8,9 @@ export const SIDENAV_LAYOUT_CONFIGURATION: LayoutConfiguration = {
   component: SidenavLayout,
   routes: [
     {
-      path: ROUTE_PATHS.TEST,
+      path: ROUTE_PATHS.COMMON,
       exact: true,
-      component: () => (
-        <div>
-          <header>
-            <a
-              href={`#${SIDENAV_ID}`}
-              id="sidenav-button"
-              className="hamburger"
-              title="Open Menu"
-              aria-label="Open Menu"
-            >
-              Open Sidenav
-            </a>
-            <h1>Site Title</h1>
-          </header>
-
-          <article>
-            <h2>Totam Header</h2>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              consectetur, necessitatibus velit officia ut impedit veritatis
-              temporibus soluta? Totam odit cupiditate facilis nisi sunt hic
-              necessitatibus voluptatem nihil doloribus! Enim.
-            </p>
-          </article>
-        </div>
-      ),
+      component: CommonPage,
     },
   ],
 };
