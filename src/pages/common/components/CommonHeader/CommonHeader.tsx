b81e0eac11bd6bb2312cda332c3afc8706aa175b
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-22 17:41:28 +0100

    update common header for mobile view

--- src/pages/common/components/CommonHeader/CommonHeader.tsx
+++ src/pages/common/components/CommonHeader/CommonHeader.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import classNames from "classnames";
+import { useIsTabletView } from "@/shared/hooks/viewport";
 import { Button, ButtonSize, ButtonVariant } from "@/shared/ui-kit";
 import { formatPrice } from "@/shared/utils";
 import { KeyValueItem, KeyValuePairs } from "./components";
@@ -19,6 +20,8 @@ const CommonHeader: FC<CommonHeaderProps> = (props) => {
     description,
     joinButtonText = "Join the effort",
   } = props;
+  const isTabletView = useIsTabletView();
+  const isJoinButtonVisible = !isTabletView;
   const items: KeyValueItem[] = [
     {
       id: "available-funds",
@@ -40,23 +43,27 @@ const CommonHeader: FC<CommonHeaderProps> = (props) => {
           src={commonSrc}
           alt={`${commonName}'s image`}
         />
-        <div className={styles.commonDetailsWrapper}>
-          <h1 className={styles.commonName}>{commonName}</h1>
+        <div>
+          <h1 className={styles.commonName} title={commonName}>
+            {commonName}
+          </h1>
           <p className={styles.description} title={description}>
             {description}
           </p>
         </div>
       </header>
       <div className={styles.rightHalf}>
-        <Button
-          className={classNames(styles.joinButton, {
-            [styles.joinButtonWithMargin]: true,
-          })}
-          variant={ButtonVariant.OutlineBlue}
-          size={ButtonSize.Medium}
-        >
-          {joinButtonText}
-        </Button>
+        {isJoinButtonVisible && (
+          <Button
+            className={classNames(styles.joinButton, {
+              [styles.joinButtonWithMargin]: true,
+            })}
+            variant={ButtonVariant.OutlineBlue}
+            size={ButtonSize.Medium}
+          >
+            {joinButtonText}
+          </Button>
+        )}
         <KeyValuePairs items={items} />
       </div>
     </section>
