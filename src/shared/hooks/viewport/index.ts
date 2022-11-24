74a572d939c71d12717997ee494443f030828953
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-24 15:46:42 +0100

    add hooks for the rest viewport breakpoints

--- src/shared/hooks/viewport/index.ts
+++ src/shared/hooks/viewport/index.ts
@@ -1,1 +1,5 @@
+export * from "./useIsDesktopView";
+export * from "./useIsLaptopView";
+export * from "./useIsPhoneOrientedView";
+export * from "./useIsPhoneView";
 export * from "./useIsTabletView";
