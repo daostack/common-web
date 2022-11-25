9b2e2a2999b54a1ed5b2081a470a4bff2ce0c3d0
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-25 15:32:50 +0100

    not allow closing of login modal during auth loading state

--- src/shared/layouts/SidenavLayout/components/SidenavContent/components/UserInfo/components/UnauthenticatedContent/UnauthenticatedContent.tsx
+++ src/shared/layouts/SidenavLayout/components/SidenavContent/components/UserInfo/components/UnauthenticatedContent/UnauthenticatedContent.tsx
@@ -1,24 +1,35 @@
 import React, { FC } from "react";
+import { useDispatch } from "react-redux";
+import { setLoginModalState } from "@/pages/Auth/store/actions";
 import avatarPlaceholderSrc from "@/shared/assets/images/avatar-placeholder.svg";
 import { Button, ButtonSize, ButtonVariant } from "@/shared/ui-kit/Button";
 import { Content } from "../Content";
 import styles from "./UnauthenticatedContent.module.scss";
 
-const UnauthenticatedContent: FC = () => (
-  <Content
-    avatarURL={avatarPlaceholderSrc}
-    userName="Guest"
-    as="div"
-    leftSideEl={
-      <Button
-        className={styles.loginButton}
-        variant={ButtonVariant.OutlineBlue}
-        size={ButtonSize.Xsmall}
-      >
-        Login
-      </Button>
-    }
-  />
-);
+const UnauthenticatedContent: FC = () => {
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    dispatch(setLoginModalState({ isShowing: true }));
+  };
+
+  return (
+    <Content
+      avatarURL={avatarPlaceholderSrc}
+      userName="Guest"
+      as="div"
+      leftSideEl={
+        <Button
+          className={styles.loginButton}
+          variant={ButtonVariant.OutlineBlue}
+          size={ButtonSize.Xsmall}
+          onClick={handleLogin}
+        >
+          Login
+        </Button>
+      }
+    />
+  );
+};
 
 export default UnauthenticatedContent;
