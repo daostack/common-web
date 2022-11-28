b2ffe2c0d13454cbe5293965a183dd8dc3279221
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-28 14:07:53 +0100

    fetch projects based on user id

--- src/store/states/projects/saga.ts
+++ src/store/states/projects/saga.ts
@@ -1,15 +1,23 @@
-import { call, put, takeLatest } from "redux-saga/effects";
+import { call, put, select, takeLatest } from "redux-saga/effects";
+import { selectUser } from "@/pages/Auth/store/selectors";
 import { CommonService } from "@/services";
 import { Awaited } from "@/shared/interfaces";
+import { User } from "@/shared/models";
 import { isError } from "@/shared/utils";
 import { ProjectsStateItem } from "@/store/states";
 import * as actions from "./actions";
 
 function* getProjects() {
   try {
+    const user = (yield select(selectUser())) as User | null;
+
+    if (!user) {
+      throw new Error("There is no user to fetch projects");
+    }
+
     const data = (yield call(
       CommonService.getUserProjectsInfo,
-      "jTvCqKFjlVaNfANUPJq4v8d5bI43",
+      user.id,
     )) as Awaited<ReturnType<typeof CommonService.getUserProjectsInfo>>;
     const projectsData: ProjectsStateItem[] = data.map(
       ({ common, hasMembership }) => ({
