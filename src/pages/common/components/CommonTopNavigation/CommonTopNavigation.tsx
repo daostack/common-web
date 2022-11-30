6f6473f234025f3f006e43fa1da3df7d80e84211
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-11-30 12:00:00 +0100

    add open sidenav button to common top navigation

--- src/pages/common/components/CommonTopNavigation/CommonTopNavigation.tsx
+++ src/pages/common/components/CommonTopNavigation/CommonTopNavigation.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from "react";
-import { TopNavigation } from "@/shared/ui-kit";
+import { TopNavigation, TopNavigationOpenSidenavButton } from "@/shared/ui-kit";
 import styles from "./CommonTopNavigation.module.scss";
 
 const CommonTopNavigation: FC = () => {
-  return <TopNavigation className={styles.container}>Content</TopNavigation>;
+  return (
+    <TopNavigation className={styles.container}>
+      <TopNavigationOpenSidenavButton />
+    </TopNavigation>
+  );
 };
 
 export default CommonTopNavigation;
