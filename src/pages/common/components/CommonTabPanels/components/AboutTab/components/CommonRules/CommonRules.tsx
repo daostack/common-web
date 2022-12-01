c409d5e02747a7f2f4e1749cece43d97ee9e286f
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-12-01 18:16:39 +0100

    update common rules styles for mobile view

--- src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonRules/CommonRules.tsx
+++ src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonRules/CommonRules.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
 import classNames from "classnames";
+import { ViewportBreakpointVariant } from "@/shared/constants";
+import { useIsTabletView } from "@/shared/hooks/viewport";
 import { UnstructuredRules } from "@/shared/models";
+import { Container } from "@/shared/ui-kit";
 import { isRTL } from "@/shared/utils";
 import { CommonCard } from "../../../../../CommonCard";
 import styles from "./CommonRules.module.scss";
@@ -11,24 +14,33 @@ interface CommonRulesProps {
 
 const CommonRules: FC<CommonRulesProps> = (props) => {
   const { rules } = props;
+  const isTabletView = useIsTabletView();
 
   return (
-    <CommonCard>
-      <h3 className={styles.title}>Common Rules</h3>
-      <dl className={styles.list}>
-        {rules.map((rule, index) => (
-          <div
-            key={index}
-            className={classNames(styles.item, {
-              [styles.itemRTL]: isRTL(rule.title) || isRTL(rule.definition),
-            })}
-          >
-            <dt className={styles.itemTitle}>{rule.title}</dt>
-            <dd className={styles.itemDescription}>{rule.definition}</dd>
-          </div>
-        ))}
-      </dl>
-    </CommonCard>
+    <Container
+      viewports={[
+        ViewportBreakpointVariant.Tablet,
+        ViewportBreakpointVariant.PhoneOriented,
+        ViewportBreakpointVariant.Phone,
+      ]}
+    >
+      <CommonCard hideCardStyles={isTabletView}>
+        <h3 className={styles.title}>Common Rules</h3>
+        <dl className={styles.list}>
+          {rules.map((rule, index) => (
+            <div
+              key={index}
+              className={classNames(styles.item, {
+                [styles.itemRTL]: isRTL(rule.title) || isRTL(rule.definition),
+              })}
+            >
+              <dt className={styles.itemTitle}>{rule.title}</dt>
+              <dd className={styles.itemDescription}>{rule.definition}</dd>
+            </div>
+          ))}
+        </dl>
+      </CommonCard>
+    </Container>
   );
 };
 
