58afeecdfc2e3e5d9cc1db55996ca6772e96e9e7
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-12-07 11:29:10 +0100

    create useTooltip hook

---
+++ src/shared/ui-kit/Tooltip/hooks/useTooltip.ts
@@ -0,0 +1,59 @@
+import { useMemo, useState } from "react";
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  useHover,
+  useFocus,
+  useDismiss,
+  useRole,
+  useInteractions,
+} from "@floating-ui/react-dom-interactions";
+import { TooltipOptions } from "../types";
+
+export const useTooltip = (options: TooltipOptions = {}) => {
+  const {
+    initialOpen = false,
+    placement = "top",
+    open: controlledOpen,
+    onOpenChange: setControlledOpen,
+  } = options;
+  const [uncontrolledOpen, setUncontrolledOpen] = useState(initialOpen);
+
+  const open = controlledOpen ?? uncontrolledOpen;
+  const setOpen = setControlledOpen ?? setUncontrolledOpen;
+
+  const data = useFloating({
+    placement,
+    open,
+    onOpenChange: setOpen,
+    whileElementsMounted: autoUpdate,
+    middleware: [offset(8), flip(), shift()],
+  });
+
+  const { context } = data;
+
+  const hover = useHover(context, {
+    move: false,
+    enabled: typeof controlledOpen === "undefined",
+  });
+  const focus = useFocus(context, {
+    enabled: typeof controlledOpen === "undefined",
+  });
+  const dismiss = useDismiss(context);
+  const role = useRole(context, { role: "tooltip" });
+
+  const interactions = useInteractions([hover, focus, dismiss, role]);
+
+  return useMemo(
+    () => ({
+      open,
+      setOpen,
+      ...interactions,
+      ...data,
+    }),
+    [open, setOpen, interactions, data],
+  );
+};
