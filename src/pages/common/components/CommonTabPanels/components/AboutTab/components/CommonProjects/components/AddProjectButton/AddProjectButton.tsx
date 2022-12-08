8dbb3793ccb2f1b32885bb26dfb27b6756307ee8
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-12-08 12:10:04 +0100

    add tooltip to add project button component

--- src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonProjects/components/AddProjectButton/AddProjectButton.tsx
+++ src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonProjects/components/AddProjectButton/AddProjectButton.tsx
@@ -1,18 +1,24 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode } from "react";
 import classNames from "classnames";
 import { PlusIcon } from "@/shared/icons";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/shared/ui-kit";
 import styles from "./AddProjectButton.module.scss";
 
 interface AddProjectButtonProps {
   text?: string;
+  tooltipContent?: ReactNode;
   onClick?: () => void;
   visuallyDisabled?: boolean;
 }
 
 const AddProjectButton: FC<AddProjectButtonProps> = (props) => {
-  const { text = "Add new project", onClick, visuallyDisabled } = props;
-
-  return (
+  const {
+    text = "Add new project",
+    tooltipContent,
+    onClick,
+    visuallyDisabled,
+  } = props;
+  const buttonEl = (
     <button
       className={classNames(styles.item, {
         [styles.itemDisabled]: visuallyDisabled,
@@ -25,6 +31,19 @@ const AddProjectButton: FC<AddProjectButtonProps> = (props) => {
       </div>
       <span className={styles.text}>{text}</span>
     </button>
+  );
+
+  if (!tooltipContent) {
+    return buttonEl;
+  }
+
+  return (
+    <Tooltip placement="bottom-end">
+      <TooltipTrigger asChild>{buttonEl}</TooltipTrigger>
+      <TooltipContent className={styles.tooltipContent}>
+        {tooltipContent}
+      </TooltipContent>
+    </Tooltip>
   );
 };
 
