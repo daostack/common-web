18017cebf7c4d2ed2ec6bfffe35db671cdc68553
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-12-08 12:04:18 +0100

    add disabled state to add project button

--- src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonProjects/components/AddProjectButton/AddProjectButton.tsx
+++ src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonProjects/components/AddProjectButton/AddProjectButton.tsx
@@ -6,13 +6,20 @@ import styles from "./AddProjectButton.module.scss";
 interface AddProjectButtonProps {
   text?: string;
   onClick?: () => void;
+  visuallyDisabled?: boolean;
 }
 
 const AddProjectButton: FC<AddProjectButtonProps> = (props) => {
-  const { text = "Add new project", onClick } = props;
+  const { text = "Add new project", onClick, visuallyDisabled } = props;
 
   return (
-    <button className={classNames(styles.item)} onClick={onClick}>
+    <button
+      className={classNames(styles.item, {
+        [styles.itemDisabled]: visuallyDisabled,
+      })}
+      aria-disabled={visuallyDisabled}
+      onClick={visuallyDisabled ? undefined : onClick}
+    >
       <div className={styles.iconWrapper}>
         <PlusIcon className={styles.icon} />
       </div>
