01e4cff00771e703c00a21ff4250276da0e108cf
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2022-12-30 11:58:35 +0300

    restructure footer

--- src/shared/ui-kit/Footer/Footer.tsx
+++ src/shared/ui-kit/Footer/Footer.tsx
@@ -2,29 +2,56 @@ import React, { FC } from "react";
 import logoDAOStackSrc from "@/shared/assets/images/logo-daostack.svg";
 import logoFooterSrc from "@/shared/assets/images/logo-footer.svg";
 import { FOOTER_ID } from "@/shared/constants";
+import { useIsTabletView } from "@/shared/hooks/viewport";
 import { CopyrightIcon } from "@/shared/icons";
 import styles from "./Footer.module.scss";
 
-const Footer: FC = () => (
-  <footer id={FOOTER_ID} className={styles.footer}>
+export enum FooterVariant {
+  Default = "default",
+  Small = "small",
+}
+
+interface FooterProps {
+  variant?: FooterVariant;
+}
+
+const Footer: FC<FooterProps> = (props) => {
+  const { variant = FooterVariant.Default } = props;
+  const isTabletView = useIsTabletView();
+  const currentYear = new Date().getFullYear();
+  const commonLogoEl = (
     <img className={styles.commonLogo} src={logoFooterSrc} alt="Common Logo" />
-    <div className={styles.content}>
-      <div className={styles.poweredByInfo}>
-        <span>Powered by</span>
-        <img
-          className={styles.daoStackLogo}
-          src={logoDAOStackSrc}
-          alt="DAOstack Logo"
-        />
-        <span>DAOstack</span>
-      </div>
-      <div className={styles.copyrightInfo}>
-        Copyrights&nbsp;
-        <CopyrightIcon />
-        &nbsp;2021. All rights reserved
-      </div>
+  );
+  const poweredByEl = (
+    <div className={styles.poweredByInfo}>
+      <span>Powered by</span>
+      <img
+        className={styles.daoStackLogo}
+        src={logoDAOStackSrc}
+        alt="DAOstack Logo"
+      />
+      <span>DAOstack</span>
     </div>
-  </footer>
-);
+  );
+  const copyrightEl = (
+    <div className={styles.copyrightInfo}>
+      Copyrights&nbsp;
+      <CopyrightIcon />
+      &nbsp;{currentYear}. All rights reserved
+    </div>
+  );
+
+  return (
+    <footer id={FOOTER_ID} className={styles.footer}>
+      {commonLogoEl}
+      <div>
+        <div className={styles.content}>
+          {poweredByEl}
+          {!isTabletView && copyrightEl}
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
