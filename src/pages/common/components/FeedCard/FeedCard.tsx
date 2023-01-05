858144850c8ea9329dea4fb422bc1f647db14043
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-01-05 16:46:54 +0300

    add active flag to feed card

--- src/pages/common/components/FeedCard/FeedCard.tsx
+++ src/pages/common/components/FeedCard/FeedCard.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { FC } from "react";
+import classNames from "classnames";
 import { CommonCard } from "../CommonCard";
 import styles from "./FeedCard.module.scss";
 
-export const FeedCard: React.FC = ({ children }) => {
-  return <CommonCard className={styles.container}>{children}</CommonCard>;
+interface FeedCardProps {
+  isActive?: boolean;
+}
+
+export const FeedCard: FC<FeedCardProps> = (props) => {
+  const { isActive = false, children } = props;
+
+  return (
+    <CommonCard
+      className={classNames(styles.container, {
+        [styles.containerActive]: isActive,
+      })}
+    >
+      {children}
+    </CommonCard>
+  );
 };
