2dc464b44949676dbe1fcae3938bb58d3d928a52
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-01-20 17:43:51 +0300

    display pending state on mobile

--- src/pages/common/components/CommonTopNavigation/CommonTopNavigation.tsx
+++ src/pages/common/components/CommonTopNavigation/CommonTopNavigation.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
 import { CirclesPermissions, CommonMember, Governance } from "@/shared/models";
 import { TopNavigation, TopNavigationOpenSidenavButton } from "@/shared/ui-kit";
+import { useCommonDataContext } from "../../providers";
 import { CommonMemberInfo } from "../CommonMemberInfo";
 import { CommonMenuButton } from "../CommonMenuButton";
+import { PendingJoinInfo } from "../PendingJoinInfo";
 import styles from "./CommonTopNavigation.module.scss";
 
 interface CommonTopNavigationProps {
@@ -14,30 +16,35 @@ interface CommonTopNavigationProps {
 
 const CommonTopNavigation: FC<CommonTopNavigationProps> = (props) => {
   const { commonMember, circles, isSubCommon, commonId } = props;
+  const { isJoinPending } = useCommonDataContext();
   const circlesMap = commonMember?.circles.map;
-  const commonAction = commonMember ? (
-    <>
-      <CommonMemberInfo
-        className={styles.memberInfo}
-        circles={circles}
-        circlesMap={circlesMap}
-        commonId={commonId}
-        commonMember={commonMember}
-        isMobileVersion
-      />
-      <CommonMenuButton
-        commonMember={commonMember}
-        circles={circles}
-        isSubCommon={isSubCommon}
-        isMobileVersion
-      />
-    </>
+  const centralEl = commonMember ? (
+    <CommonMemberInfo
+      circles={circles}
+      circlesMap={circlesMap}
+      commonId={commonId}
+      commonMember={commonMember}
+      isMobileVersion
+    />
+  ) : isJoinPending ? (
+    <PendingJoinInfo isMobileVersion />
   ) : null;
+  const rightEl = commonMember ? (
+    <CommonMenuButton
+      commonMember={commonMember}
+      circles={circles}
+      isSubCommon={isSubCommon}
+      isMobileVersion
+    />
+  ) : (
+    <div className={styles.emptyBlock} />
+  );
 
   return (
     <TopNavigation className={styles.container}>
       <TopNavigationOpenSidenavButton />
-      {commonAction}
+      {centralEl}
+      {rightEl}
     </TopNavigation>
   );
 };
