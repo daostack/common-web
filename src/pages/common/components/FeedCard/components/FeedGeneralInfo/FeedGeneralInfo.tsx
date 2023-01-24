1eb8ce0379b1d3bc6ad6aa5ce8dfd0c806c28087
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-01-24 17:04:14 +0300

    change feed item description image display

--- src/pages/common/components/FeedCard/components/FeedGeneralInfo/FeedGeneralInfo.tsx
+++ src/pages/common/components/FeedCard/components/FeedGeneralInfo/FeedGeneralInfo.tsx
@@ -29,7 +29,15 @@ export const FeedGeneralInfo: React.FC<FeedGeneralInfoProps> = (props) => {
     () => parseStringToTextEditorValue(description),
     [description],
   );
+  const isDescriptionEmpty = checkIsTextEditorValueEmpty(parsedDescription);
   const image = images[0];
+  const shouldDisplaySeeMoreButton =
+    ((shouldShowFullText || !isFullTextShowing) && !isDescriptionEmpty) ||
+    images.length > 0;
+
+  const handleSeeMoreClick = () => {
+    toggleFullText();
+  };
 
   return (
     <div className={styles.container}>
@@ -39,32 +47,30 @@ export const FeedGeneralInfo: React.FC<FeedGeneralInfoProps> = (props) => {
       {subtitle && (
         <p className={classNames(styles.text, styles.subtitle)}>{subtitle}</p>
       )}
-      {!checkIsTextEditorValueEmpty(parsedDescription) && (
-        <>
-          <TextEditor
-            editorRef={setDescriptionRef}
-            editorClassName={classNames(styles.description, {
-              [styles.descriptionShortened]: !shouldShowFullText,
-            })}
-            value={parsedDescription}
-            readOnly
-          />
-          {(shouldShowFullText || !isFullTextShowing) && (
-            <a
-              className={classNames(styles.seeMore, styles.text)}
-              onClick={toggleFullText}
-            >
-              See {shouldShowFullText ? "less" : "more"}
-            </a>
-          )}
-        </>
+      {!isDescriptionEmpty && (
+        <TextEditor
+          editorRef={setDescriptionRef}
+          editorClassName={classNames(styles.description, {
+            [styles.descriptionShortened]: !shouldShowFullText,
+          })}
+          value={parsedDescription}
+          readOnly
+        />
       )}
-      {image && (
+      {image && shouldShowFullText && (
         <Image
           src={image.value}
           className={classNames(styles.image)}
           alt={image.title}
         />
+      )}
+      {shouldDisplaySeeMoreButton && (
+        <a
+          className={classNames(styles.seeMore, styles.text)}
+          onClick={handleSeeMoreClick}
+        >
+          See {shouldShowFullText ? "less" : "more"}
+        </a>
       )}
     </div>
   );
