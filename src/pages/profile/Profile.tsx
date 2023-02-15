4a3e4819f0e1224eb21f933940187f00fcae0bb7
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-02-15 17:39:07 +0100

    wrap profile page in container

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from "react";
 import { Profile } from "@/pages/MyAccount/components/Profile";
+import { Container } from "@/shared/ui-kit";
+import styles from "./Profile.module.scss";
 
 const ProfilePage: FC = () => {
-  return <Profile />;
+  return (
+    <Container className={styles.container}>
+      <Profile />
+    </Container>
+  );
 };
 
 export default ProfilePage;
