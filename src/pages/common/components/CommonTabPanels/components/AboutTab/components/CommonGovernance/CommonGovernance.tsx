b23257908e83e273ed927ad77ab0c019b27d3820
Roie Natan <roie.natan@gmail.com>
2023-02-16 09:58:34 +0530

    added link option for governance title in common about

--- src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonGovernance/CommonGovernance.tsx
+++ src/pages/common/components/CommonTabPanels/components/AboutTab/components/CommonGovernance/CommonGovernance.tsx
@@ -1,16 +1,19 @@
 import React, { FC } from "react";
+import classNames from "classnames";
 import { ViewportBreakpointVariant } from "@/shared/constants";
 import { useIsTabletView } from "@/shared/hooks/viewport";
+import NewTabIcon from "@/shared/icons/newtab.icon";
 import { Container } from "@/shared/ui-kit";
 import { CommonCard } from "../../../../../CommonCard";
 import styles from "./CommonGovernance.module.scss";
 
 interface CommonGovernanceProps {
   commonName: string;
+  titleUrl?: string;
 }
 
 const CommonGovernance: FC<CommonGovernanceProps> = (props) => {
-  const { commonName } = props;
+  const { commonName, titleUrl } = props;
   const isTabletView = useIsTabletView();
 
   return (
@@ -22,7 +25,12 @@ const CommonGovernance: FC<CommonGovernanceProps> = (props) => {
       ]}
     >
       <CommonCard hideCardStyles={isTabletView}>
-        <h3 className={styles.title}>Governance</h3>
+        <h3
+          className={classNames(styles.title, { [styles.link]: titleUrl })}
+          onClick={() => titleUrl && window.open(titleUrl)}
+        >
+          Governance {titleUrl && <NewTabIcon className={styles.newTabIcon} />}
+        </h3>
         <p className={styles.description}>
           The various permissions for each circle in{" "}
           <strong>{commonName}</strong>, in terms of proposing and voting on
