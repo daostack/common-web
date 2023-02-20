249dbf2eadfd9006ae21b60e6125d0df13f7a694
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-02-20 11:48:14 +0800

    move logic of common for editing fetch to the page inself

--- src/pages/commonCreation/CommonCreation.tsx
+++ src/pages/commonCreation/CommonCreation.tsx
@@ -1,58 +1,26 @@
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 import { useParams } from "react-router-dom";
-import { useCommon } from "@/shared/hooks/useCases";
-import { Loader } from "@/shared/ui-kit";
+import { Common } from "@/shared/models";
 import { checkIsProject } from "@/shared/utils";
-import { CenterWrapper } from "./components";
 import { ProjectCreation } from "./components";
-import styles from "./CommonCreation.module.scss";
 
 interface CommonCreationRouterParams {
   id?: string;
 }
 
 interface CommonCreationProps {
-  isEditing?: boolean;
+  initialCommon?: Common;
 }
 
 const CommonCreation: FC<CommonCreationProps> = (props) => {
-  const { isEditing = false } = props;
+  const { initialCommon } = props;
   const { id: commonId } = useParams<CommonCreationRouterParams>();
-  const {
-    data: common,
-    fetched: isCommonFetched,
-    fetchCommon,
-    setCommon,
-  } = useCommon();
 
-  useEffect(() => {
-    if (commonId) {
-      fetchCommon(commonId);
-    } else {
-      setCommon(null);
-    }
-  }, [commonId]);
-
-  if (isEditing) {
-    if (!isCommonFetched) {
-      return (
-        <CenterWrapper>
-          <Loader />
-        </CenterWrapper>
-      );
-    }
-    if (!common) {
-      return (
-        <CenterWrapper>
-          <p className={styles.dataErrorText}>Common does not exist</p>
-        </CenterWrapper>
-      );
-    }
-
-    return checkIsProject(common) ? (
+  if (initialCommon) {
+    return checkIsProject(initialCommon) ? (
       <ProjectCreation
-        initialCommon={common}
-        parentCommonId={common.directParent.commonId}
+        initialCommon={initialCommon}
+        parentCommonId={initialCommon.directParent.commonId}
       />
     ) : null;
   }
--- src/pages/commonEditing/CommonEditing.tsx
+++ src/pages/commonEditing/CommonEditing.tsx
@@ -1,8 +1,47 @@
-import React from "react";
-import { CommonCreationPage } from "../commonCreation";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useCommon } from "@/shared/hooks/useCases";
+import { Loader } from "@/shared/ui-kit";
+import { CommonCreationPage, CenterWrapper } from "../commonCreation";
+import styles from "./CommonEditing.module.scss";
+
+interface CommonEditingRouterParams {
+  id: string;
+}
 
 const CommonEditing = () => {
-  return <CommonCreationPage isEditing />;
+  const { id: commonId } = useParams<CommonEditingRouterParams>();
+  const {
+    data: common,
+    fetched: isCommonFetched,
+    fetchCommon,
+    setCommon,
+  } = useCommon();
+
+  useEffect(() => {
+    if (commonId) {
+      fetchCommon(commonId);
+    } else {
+      setCommon(null);
+    }
+  }, [commonId]);
+
+  if (!isCommonFetched) {
+    return (
+      <CenterWrapper>
+        <Loader />
+      </CenterWrapper>
+    );
+  }
+  if (!common) {
+    return (
+      <CenterWrapper>
+        <p className={styles.dataErrorText}>Common does not exist</p>
+      </CenterWrapper>
+    );
+  }
+
+  return <CommonCreationPage initialCommon={common} />;
 };
 
 export default CommonEditing;
