5bb49e7a5dc0310ce57afaa129c861e8680a5d42
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-02-20 12:04:37 +0800

    update project creation page to have exports of some inner logic

--- src/pages/commonCreation/components/ProjectCreation/components/ProjectCreationForm/index.ts
+++ src/pages/commonCreation/components/ProjectCreation/components/ProjectCreationForm/index.ts
@@ -1,1 +1,4 @@
 export { default as ProjectCreationForm } from "./ProjectCreationForm";
+export { default as styles } from "./ProjectCreationForm.module.scss";
+export * from "./configuration";
+export * from "./types";
