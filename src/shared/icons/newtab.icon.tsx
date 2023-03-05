f875464414ea81c769f11656e88c601cf53464c8
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-03-05 13:16:51 +0800

    fix svg props of new tab icon

--- src/shared/icons/newtab.icon.tsx
+++ src/shared/icons/newtab.icon.tsx
@@ -19,9 +19,9 @@ const NewTabIcon: FC<NewTabIconProps> = ({ className }) => {
       <path
         d="M12.5 9.5V13.25C12.5 13.6478 12.342 14.0294 12.0607 14.3107C11.7794 14.592 11.3978 14.75 11 14.75H2.75C2.35218 14.75 1.97064 14.592 1.68934 14.3107C1.40804 14.0294 1.25 13.6478 1.25 13.25V5C1.25 4.175 1.925 3.5 2.75 3.5H6.5M10.25 1.25H14.75V5.75M6.5 9.5L14.15 1.85"
         stroke={color}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
