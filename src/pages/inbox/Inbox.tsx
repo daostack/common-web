3a71989f4af688c8856c5ed786054e60c5db7986
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-03-20 16:36:37 +0800

    remove react fragment from inbox page

--- src/pages/inbox/Inbox.tsx
+++ src/pages/inbox/Inbox.tsx
@@ -3,11 +3,7 @@ import { CommonSidenavLayoutTabs } from "@/shared/layouts";
 import styles from "./Inbox.module.scss";
 
 const InboxPage: FC = () => {
-  return (
-    <>
-      <CommonSidenavLayoutTabs className={styles.tabs} />
-    </>
-  );
+  return <CommonSidenavLayoutTabs className={styles.tabs} />;
 };
 
 export default InboxPage;
