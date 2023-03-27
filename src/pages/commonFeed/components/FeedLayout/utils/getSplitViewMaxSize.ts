6d619ed2205983c0d0b3e6be3efd525a1f94b13b
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-03-27 17:15:43 +0800

    update max size calculation

--- src/pages/commonFeed/components/FeedLayout/utils/getSplitViewMaxSize.ts
+++ src/pages/commonFeed/components/FeedLayout/utils/getSplitViewMaxSize.ts
@@ -5,23 +5,23 @@ export const getSplitViewMaxSize = (width: number): number => {
     return MIN_CHAT_WIDTH;
   }
   if (width < 1180) {
-    return 420;
+    return 524;
   }
   if (width < 1270) {
-    return 450;
+    return 550;
   }
   if (width < 1360) {
-    return 420;
+    return 640;
   }
   if (width < 1460) {
-    return 470;
+    return 730;
   }
   if (width < 1620) {
-    return 600;
+    return 830;
   }
   if (width < 1800) {
-    return 750;
+    return 990;
   }
 
-  return 940;
+  return 1170;
 };
