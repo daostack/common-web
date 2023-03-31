a68236420f1e086b7d10ac0183d9eb5daa7f4930
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-03-31 15:28:17 +0800

    fix page content header on feed layout

--- src/shared/layouts/CommonSidenavLayout/components/PageContent/PageContent.tsx
+++ src/shared/layouts/CommonSidenavLayout/components/PageContent/PageContent.tsx
@@ -1,22 +1,41 @@
-import React, { cloneElement, FC, isValidElement, ReactNode } from "react";
+import React, {
+  cloneElement,
+  CSSProperties,
+  FC,
+  isValidElement,
+  ReactNode,
+} from "react";
 import classNames from "classnames";
 import { Loader, LoaderVariant } from "@/shared/ui-kit";
 import styles from "./PageContent.module.scss";
 
 interface FeedLayoutProps {
   className?: string;
+  headerClassName?: string;
   headerContent: ReactNode;
   isGlobalLoading?: boolean;
+  styles?: CSSProperties;
 }
 
 const PageContent: FC<FeedLayoutProps> = (props) => {
-  const { className, headerContent, children, isGlobalLoading = false } = props;
+  const {
+    className,
+    headerClassName,
+    headerContent,
+    children,
+    isGlobalLoading = false,
+    styles: pageContentStyles,
+  } = props;
   let headerEl: ReactNode = null;
 
   if (headerContent && isValidElement(headerContent)) {
     headerEl = cloneElement(headerContent, {
       ...headerContent.props,
-      className: classNames(styles.header, headerContent.props.className),
+      className: classNames(
+        styles.header,
+        headerContent.props.className,
+        headerClassName,
+      ),
     });
   }
 
@@ -36,6 +55,7 @@ const PageContent: FC<FeedLayoutProps> = (props) => {
           },
           className,
         )}
+        style={pageContentStyles}
       >
         {headerEl}
         {children}
