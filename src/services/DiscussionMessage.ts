75009a309ffc196b7f33f01bbc0c3ff089fb800c
Pavel Meyer <meyerpavel1207@gmail.com>
2023-04-04 13:29:41 +0300

    CW-1359 Refactoring

--- src/services/DiscussionMessage.ts
+++ src/services/DiscussionMessage.ts
@@ -9,15 +9,10 @@ import firebase from "@/shared/utils/firebase";
 import { ApiEndpoint } from "@/shared/constants";
 import { CreateDiscussionMessageDto } from "@/shared/interfaces/api/discussionMessages";
 import { Api } from ".";
-import { throttle } from "lodash";
 
 const converter = firestoreDataConverter<DiscussionMessage>();
 
 class DiscussionMessageService {
-  // constructor() {
-  //   this.createMessage = throttle(this.createMessage, 1000);
-  // }
-  
   private getDiscussionMessageCollection = () =>
     firebase
       .firestore()
@@ -38,22 +33,9 @@ class DiscussionMessageService {
     });
   };
 
-  // public createMessage = throttle(async (
-  //   payload: CreateDiscussionMessageDto,
-  // ): Promise<DiscussionMessage>  => {
-  //   console.log('---payload',payload.text);
-  //   const { data } = await Api.post<DiscussionMessage>(
-  //     ApiEndpoint.CreateDiscussionMessage,
-  //     payload,
-  //   );
-  
-  //   return convertObjectDatesToFirestoreTimestamps(data);
-  // },800)
-
   public createMessage = async (
     payload: CreateDiscussionMessageDto,
   ): Promise<DiscussionMessage>  => {
-    console.log('---payload',payload.text);
     const { data } = await Api.post<DiscussionMessage>(
       ApiEndpoint.CreateDiscussionMessage,
       payload,
