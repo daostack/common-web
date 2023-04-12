a6ed5d113be78592e420269e6c0c6cb37ecafc57
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-04-12 14:48:45 +0800

    create inbox page header content

---
+++ src/pages/inbox/components/HeaderContent/HeaderContent.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import classNames from "classnames";
+import { InboxIcon, RightArrowThinIcon } from "@/shared/icons";
+import { TopNavigationOpenSidenavButton } from "@/shared/ui-kit";
+import styles from "./HeaderContent.module.scss";
+
+interface HeaderContentProps {
+  className?: string;
+  newMessagesAmount: number;
+}
+
+const HeaderContent: FC<HeaderContentProps> = (props) => {
+  const { className, newMessagesAmount } = props;
+
+  return (
+    <div className={classNames(styles.container, className)}>
+      <div className={styles.content}>
+        <TopNavigationOpenSidenavButton
+          className={styles.openSidenavButton}
+          iconEl={<RightArrowThinIcon className={styles.openSidenavIcon} />}
+        />
+        <div className={styles.infoContainer}>
+          <InboxIcon className={styles.inboxIcon} />
+          <div className={styles.infoWrapper}>
+            <h1 className={styles.title}>Inbox</h1>
+            <p className={styles.newMessagesAmount}>
+              {newMessagesAmount} new message
+              {newMessagesAmount === 1 ? "" : "s"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContent;
