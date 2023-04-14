52be05e3409072cb7881f3e1506d3d4aa70253d3
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-04-14 14:53:23 +0800

    create getLastMessage util for inbox page

--- src/pages/inbox/Inbox.tsx
+++ src/pages/inbox/Inbox.tsx
@@ -16,6 +16,7 @@ import { Loader, NotFound, PureCommonTopNavigation } from "@/shared/ui-kit";
 import { inboxActions, selectSharedInboxItem } from "@/store/states";
 import { HeaderContent, FeedItemBaseContent } from "./components";
 import { useInboxData } from "./hooks";
+import { getLastMessage } from "./utils";
 import styles from "./Inbox.module.scss";
 
 const InboxPage: FC = () => {
@@ -155,6 +156,7 @@ const InboxPage: FC = () => {
         onFetchNext={fetchMoreInboxItems}
         renderFeedItemBaseContent={renderFeedItemBaseContent}
         onFeedItemUpdate={handleFeedItemUpdate}
+        getLastMessage={getLastMessage}
       />
       <CommonSidenavLayoutTabs className={styles.tabs} />
     </>
