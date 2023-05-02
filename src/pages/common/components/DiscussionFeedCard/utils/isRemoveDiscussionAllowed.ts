183450b030b29f91951ee10629779a636eedfeac
Daniel Reinoso <danielreinoso1807@gmail.com>
2023-05-01 23:09:15 -0400

    Allow owner to delete their own discussions

--- src/pages/common/components/DiscussionFeedCard/utils/isRemoveDiscussionAllowed.ts
+++ src/pages/common/components/DiscussionFeedCard/utils/isRemoveDiscussionAllowed.ts
@@ -1,28 +1,37 @@
 import { GovernanceActions } from "@/shared/constants";
+import { PredefinedTypes } from "@/shared/models";
 import { checkIsCountdownState, hasPermission } from "@/shared/utils";
 import { GetAllowedItemsOptions } from "./shared";
 
 export function isRemoveDiscussionAllowed(options: GetAllowedItemsOptions) {
   if (!options.commonMember) return false;
-  let isAllowed = hasPermission({
-    commonMember: options.commonMember,
-    governance: {
-      circles: options.governanceCircles || {},
-    },
-    key: GovernanceActions.HIDE_OR_UNHIDE_DISCUSSION,
-  });
+  if (options.discussion?.predefinedType === PredefinedTypes.General)
+    return false;
+
+  const hasPermissionToRemoveDiscussion =
+    hasPermission({
+      commonMember: options.commonMember,
+      governance: {
+        circles: options.governanceCircles || {},
+      },
+      key: GovernanceActions.HIDE_OR_UNHIDE_DISCUSSION,
+    }) || options.commonMember.userId === options.discussion?.ownerId;
+
+  let isAllowed = hasPermissionToRemoveDiscussion;
   if (options.discussion?.proposalId && isAllowed) {
     const { proposal } = options;
-    isAllowed =
-      !!proposal &&
-      checkIsCountdownState({ state: proposal.state }) &&
+    const hasPermissionToRemoveProposal =
       hasPermission({
         commonMember: options.commonMember,
         governance: {
           circles: options.governanceCircles || {},
         },
         key: GovernanceActions.HIDE_OR_UNHIDE_PROPOSAL,
-      });
+      }) || options.commonMember.userId === options.discussion?.ownerId;
+    isAllowed =
+      !!proposal &&
+      checkIsCountdownState({ state: proposal.state }) &&
+      hasPermissionToRemoveProposal;
   }
   return isAllowed;
 }
