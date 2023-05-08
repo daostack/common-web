7e3dd4d075398557b60d7712647a8646cb085eed
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-05-08 17:49:39 +0800

    move ids for not listening to ref in common feed items use-case
    Since now idsForNotListening may change more often, due to
    references changes in the parent, so to avoid unnecessary
    re-subscriptions I decided to make so that the logic will not
    depend on idsForNotListening variable.

--- src/shared/hooks/useCases/useCommonFeedItems.ts
+++ src/shared/hooks/useCases/useCommonFeedItems.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CommonFeedService } from "@/services";
 import { commonActions, FeedItems, selectFeedItems } from "@/store/states";
@@ -13,6 +13,7 @@ export const useCommonFeedItems = (
 ): Return => {
   const dispatch = useDispatch();
   const feedItems = useSelector(selectFeedItems);
+  const idsForNotListeningRef = useRef<string[]>(idsForNotListening || []);
 
   const fetch = () => {
     dispatch(
@@ -22,6 +23,10 @@ export const useCommonFeedItems = (
       }),
     );
   };
+
+  useEffect(() => {
+    idsForNotListeningRef.current = idsForNotListening || [];
+  }, [idsForNotListening]);
 
   useEffect(() => {
     if (!feedItems.firstDocTimestamp) {
@@ -39,9 +44,12 @@ export const useCommonFeedItems = (
         }
 
         const finalData =
-          idsForNotListening && idsForNotListening.length > 0
+          idsForNotListeningRef.current.length > 0
             ? data.filter(
-                (item) => !idsForNotListening.includes(item.commonFeedItem.id),
+                (item) =>
+                  !idsForNotListeningRef.current.includes(
+                    item.commonFeedItem.id,
+                  ),
               )
             : data;
 
@@ -50,7 +58,7 @@ export const useCommonFeedItems = (
     );
 
     return unsubscribe;
-  }, [feedItems.firstDocTimestamp, commonId, idsForNotListening]);
+  }, [feedItems.firstDocTimestamp, commonId]);
 
   return {
     ...feedItems,
