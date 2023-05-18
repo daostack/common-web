33a84593782f14d3bbf8326ad94bc03c67057e0a
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-05-18 16:52:15 +0300

    add logic to save chat width to local storage

--- src/pages/commonFeed/components/FeedLayout/utils/getDefaultSize.ts
+++ src/pages/commonFeed/components/FeedLayout/utils/getDefaultSize.ts
@@ -1,4 +1,5 @@
 import { MIN_CHAT_WIDTH } from "../constants";
+import { getSavedChatSize } from "./chatSize";
 
 export const getDefaultSize = (
   windowWidth: number,
@@ -6,7 +7,9 @@ export const getDefaultSize = (
 ): number => {
   const pageWidth = Math.min(windowWidth, 1920);
   const sidenavWidth = 336;
-  const chatWidth = Math.floor((pageWidth - sidenavWidth) * 0.6);
+  const savedChatSize = getSavedChatSize();
+  const chatWidth =
+    savedChatSize || Math.floor((pageWidth - sidenavWidth) * 0.6);
 
   return Math.max(Math.min(maxChatSize, chatWidth), MIN_CHAT_WIDTH);
 };
