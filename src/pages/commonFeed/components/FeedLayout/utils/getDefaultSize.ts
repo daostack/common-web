b307a4c64ab1f71aaf7ff014cb60864750f9f7f1
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-05-18 16:42:58 +0300

    make chat width to be 60% by default

--- src/pages/commonFeed/components/FeedLayout/utils/getDefaultSize.ts
+++ src/pages/commonFeed/components/FeedLayout/utils/getDefaultSize.ts
@@ -6,7 +6,7 @@ export const getDefaultSize = (
 ): number => {
   const pageWidth = Math.min(windowWidth, 1920);
   const sidenavWidth = 336;
-  const chatWidth = Math.floor((pageWidth - sidenavWidth) / 2);
+  const chatWidth = Math.floor((pageWidth - sidenavWidth) * 0.6);
 
   return Math.max(Math.min(maxChatSize, chatWidth), MIN_CHAT_WIDTH);
 };
