1efe7a94e4ffe7a03e7b1c252a4db509aa4ea5f7
Roie Natan <roie.natan@gmail.com>
2023-05-18 18:33:52 +0800

    init

---
+++ src/pages/common/components/DiscussionFeedCard/utils/checkIsEditItemAllowed.ts
@@ -0,0 +1,10 @@
+import { PredefinedTypes } from "@/shared/models";
+import { GetAllowedItemsOptions } from "../../FeedItem";
+
+export function checkIsEditItemAllowed(options: GetAllowedItemsOptions) {
+  if (!options.commonMember) return false;
+  if (options.discussion?.predefinedType === PredefinedTypes.General)
+    return false;
+
+  return options.commonMember.userId === options.discussion?.ownerId;
+}
