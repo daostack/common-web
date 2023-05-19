716018821f8e0a40241298e29dba471865ce3d46
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-05-19 17:41:16 +0300

    fix dm users fetch

--- src/services/Chat.ts
+++ src/services/Chat.ts
@@ -5,11 +5,11 @@ import Api from "./Api";
 
 class ChatService {
   public getDMUsers = async (): Promise<DMUser[]> => {
-    const { data } = await Api.get<Omit<DMUser, "userName">[]>(
+    const { data } = await Api.get<{ data: Omit<DMUser, "userName">[] }>(
       ApiEndpoint.GetDMUsers,
     );
 
-    return data.map((item) => ({
+    return data.data.map((item) => ({
       ...item,
       userName: getUserName(item),
     }));
