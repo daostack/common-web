d1f3c925049370cd9741c7c3974adc96c01594f8
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-05-23 15:55:41 +0300

    update FeedLayoutItem type to have chat channels

--- src/pages/commonFeed/components/FeedLayout/types.ts
+++ src/pages/commonFeed/components/FeedLayout/types.ts
@@ -1,15 +1,36 @@
-import { CommonFeed, FeedItemFollowWithMetadata } from "@/shared/models";
+import { InboxItemType } from "@/shared/constants";
+import {
+  ChatChannel,
+  CommonFeed,
+  FeedItemFollowWithMetadata,
+} from "@/shared/models";
 
 export interface FeedLayoutRef {
   setExpandedFeedItemId: (feedItemId: string | null) => void;
 }
 
-export interface FeedLayoutItem {
+interface BaseLayoutItem {
+  type?: InboxItemType;
+  itemId: string;
+}
+
+interface ChatChannelLayoutItem extends BaseLayoutItem {
+  type: InboxItemType.ChatChannel;
+  chatChannel: ChatChannel;
+}
+
+interface FeedItemFollowLayoutItem extends BaseLayoutItem {
+  type?: InboxItemType.FeedItemFollow;
   feedItem: CommonFeed;
   feedItemFollowWithMetadata?: FeedItemFollowWithMetadata;
 }
 
-export interface FeedLayoutItemWithFollowData
-  extends Omit<FeedLayoutItem, "feedItemFollowWithMetadata"> {
+export interface FeedItemFollowLayoutItemWithFollowData
+  extends Omit<FeedItemFollowLayoutItem, "feedItemFollowWithMetadata"> {
   feedItemFollowWithMetadata: FeedItemFollowWithMetadata;
 }
+
+export type FeedLayoutItem = FeedItemFollowLayoutItem | ChatChannelLayoutItem;
+export type FeedLayoutItemWithFollowData =
+  | FeedItemFollowLayoutItemWithFollowData
+  | ChatChannelLayoutItem;
