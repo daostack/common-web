22ab7b186decba88b6f73ff12fd467cca322ad43
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-05-31 17:22:01 +0300

    create converter for chat channel to discussion

---
+++ src/shared/converters/ChatChannelToDiscussionConverter.ts
@@ -0,0 +1,36 @@
+import { ChatChannel, Discussion, Timestamp } from "@/shared/models";
+import { Converter } from "./Converter";
+
+class ChatChannelToDiscussionConverter extends Converter<
+  ChatChannel,
+  Discussion
+> {
+  public toTargetEntity = (chatChannel: ChatChannel): Discussion => ({
+    id: chatChannel.id,
+    title: chatChannel.name,
+    message: chatChannel.description,
+    ownerId: chatChannel.createdBy,
+    commonId: "",
+    lastMessage: chatChannel.lastMessage?.createdAt || Timestamp.now(),
+    files: [],
+    images: [],
+    followers: [],
+    messageCount: chatChannel.messageCount,
+    discussionMessages: [],
+    createdAt: chatChannel.createdAt,
+    updatedAt: chatChannel.updatedAt,
+  });
+
+  public toBaseEntity = (discussion: Discussion): ChatChannel => ({
+    id: discussion.id,
+    name: discussion.title,
+    description: discussion.message,
+    participants: [],
+    createdBy: discussion.ownerId,
+    messageCount: discussion.messageCount,
+    createdAt: discussion.createdAt,
+    updatedAt: discussion.updatedAt,
+  });
+}
+
+export default new ChatChannelToDiscussionConverter();
