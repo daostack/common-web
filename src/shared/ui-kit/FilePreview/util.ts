c4484ba79dbea729762533414b03f5a3a83d175a
Pavel Meyer <meyerpavel1207@gmail.com>
2023-06-19 13:59:46 +0300

    CW-1690 Fixed PR comments

--- src/shared/ui-kit/FilePreview/util.ts
+++ src/shared/ui-kit/FilePreview/util.ts
@@ -1,5 +1,7 @@
 export const getFileName = (name: string, limit = 14) => {
-  const [fileName, extension] = name.split(".");
+  const fileNameParts = name.trim().split(".");
+  const fileName = fileNameParts.slice(0, -1).join(".");
+  const extension = fileNameParts.pop();
 
   if (fileName.length > limit - extension.length) {
     return `${fileName.slice(0, limit)}...${extension}`;
