5254c119f2e3a7d5b52cd55444990b76993a9717
Pavel Meyer <meyerpavel1207@gmail.com>
2023-06-19 14:14:24 +0300

    CW-1690 Fixed eslint

--- src/shared/ui-kit/FilePreview/util.ts
+++ src/shared/ui-kit/FilePreview/util.ts
@@ -1,7 +1,7 @@
 export const getFileName = (name: string, limit = 14) => {
   const fileNameParts = name.trim().split(".");
   const fileName = fileNameParts.slice(0, -1).join(".");
-  const extension = fileNameParts.pop();
+  const extension = fileNameParts.pop() ?? "";
 
   if (fileName.length > limit - extension.length) {
     return `${fileName.slice(0, limit)}...${extension}`;
