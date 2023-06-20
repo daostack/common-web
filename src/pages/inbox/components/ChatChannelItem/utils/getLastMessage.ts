046a13f004774ac1c19e17f1d907f03e68babe97
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-06-20 13:26:39 +0300

    add file icon to the chat channel last message

---
+++ src/pages/inbox/components/ChatChannelItem/utils/getLastMessage.ts
@@ -0,0 +1,28 @@
+import { getLastMessageIconWithText } from "@/pages/commonFeed/utils";
+import { ChatChannel } from "@/shared/models";
+import {
+  checkIsTextEditorValueEmpty,
+  parseStringToTextEditorValue,
+  prependTextInTextEditorValue,
+  TextEditorValue,
+} from "@/shared/ui-kit";
+
+export const getLastMessage = (
+  lastMessage: ChatChannel["lastMessage"],
+): TextEditorValue | undefined => {
+  if (!lastMessage?.text) {
+    return;
+  }
+
+  const parsedMessageContent = parseStringToTextEditorValue(lastMessage.text);
+  const hasText = !checkIsTextEditorValueEmpty(parsedMessageContent);
+
+  return prependTextInTextEditorValue(
+    getLastMessageIconWithText({
+      hasText,
+      hasImages: lastMessage.hasImages,
+      hasFiles: lastMessage.hasFiles,
+    }),
+    parsedMessageContent,
+  );
+};
