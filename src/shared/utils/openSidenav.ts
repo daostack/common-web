2cfcf041e84c4cbb3cd3e64e264c32a1aa75b651
Pavel Meyer <meyerpavel1207@gmail.com>
2023-06-26 12:15:28 +0300

    CW-1764 Fix PR comments

--- src/shared/utils/openSidenav.ts
+++ src/shared/utils/openSidenav.ts
@@ -1,10 +1,16 @@
+import queryString from "query-string";
 import { history } from "@/shared/appConfig";
 import { SIDENAV_KEY, SIDENAV_OPEN } from "@/shared/constants";
-import { getInboxPagePath } from "@/shared/utils";
 
 export const openSidenav = () => {
+  const params = queryString.parse(window.location.search);
+  
+  const search = queryString.stringify({
+    ...params,
+    [SIDENAV_KEY]: SIDENAV_OPEN,
+  });
   history.push({
-    pathname: getInboxPagePath(),
-    search: `?${SIDENAV_KEY}=${SIDENAV_OPEN}`,
+    pathname: window.location.pathname,
+    search,
   });
 }
