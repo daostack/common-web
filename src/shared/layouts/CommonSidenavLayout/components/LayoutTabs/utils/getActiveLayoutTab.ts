7affa2f664a9e3de809104dba9774e81dd841d67
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-06-26 14:17:59 +0300

    update active layout tab util for different page versions

--- src/shared/layouts/CommonSidenavLayout/components/LayoutTabs/utils/getActiveLayoutTab.ts
+++ src/shared/layouts/CommonSidenavLayout/components/LayoutTabs/utils/getActiveLayoutTab.ts
@@ -1,15 +1,19 @@
 import { ROUTE_PATHS } from "@/shared/constants";
-import { matchRoute } from "@/shared/utils";
+import { matchOneOfRoutes, matchRoute } from "@/shared/utils";
 import { LayoutTab } from "../../../constants";
 
 export const getActiveLayoutTab = (path: string): LayoutTab | null => {
   if (matchRoute(path, ROUTE_PATHS.PROFILE)) {
     return LayoutTab.Profile;
   }
-  if (matchRoute(path, ROUTE_PATHS.V04_INBOX)) {
+  if (matchOneOfRoutes(path, [ROUTE_PATHS.INBOX, ROUTE_PATHS.V04_INBOX])) {
     return LayoutTab.Inbox;
   }
-  if (matchRoute(path, ROUTE_PATHS.V04_COMMON, { exact: false })) {
+  if (
+    matchOneOfRoutes(path, [ROUTE_PATHS.COMMON, ROUTE_PATHS.V04_COMMON], {
+      exact: false,
+    })
+  ) {
     return LayoutTab.Spaces;
   }
 
