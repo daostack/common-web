6ea20e6b40e27bd02e9d57b39686ae205dba8974
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-06-30 17:49:47 +0300

    add common back page to multiple spaces layout

--- src/pages/App/router/configuration/multipleSpacesLayout.tsx
+++ src/pages/App/router/configuration/multipleSpacesLayout.tsx
@@ -1,7 +1,15 @@
+import { ALL_COMMON_PAGE_TABS, CommonPage } from "@/pages/common";
 import { CommonFeedPage } from "@/pages/commonFeed";
 import { ROUTE_PATHS } from "@/shared/constants";
 import { MultipleSpacesLayout } from "@/shared/layouts";
 import { LayoutConfiguration } from "../types";
+
+const getCommonPageConfiguration = (): LayoutConfiguration["routes"] =>
+  ALL_COMMON_PAGE_TABS.map((tab) => ({
+    path: `${ROUTE_PATHS.COMMON}/${tab}` as ROUTE_PATHS,
+    exact: true,
+    component: CommonPage,
+  }));
 
 export const MULTIPLE_SPACES_LAYOUT_CONFIGURATION: LayoutConfiguration = {
   component: MultipleSpacesLayout,
@@ -11,5 +19,6 @@ export const MULTIPLE_SPACES_LAYOUT_CONFIGURATION: LayoutConfiguration = {
       exact: true,
       component: CommonFeedPage,
     },
+    ...getCommonPageConfiguration(),
   ],
 };
--- src/pages/common/Common.tsx
+++ src/pages/common/Common.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { MainRoutesProvider } from "@/shared/contexts";
 import BaseCommonPage from "./BaseCommon";
+import { CommonPageSettings } from "./types";
+import styles from "./Common.module.scss";
+
+const SETTINGS: CommonPageSettings = {
+  pageContentClassName: styles.mainPageContent,
+  renderHeaderContent: () => null,
+};
 
 const Common = () => (
   <MainRoutesProvider>
-    <BaseCommonPage />
+    <BaseCommonPage settings={SETTINGS} />
   </MainRoutesProvider>
 );
 
