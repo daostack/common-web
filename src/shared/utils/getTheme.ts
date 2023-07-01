6d553b025956c4b530a050c0dd08849204056f3f
Roie Natan <roie.natan@gmail.com>
2023-07-02 10:58:45 +1300

    updated getTheme logic

--- src/shared/utils/getTheme.ts
+++ src/shared/utils/getTheme.ts
@@ -2,8 +2,8 @@ import { StorageKey } from "@/shared/constants";
 import { Theme } from "../constants/theme";
 
 export const getTheme = () => {
-  const theme = localStorage.getItem(StorageKey.Theme) as Theme;
-  if (Object.values(Theme).includes(theme)) {
+  const theme = localStorage.getItem(StorageKey.Theme) as Theme | null;
+  if (theme && Object.values(Theme).includes(theme)) {
     return theme;
   }
 
