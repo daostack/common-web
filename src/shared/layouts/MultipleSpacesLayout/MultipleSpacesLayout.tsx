6726453d19d4e6ad74c9ff3b12eb35227bf9978c
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-07-07 14:03:19 +0300

    remove sidenav related logic from multiple spaces layout

--- src/shared/layouts/MultipleSpacesLayout/MultipleSpacesLayout.tsx
+++ src/shared/layouts/MultipleSpacesLayout/MultipleSpacesLayout.tsx
@@ -1,10 +1,7 @@
-import React, { CSSProperties, FC, useCallback } from "react";
+import React, { CSSProperties, FC } from "react";
 import { useWindowSize } from "react-use";
 import { MainRoutesProvider } from "@/shared/contexts";
-import { useLockedBody } from "@/shared/hooks";
 import { useIsTabletView } from "@/shared/hooks/viewport";
-import { Sidenav } from "@/shared/ui-kit";
-import { SidenavContent } from "../CommonSidenavLayout/components";
 import { getSidenavLeft } from "../CommonSidenavLayout/utils";
 import { Header } from "./components";
 import styles from "./MultipleSpacesLayout.module.scss";
@@ -13,34 +10,15 @@ const MultipleSpacesLayout: FC = (props) => {
   const { children } = props;
   const isTabletView = useIsTabletView();
   const { width } = useWindowSize();
-  const { lockBodyScroll, unlockBodyScroll } = useLockedBody();
   const sidenavLeft = getSidenavLeft(width);
   const style = {
     "--sb-h-indent": `${sidenavLeft}px`,
   } as CSSProperties;
 
-  const handleSidenavOpenToggle = useCallback(
-    (isOpen: boolean) => {
-      if (isOpen) {
-        lockBodyScroll();
-      } else {
-        unlockBodyScroll();
-      }
-    },
-    [lockBodyScroll, unlockBodyScroll],
-  );
-
   return (
     <MainRoutesProvider>
       <div className={styles.container} style={style}>
-        {isTabletView && (
-          <Sidenav
-            contentWrapperClassName={styles.sidenavContentWrapper}
-            onOpenToggle={handleSidenavOpenToggle}
-          >
-            <SidenavContent className={styles.sidenavContent} />
-          </Sidenav>
-        )}
+        {isTabletView && <div>Sidenav</div>}
         <main className={styles.main}>
           {!isTabletView && <Header />}
           {children}
