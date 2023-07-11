3b2748b75141bf06d1bf53f24288ed31d5a11641
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-07-11 15:02:56 +0300

    add icons to feed item context menu items

---
+++ src/shared/icons/unfollow.icon.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react";
+
+interface UnfollowIconProps {
+  className?: string;
+}
+
+export const UnfollowIcon: FC<UnfollowIconProps> = ({ className }) => {
+  const color = "currentColor";
+
+  return (
+    <svg
+      className={className}
+      width="20"
+      height="19"
+      viewBox="0 0 20 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 1L12.781 6.634L19 7.543L14.5 11.926L15.562 18.118L10 15.193L4.438 18.118L5.5 11.926L1 7.543L7.219 6.634L10 1Z"
+        fill="#FFC23D"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
