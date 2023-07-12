d5ad8e16c1e6818e16257adb96523e59b02354df
Roie Natan <roie.natan@gmail.com>
2023-07-12 20:17:14 +1300

    show all circle options in first dropdown - eliminate CircleSelectionModal

--- src/pages/common/components/CommonTabPanels/components/FeedTab/components/NewDiscussionCreation/components/CircleSelectionModal/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CircleSelectionModal } from "./CircleSelectionModal";
--- src/pages/common/components/CommonTabPanels/components/FeedTab/components/NewDiscussionCreation/components/CircleSelectionModal/utils/getCircleNamesAsString.ts
+++
@@ -1,9 +0,0 @@
-export const getCircleNamesAsString = (circleNames: string[]): string => {
-  if (circleNames.length <= 2) {
-    return circleNames.join(" and ");
-  }
-
-  return `${circleNames.slice(0, -1).join(", ")} and ${
-    circleNames[circleNames.length - 1]
-  }`;
-};
