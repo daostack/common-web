5c6347b80f836f02f28d48948f38ddab95f31c70
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-07-18 11:27:51 +0300

    add dm button to inbox header

--- src/pages/inbox/components/DirectMessageButton/DirectMessageButton.tsx
+++ src/pages/inbox/components/DirectMessageButton/DirectMessageButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { ComponentType, FC } from "react";
 import { useModal } from "@/shared/hooks";
 import { Message2Icon } from "@/shared/icons";
 import { Button, ButtonIcon, ButtonSize, ButtonVariant } from "@/shared/ui-kit";
@@ -8,10 +8,15 @@ import styles from "./DirectMessageButton.module.scss";
 interface DirectMessageButtonProps {
   className?: string;
   isMobileVersion?: boolean;
+  ButtonComponent?: ComponentType<{
+    className?: string;
+    isMobileVersion?: boolean;
+    onClick?: () => void;
+  }>;
 }
 
 const DirectMessageButton: FC<DirectMessageButtonProps> = (props) => {
-  const { className, isMobileVersion = false } = props;
+  const { className, isMobileVersion = false, ButtonComponent } = props;
   const {
     isShowing: isDirectMessageModalOpen,
     onOpen: onDirectMessageModalOpen,
@@ -19,25 +24,33 @@ const DirectMessageButton: FC<DirectMessageButtonProps> = (props) => {
   } = useModal(false);
   const buttonVariant = ButtonVariant.OutlineDarkPink;
   const iconEl = <Message2Icon className={styles.icon} />;
-  const buttonEl = isMobileVersion ? (
-    <ButtonIcon
-      className={className}
-      variant={buttonVariant}
-      onClick={onDirectMessageModalOpen}
-    >
-      {iconEl}
-    </ButtonIcon>
-  ) : (
-    <Button
-      className={className}
-      variant={buttonVariant}
-      size={ButtonSize.Xsmall}
-      leftIcon={iconEl}
-      onClick={onDirectMessageModalOpen}
-    >
-      Direct message
-    </Button>
-  );
+  const buttonEl =
+    (ButtonComponent && (
+      <ButtonComponent
+        className={className}
+        isMobileVersion={isMobileVersion}
+        onClick={onDirectMessageModalOpen}
+      />
+    )) ||
+    (isMobileVersion ? (
+      <ButtonIcon
+        className={className}
+        variant={buttonVariant}
+        onClick={onDirectMessageModalOpen}
+      >
+        {iconEl}
+      </ButtonIcon>
+    ) : (
+      <Button
+        className={className}
+        variant={buttonVariant}
+        size={ButtonSize.Xsmall}
+        leftIcon={iconEl}
+        onClick={onDirectMessageModalOpen}
+      >
+        Direct message
+      </Button>
+    ));
 
   return (
     <>
