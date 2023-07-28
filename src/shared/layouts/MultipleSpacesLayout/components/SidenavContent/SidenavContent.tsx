e68e956e4e4371c524bbf855b38727c7e8c8ddf7
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-07-28 16:35:33 +0400

    add empty state to sidenav content

--- src/shared/layouts/MultipleSpacesLayout/components/SidenavContent/SidenavContent.tsx
+++ src/shared/layouts/MultipleSpacesLayout/components/SidenavContent/SidenavContent.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode, useCallback, useRef } from "react";
 import classNames from "classnames";
 import { ButtonIcon } from "@/shared/components";
 import { Close2Icon } from "@/shared/icons";
+import { Button, ButtonVariant } from "@/shared/ui-kit";
 import {
   Projects,
   ProjectsRef,
@@ -15,13 +16,32 @@ interface SidenavContentProps {
 
 const SidenavContent: FC<SidenavContentProps> = (props) => {
   const { className, onClose } = props;
+  const projectsRef = useRef<ProjectsRef>(null);
+
+  const renderNoItemsInfo = useCallback((): ReactNode => {
+    return (
+      <div className={styles.noCommonsInfoContainer}>
+        <p className={styles.noCommonsText}>
+          You do not have spaces yet. You might want to create a new common or
+          ask your friends for the link to an existing one.
+        </p>
+        <Button
+          className={styles.createCommonButton}
+          variant={ButtonVariant.PrimaryPink}
+          onClick={projectsRef.current?.openCreateCommonModal}
+        >
+          Create common
+        </Button>
+      </div>
+    );
+  }, []);
 
   return (
     <div className={classNames(styles.container, className)}>
       <ButtonIcon className={styles.closeIconWrapper} onClick={onClose}>
         <Close2Icon />
       </ButtonIcon>
-      <Projects />
+      <Projects ref={projectsRef} renderNoItemsInfo={renderNoItemsInfo} />
     </div>
   );
 };
