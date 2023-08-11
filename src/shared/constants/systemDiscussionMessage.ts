c7a90c925f8e340aa8fd4574a8f97f1dd89445ac
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-08-11 11:30:43 +0600

    add feed-item-created system message type

--- src/shared/constants/systemDiscussionMessage.ts
+++ src/shared/constants/systemDiscussionMessage.ts
@@ -2,4 +2,5 @@ export enum SystemDiscussionMessageType {
   CommonMemberAdded = "common-member-added",
   CommonEdited = "common-edited",
   CommonCreated = "common-created",
+  FeedItemCreated = "feed-item-created",
 }
