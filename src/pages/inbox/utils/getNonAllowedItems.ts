026f4dfb3feace0d62923de12921ad732e3152bf
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-09-04 13:29:35 +0300

    remove "Edit" and "Delete" options from inbox items

--- src/pages/inbox/utils/getNonAllowedItems.ts
+++ src/pages/inbox/utils/getNonAllowedItems.ts
@@ -1,15 +1,8 @@
 import { FeedItemMenuItem, GetNonAllowedItemsOptions } from "@/pages/common";
-import { CommonFeedType } from "@/shared/models";
 
-export const getNonAllowedItems: GetNonAllowedItemsOptions = (type) => {
-  const items: FeedItemMenuItem[] = [
-    FeedItemMenuItem.Pin,
-    FeedItemMenuItem.Unpin,
-  ];
-
-  if (type !== CommonFeedType.Discussion) {
-    items.push(FeedItemMenuItem.Remove);
-  }
-
-  return items;
-};
+export const getNonAllowedItems: GetNonAllowedItemsOptions = () => [
+  FeedItemMenuItem.Pin,
+  FeedItemMenuItem.Unpin,
+  FeedItemMenuItem.Edit,
+  FeedItemMenuItem.Remove,
+];
