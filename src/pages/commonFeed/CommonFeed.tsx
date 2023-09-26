c9f1dc15da46b613ce820a9e2953295e9db13c09
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-09-26 11:37:56 +0300

    add shared feed item after pinned ones

--- src/pages/commonFeed/CommonFeed.tsx
+++ src/pages/commonFeed/CommonFeed.tsx
@@ -188,11 +188,11 @@ const CommonFeedComponent: FC<CommonFeedProps> = (props) => {
         (item) => item.itemId !== sharedFeedItemId,
       ) || [];
 
-    if (sharedFeedItem) {
-      items.push(sharedFeedItem);
-    }
     if (filteredPinnedItems.length > 0) {
       items.push(...filteredPinnedItems);
+    }
+    if (sharedFeedItem) {
+      items.push(sharedFeedItem);
     }
 
     return items;
