765e42b3790363cd5cfdb8f46103096677be2bbc
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-09-26 16:07:53 +0300

    fix logic of useLockedBody hook

--- src/shared/hooks/useLockedBody.ts
+++ src/shared/hooks/useLockedBody.ts
@@ -24,25 +24,30 @@ const useLockedBody = (initialLocked = false, rootId = "root"): Return => {
 
     // Save initial body style
     const originalOverflow = document.body.style.overflow;
-    const originalPaddingRight = document.body.style.paddingRight;
+    const originalPaddingRight = parseInt(
+      window.getComputedStyle(document.body).getPropertyValue("padding-right"),
+      10,
+    );
+
+    // Get the scrollBar width
+    const scrollBarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
 
     // Lock body scroll
     document.body.style.overflow = "hidden";
 
-    // Get the scrollBar width
-    const root = document.getElementById(rootId); // or root
-    const scrollBarWidth = root ? root.offsetWidth - root.scrollWidth : 0;
-
     // Avoid width reflow
     if (scrollBarWidth) {
-      document.body.style.paddingRight = `${scrollBarWidth}px`;
+      document.body.style.paddingRight = `${
+        originalPaddingRight + scrollBarWidth
+      }px`;
     }
 
     return () => {
       document.body.style.overflow = originalOverflow;
 
       if (scrollBarWidth) {
-        document.body.style.paddingRight = originalPaddingRight;
+        document.body.style.paddingRight = `${originalPaddingRight}px`;
       }
     };
   }, [locked]);
