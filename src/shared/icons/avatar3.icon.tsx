af9922d41ecaaa3e5a5635089a23f3d482d4c224
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-10-16 16:33:34 +0300

    add icons to the profile menu

---
+++ src/shared/icons/avatar3.icon.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from "react";
+
+interface Avatar3IconProps {
+  className?: string;
+}
+
+const Avatar3Icon: FC<Avatar3IconProps> = ({ className }) => {
+  const color = "currentColor";
+
+  return (
+    <svg
+      className={className}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.52002 19C6.16002 16.8 7.36002 16 8.74002 16H15.26C16.64 16 17.84 16.8 18.48 19"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Avatar3Icon;
