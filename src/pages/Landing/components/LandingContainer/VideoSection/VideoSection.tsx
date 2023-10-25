5140f4365cc9b46de547f34fc3301983aea43311
Roie Natan <roie.natan@gmail.com>
2023-10-25 11:28:28 +0100

    updates

--- src/pages/Landing/components/LandingContainer/VideoSection/VideoSection.tsx
+++ src/pages/Landing/components/LandingContainer/VideoSection/VideoSection.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
+import landingMobilePosterSec from "@/shared/assets/images/landing-mobile-poster.jpg";
+import landingVideoPosterSrc from "@/shared/assets/images/landing-video-poster.jpg";
+import landingVideoSrc from "@/shared/assets/videos/landing-video.mp4";
+import { useIsPhoneView } from "@/shared/hooks/viewport";
 import { Button, ButtonVariant } from "@/shared/ui-kit";
 import "./index.scss";
 
@@ -11,9 +15,25 @@ const VideoSection: FC<VideoSectionProps> = ({ onLaunchClick }) => {
   const { t } = useTranslation("translation", {
     keyPrefix: "landing",
   });
+  const isPhoneView = useIsPhoneView();
 
   return (
     <section className="landing-video-section">
+      <div className="landing-video-section__video-wrapper">
+        {!isPhoneView && (
+          <video
+            className="landing-video-section__video"
+            autoPlay
+            loop
+            muted
+            playsInline
+            poster={landingVideoPosterSrc}
+            preload="auto"
+          >
+            <source src={landingVideoSrc} type="video/mp4" />
+          </video>
+        )}
+      </div>
       <div className="landing-video-section__main-info">
         <h1 className="landing-video-section__main-info-title">
           {t("videoSection.title.part1")}
