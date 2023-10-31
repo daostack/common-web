acfe43fd9859aaca116b4e904e885a7a1cbb1546
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-10-31 14:53:46 +0300

    uncomment getTheme util

--- src/shared/utils/getTheme.ts
+++ src/shared/utils/getTheme.ts
@@ -1,19 +1,16 @@
-//import { StorageKey } from "@/shared/constants";
+import { StorageKey } from "@/shared/constants";
 import { Theme } from "../constants/theme";
 
 export const getTheme = () => {
-  /**
-   * Light mode is the default until we have a complete color scheme for dark mode.
-   */
-  return Theme.Light;
+  const theme = localStorage.getItem(StorageKey.Theme) as Theme | null;
 
-  // const theme = localStorage.getItem(StorageKey.Theme) as Theme | null;
-  // if (theme && Object.values(Theme).includes(theme)) {
-  //   return theme;
-  // }
+  if (theme && Object.values(Theme).includes(theme)) {
+    return theme;
+  }
 
-  // const isDarkThemePreferred = window.matchMedia(
-  //   `(prefers-color-scheme: ${Theme.Dark})`,
-  // );
-  // return isDarkThemePreferred ? Theme.Dark : Theme.Light;
+  const isDarkThemePreferred = window.matchMedia(
+    `(prefers-color-scheme: ${Theme.Dark})`,
+  );
+
+  return isDarkThemePreferred ? Theme.Dark : Theme.Light;
 };
