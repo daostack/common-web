8008b453c66857aa49b04d9e721ca95df2875352
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-10-31 17:29:29 +0300

    create useMount hook

---
+++ src/shared/hooks/useMount.ts
@@ -0,0 +1,35 @@
+import { useRef, useState } from "react";
+import { useUpdateEffect } from "react-use";
+
+interface Options {
+  isOpen: boolean;
+  delay: number;
+}
+
+export const useMount = (options: Options): boolean => {
+  const { isOpen, delay } = options;
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [isMounted, setIsMounted] = useState(isOpen);
+
+  useUpdateEffect(() => {
+    if (isOpen && !isMounted) {
+      setIsMounted(true);
+      return;
+    }
+    if (isOpen || !isMounted) {
+      return;
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      setIsMounted(false);
+    }, delay);
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [isOpen]);
+
+  return isMounted;
+};
