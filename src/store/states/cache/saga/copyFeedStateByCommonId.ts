dc7922e315a4f595a4bf58cb42d44365aaaa9e7c
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-11-07 15:23:46 +0300

    cache only first 30 feed items

--- src/store/states/cache/saga/copyFeedStateByCommonId.ts
+++ src/store/states/cache/saga/copyFeedStateByCommonId.ts
@@ -1,4 +1,5 @@
 import { put, select } from "redux-saga/effects";
+import { getFeedLayoutItemDateForSorting } from "@/store/states/inbox/utils";
 import { selectCommonState, CommonState } from "../../common";
 import * as actions from "../actions";
 
@@ -6,11 +7,26 @@ export function* copyFeedStateByCommonId({
   payload: commonId,
 }: ReturnType<typeof actions.copyFeedStateByCommonId>) {
   const commonState = (yield select(selectCommonState)) as CommonState;
+  const data =
+    commonState.feedItems.data && commonState.feedItems.data.slice(0, 30);
+  const feedItems = {
+    ...commonState.feedItems,
+    data,
+    loading: false,
+    hasMore: true,
+    firstDocTimestamp: data?.[0]
+      ? getFeedLayoutItemDateForSorting(data[0])
+      : null,
+    lastDocTimestamp: data?.[data.length - 1]
+      ? getFeedLayoutItemDateForSorting(data[data.length - 1])
+      : null,
+  };
+
   yield put(
     actions.updateFeedStateByCommonId({
       commonId,
       state: {
-        feedItems: commonState.feedItems,
+        feedItems,
         pinnedFeedItems: commonState.pinnedFeedItems,
         sharedFeedItem: commonState.sharedFeedItem,
       },
