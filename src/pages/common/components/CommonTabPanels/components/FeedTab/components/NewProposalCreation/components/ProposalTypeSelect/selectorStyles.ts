43ff266bda853a1f3e0f413eebea8fc4b86d3780
Roie Natan <roie.natan@gmail.com>
2023-11-16 12:24:53 -0500

    version 8

--- src/pages/common/components/CommonTabPanels/components/FeedTab/components/NewProposalCreation/components/ProposalTypeSelect/selectorStyles.ts
+++ src/pages/common/components/CommonTabPanels/components/FeedTab/components/NewProposalCreation/components/ProposalTypeSelect/selectorStyles.ts
@@ -5,7 +5,7 @@ export const selectorStyles = (hasError: boolean, isDarkMode: boolean) => ({
   menuPortal: (provided) => ({ ...provided, zIndex: 10000 }),
   menuList: (provided) => ({
     ...provided,
-    backgroundColor: isDarkMode ? "#2e3452" : "#f8f8f5",
+    backgroundColor: isDarkMode ? "#131b23" : "#ffffff",
   }),
   control: (provided) => ({
     ...provided,
@@ -13,12 +13,19 @@ export const selectorStyles = (hasError: boolean, isDarkMode: boolean) => ({
     ...(hasError && { borderColor: Colors.error }),
     backgroundColor: isDarkMode ? "#2e3452" : "#f8f8f5",
     color: isDarkMode ? "#ffffff" : "#001a36",
-    border: "none",
+    borderColor: "rgba(255, 255, 255, 0)",
+    boxShadow: "rgba(255, 255, 255, 0)",
+    "&:hover": {
+      boxShadow: "rgba(255, 255, 255, 0)",
+    },
   }),
   option: (provided) => ({
     ...provided,
-    backgroundColor: isDarkMode ? "#2e3452" : "#f8f8f5",
+    backgroundColor: isDarkMode ? "#131b23" : "#ffffff",
     color: isDarkMode ? "#ffffff" : "#001a36",
+    "&:hover": {
+      backgroundColor: isDarkMode ? "#2e3452" : "#fff9fd",
+    },
   }),
   indicatorSeparator: () => ({ display: "none" }),
   indicatorsContainer: (provided) => ({
