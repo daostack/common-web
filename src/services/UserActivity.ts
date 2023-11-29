f7df2144df32fcfa2d292eb56c2700a533702e36
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-11-29 15:41:38 +0400

    add method to get user activity

--- src/services/UserActivity.ts
+++ src/services/UserActivity.ts
@@ -1,7 +1,7 @@
 import { UnsubscribeFunction } from "@/shared/interfaces";
 import { Collection, UserActivity } from "@/shared/models";
 import { firestoreDataConverter } from "@/shared/utils";
-import firebase from "@/shared/utils/firebase";
+import firebase, { isFirestoreCacheError } from "@/shared/utils/firebase";
 
 const userActivityConverter = firestoreDataConverter<UserActivity>();
 
@@ -11,6 +11,25 @@ class UserActivityService {
       .firestore()
       .collection(Collection.UsersActivity)
       .withConverter(userActivityConverter);
+
+  public getUserActivity = async (
+    userId: string,
+    cached = false,
+  ): Promise<UserActivity | null> => {
+    try {
+      const snapshot = await this.getUsersActivityCollection()
+        .doc(userId)
+        .get({ source: cached ? "cache" : "default" });
+
+      return snapshot?.data() || null;
+    } catch (error) {
+      if (cached && isFirestoreCacheError(error)) {
+        return this.getUserActivity(userId);
+      } else {
+        throw error;
+      }
+    }
+  };
 
   public updateUserActivity = async (
     userId: string,
