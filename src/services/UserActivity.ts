4b407d393cb791564383ac2124bf5b9dece1b182
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-11-29 11:40:50 +0400

    add logic to get and update user activity

---
+++ src/services/UserActivity.ts
@@ -0,0 +1,41 @@
+import { Collection, UserActivity } from "@/shared/models";
+import { firestoreDataConverter } from "@/shared/utils";
+import firebase, { isFirestoreCacheError } from "@/shared/utils/firebase";
+
+const userActivityConverter = firestoreDataConverter<UserActivity>();
+
+class UserActivityService {
+  private getUsersActivityCollection = () =>
+    firebase
+      .firestore()
+      .collection(Collection.UsersActivity)
+      .withConverter(userActivityConverter);
+
+  public getUserActivity = async (
+    userId: string,
+    cached = false,
+  ): Promise<UserActivity | null> => {
+    try {
+      const snapshot = await this.getUsersActivityCollection()
+        .doc(userId)
+        .get({ source: cached ? "cache" : "default" });
+
+      return snapshot?.data() || null;
+    } catch (error) {
+      if (cached && isFirestoreCacheError(error)) {
+        return this.getUserActivity(userId);
+      } else {
+        throw error;
+      }
+    }
+  };
+
+  public updateUserActivity = async (
+    userId: string,
+    data: Partial<UserActivity>,
+  ): Promise<void> => {
+    await this.getUsersActivityCollection().doc(userId).update(data);
+  };
+}
+
+export default new UserActivityService();
