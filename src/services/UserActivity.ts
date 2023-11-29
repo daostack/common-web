95fce21e828c479530ca55cd27bd38248690716c
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-11-29 14:22:28 +0400

    add subscription to user activity use-case

--- src/services/UserActivity.ts
+++ src/services/UserActivity.ts
@@ -1,6 +1,7 @@
+import { UnsubscribeFunction } from "@/shared/interfaces";
 import { Collection, UserActivity } from "@/shared/models";
 import { firestoreDataConverter } from "@/shared/utils";
-import firebase, { isFirestoreCacheError } from "@/shared/utils/firebase";
+import firebase from "@/shared/utils/firebase";
 
 const userActivityConverter = firestoreDataConverter<UserActivity>();
 
@@ -11,25 +12,6 @@ class UserActivityService {
       .collection(Collection.UsersActivity)
       .withConverter(userActivityConverter);
 
-  public getUserActivity = async (
-    userId: string,
-    cached = false,
-  ): Promise<UserActivity | null> => {
-    try {
-      const snapshot = await this.getUsersActivityCollection()
-        .doc(userId)
-        .get({ source: cached ? "cache" : "default" });
-
-      return snapshot?.data() || null;
-    } catch (error) {
-      if (cached && isFirestoreCacheError(error)) {
-        return this.getUserActivity(userId);
-      } else {
-        throw error;
-      }
-    }
-  };
-
   public updateUserActivity = async (
     userId: string,
     data: Partial<UserActivity>,
@@ -37,6 +19,21 @@ class UserActivityService {
     await this.getUsersActivityCollection()
       .doc(userId)
       .set(data, { merge: true });
+  };
+
+  public subscribeToUserActivity = (
+    userId: string,
+    callback: (userActivity: UserActivity) => void,
+  ): UnsubscribeFunction => {
+    const query = this.getUsersActivityCollection().doc(userId);
+
+    return query.onSnapshot((snapshot) => {
+      const userActivity = snapshot.data();
+
+      if (userActivity) {
+        callback(userActivity);
+      }
+    });
   };
 }
 
--- src/shared/hooks/useCases/useUserActivity.ts
+++ src/shared/hooks/useCases/useUserActivity.ts
@@ -1,12 +1,17 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { Logger, UserActivityService } from "@/services";
+import { useLoadingState } from "@/shared/hooks";
+import { LoadingState } from "@/shared/interfaces";
 import { UserActivity } from "@/shared/models";
 
-interface Return {
+interface Return extends LoadingState<UserActivity | null> {
   updateUserActivity: (data: Partial<UserActivity>) => void;
 }
 
 export const useUserActivity = (userId?: string): Return => {
+  const [userActivityState, setUserActivityState] =
+    useLoadingState<UserActivity | null>(null, { loading: true });
+
   const updateUserActivity = useCallback(
     async (data: Partial<UserActivity>) => {
       if (!userId) {
@@ -22,5 +27,27 @@ export const useUserActivity = (userId?: string): Return => {
     [userId],
   );
 
-  return { updateUserActivity };
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+
+    const unsubscribe = UserActivityService.subscribeToUserActivity(
+      userId,
+      (updatedUserActivity) => {
+        setUserActivityState({
+          loading: false,
+          fetched: true,
+          data: updatedUserActivity,
+        });
+      },
+    );
+
+    return unsubscribe;
+  }, [userId]);
+
+  return {
+    ...userActivityState,
+    updateUserActivity,
+  };
 };
