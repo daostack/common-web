cb44ae814924d192c8e484b1d274bc49e6a3d313
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-12-01 23:16:43 +0400

    add check for empty value in cache transform

--- src/store/transforms.ts
+++ src/store/transforms.ts
@@ -18,7 +18,7 @@ const clearNonFinishedStates = <T extends unknown>(
   states: Record<string, LoadingState<T>>,
 ): Record<string, LoadingState<T>> =>
   Object.entries(states).reduce((acc, [key, value]) => {
-    if (value.loading || !value.fetched) {
+    if (value.loading || !value.fetched || !value) {
       return acc;
     }
 
