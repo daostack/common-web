a30f22b9d9b9d0cb9a42e6fa6de0ac71e8a22fb7
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-12-04 11:48:57 +0400

    update InboxItem model

--- src/shared/models/InboxItem.ts
+++ src/shared/models/InboxItem.ts
@@ -1,8 +1,32 @@
 import { InboxItemType } from "@/shared/constants";
+import { CommonFeedType } from "./CommonFeed";
 import { Timestamp } from "./Timestamp";
 
-export interface InboxItem {
+export type InboxItemFeedItemData =
+  | {
+      feedItemType: CommonFeedType.Discussion;
+      discussionId: string;
+    }
+  | {
+      feedItemType: CommonFeedType.Proposal;
+      proposalId: string;
+      discussionId: string | null;
+    }
+  | {
+      feedItemType: Exclude<
+        CommonFeedType,
+        CommonFeedType.Discussion | CommonFeedType.Proposal
+      >;
+    };
+
+export type InboxItem = {
   itemId: string; // id of feedItemFollow or ChatChannel
   updatedAt: Timestamp;
-  type: InboxItemType;
-}
+} & (
+  | ({
+      type: InboxItemType.FeedItemFollow;
+    } & InboxItemFeedItemData)
+  | {
+      type: InboxItemType.ChatChannel;
+    }
+);
