4c13f6abb4392ea560f1582b01b98accd2908204
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-12-05 12:17:59 +0400

    create logic to refresh unread inbox items list

--- src/store/states/inbox/actions.ts
+++ src/store/states/inbox/actions.ts
@@ -23,6 +23,13 @@ export const getInboxItems = createAsyncAction(
   string
 >();
 
+export const refreshUnreadInboxItems = createAsyncAction(
+  InboxActionType.REFRESH_UNREAD_INBOX_ITEMS,
+  InboxActionType.REFRESH_UNREAD_INBOX_ITEMS_SUCCESS,
+  InboxActionType.REFRESH_UNREAD_INBOX_ITEMS_FAILURE,
+  InboxActionType.REFRESH_UNREAD_INBOX_ITEMS_CANCEL,
+)<{ newItemsAmount: number }, void, void, string>();
+
 export const addNewInboxItems = createStandardAction(
   InboxActionType.ADD_NEW_INBOX_ITEMS,
 )<
---
+++ src/store/states/inbox/saga/refreshUnreadInboxItems.ts
@@ -0,0 +1,96 @@
+import { call, put, select } from "redux-saga/effects";
+import { Logger, UserService } from "@/services";
+import { InboxItemType } from "@/shared/constants";
+import {
+  Awaited,
+  ChatChannelLayoutItem,
+  FeedItemFollowLayoutItemWithFollowData,
+  FeedLayoutItemWithFollowData,
+} from "@/shared/interfaces";
+import { Timestamp } from "@/shared/models";
+import * as actions from "../actions";
+import { selectInboxItems } from "../selectors";
+import { InboxItems } from "../types";
+
+export function* refreshUnreadInboxItems(
+  action: ReturnType<typeof actions.refreshUnreadInboxItems.request>,
+) {
+  const {
+    payload: { newItemsAmount },
+  } = action;
+
+  try {
+    const currentItems = (yield select(selectInboxItems)) as InboxItems;
+    const newItemsAmountToFetch =
+      newItemsAmount - (currentItems.data?.length || 0);
+
+    if (newItemsAmountToFetch <= 0) {
+      return;
+    }
+
+    const newInboxItems: FeedLayoutItemWithFollowData[] = [];
+    let startAfter: Timestamp | null = null;
+    let keepItemsFetching = true;
+
+    while (keepItemsFetching) {
+      const { data, lastDocTimestamp, hasMore } = (yield call(
+        UserService.getInboxItems,
+        {
+          startAfter,
+          limit: 5,
+          unread: true,
+        },
+      )) as Awaited<ReturnType<typeof UserService.getInboxItems>>;
+      const chatChannelItems = data.chatChannels
+        .filter(
+          (chatChannel) =>
+            chatChannel.messageCount > 0 &&
+            (!currentItems.data ||
+              currentItems.data.every(
+                (item) => item.itemId !== chatChannel.id,
+              )),
+        )
+        .map<ChatChannelLayoutItem>((chatChannel) => ({
+          type: InboxItemType.ChatChannel,
+          itemId: chatChannel.id,
+          chatChannel,
+        }));
+      const feedItemFollowItems = data.feedItemFollows
+        .filter(
+          (feedItemFollow) =>
+            !currentItems.data ||
+            currentItems.data.every(
+              (item) => item.itemId !== feedItemFollow.id,
+            ),
+        )
+        .map<FeedItemFollowLayoutItemWithFollowData>(
+          (feedItemFollowWithMetadata) => ({
+            type: InboxItemType.FeedItemFollow,
+            itemId: feedItemFollowWithMetadata.feedItemId,
+            feedItem: feedItemFollowWithMetadata.feedItem,
+            feedItemFollowWithMetadata: feedItemFollowWithMetadata,
+          }),
+        );
+      newInboxItems.push(...chatChannelItems, ...feedItemFollowItems);
+      keepItemsFetching =
+        newInboxItems.length < newItemsAmountToFetch && hasMore;
+      startAfter = lastDocTimestamp;
+    }
+
+    if (newInboxItems.length > 0) {
+      yield put(
+        actions.addNewInboxItems(
+          newInboxItems.map((item) => ({
+            item,
+            statuses: {
+              isAdded: false,
+              isRemoved: false,
+            },
+          })),
+        ),
+      );
+    }
+  } catch (err) {
+    Logger.error(err);
+  }
+}
