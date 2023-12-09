5ce3f9f998f1be38f2ded97232d58ba60b1cb1b8
Roie Natan <roie.natan@gmail.com>
2023-12-09 15:50:02 -0600

    clean

--- src/pages/inbox/components/DirectMessageButton/components/DirectMessageModal/DirectMessageModal.tsx
+++ src/pages/inbox/components/DirectMessageButton/components/DirectMessageModal/DirectMessageModal.tsx
@@ -1,16 +1,7 @@
-import React, {
-  FC,
-  ReactElement,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import React, { FC, ReactElement, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Select from "react-select";
 import { Modal, UserAvatar } from "@/shared/components";
-import { KeyboardKeys } from "@/shared/constants";
 import { useDMUserChatChannel } from "@/shared/hooks/useCases";
 import useThemeColor from "@/shared/hooks/useThemeColor";
 import { SearchIcon } from "@/shared/icons";
@@ -18,7 +9,6 @@ import { DMUser } from "@/shared/interfaces";
 import { Button, Loader } from "@/shared/ui-kit";
 import { emptyFunction } from "@/shared/utils";
 import { inboxActions } from "@/store/states";
-import { DirectMessageUserItem, SearchInput } from "./components";
 import { selectorStyles } from "./components/selectorStyles";
 import { useDMUsers } from "./hooks";
 import styles from "./DirectMessageModal.module.scss";
@@ -30,6 +20,12 @@ interface DirectMessageModalProps {
   groupMessage?: boolean;
 }
 
+interface SelectOption {
+  value: string;
+  user: DMUser;
+  label: JSX.Element;
+}
+
 const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
   const {
     isOpen,
@@ -39,9 +35,6 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
   } = props;
   const dispatch = useDispatch();
   const { getThemeColor } = useThemeColor();
-  const listRef = useRef<HTMLUListElement>(null);
-  //const [searchText, setSearchText] = useState("");
-  const [activeItemIndex, setActiveItemIndex] = useState<number | null>(null);
   const {
     loading: areDMUsersLoading,
     dmUsers,
@@ -56,50 +49,6 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
     error: isChannelLoadedWithError,
   } = useDMUserChatChannel();
 
-  // const filteredDMUsers = useMemo(() => {
-  //   if (!searchText) {
-  //     return dmUsers;
-  //   }
-
-  //   const lowerCasedSearchText = searchText.toLowerCase();
-
-  //   return dmUsers.filter((item) =>
-  //     item.userName.toLowerCase().startsWith(lowerCasedSearchText),
-  //   );
-  // }, [dmUsers, searchText]);
-
-  //const totalUsersAmount = filteredDMUsers.length;
-
-  // const handleArrowUp = useCallback(() => {
-  //   setActiveItemIndex((currentIndex) => {
-  //     if (totalUsersAmount === 0) {
-  //       return null;
-  //     }
-  //     if (currentIndex === null) {
-  //       return totalUsersAmount - 1;
-  //     }
-
-  //     const nextIndex = currentIndex - 1;
-
-  //     return nextIndex < 0 ? totalUsersAmount - 1 : nextIndex;
-  //   });
-  // }, [totalUsersAmount]);
-
-  // const handleArrowDown = useCallback(() => {
-  //   setActiveItemIndex((currentIndex) => {
-  //     if (totalUsersAmount === 0) {
-  //       return null;
-  //     }
-  //     if (currentIndex === null) {
-  //       return 0;
-  //     }
-
-  //     const nextIndex = currentIndex + 1;
-
-  //     return nextIndex >= totalUsersAmount ? 0 : nextIndex;
-  //   });
-  // }, [totalUsersAmount]);
-
   const handleUserItemClick = (item: DMUser) => {
     fetchDMUserChatChannel(item.uid);
   };
@@ -110,76 +59,8 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
       return;
     }
 
-    setActiveItemIndex(null);
-    //setSearchText("");
     resetDMUserChatChannel();
   }, [isOpen]);
-
-  // useEffect(() => {
-  //   setActiveItemIndex(null);
-  // }, [searchText]);
-
-  useEffect(() => {
-    if (activeItemIndex !== null) {
-      const listElement = listRef.current?.children.item(activeItemIndex);
-      listElement?.scrollIntoView(false);
-    }
-  }, [activeItemIndex]);
-
-  // useEffect(() => {
-  //   if (!isOpen) {
-  //     return;
-  //   }
-
-  //   const handler = (event: KeyboardEvent) => {
-  //     const key = event.key as KeyboardKeys;
-
-  //     switch (key) {
-  //       case KeyboardKeys.ArrowUp:
-  //         handleArrowUp();
-  //         break;
-  //       case KeyboardKeys.ArrowDown:
-  //         handleArrowDown();
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //   };
-
-  //   window.addEventListener("keyup", handler);
-
-  //   return () => {
-  //     window.removeEventListener("keyup", handler);
-  //   };
-  // }, [isOpen, handleArrowUp, handleArrowDown]);
-
-  // useEffect(() => {
-  //   if (!isOpen) {
-  //     return;
-  //   }
-
-  //   const handler = (event: KeyboardEvent) => {
-  //     const key = event.key as KeyboardKeys;
-
-  //     if (key !== KeyboardKeys.Enter) {
-  //       return;
-  //     }
-
-  //     const item = filteredDMUsers.find(
-  //       (dmUser, index) => index === activeItemIndex,
-  //     );
-
-  //     if (item) {
-  //       handleUserItemClick(item);
-  //     }
-  //   };
-
-  //   window.addEventListener("keyup", handler);
-
-  //   return () => {
-  //     window.removeEventListener("keyup", handler);
-  //   };
-  // }, [isOpen, activeItemIndex, filteredDMUsers]);
 
   useEffect(() => {
     if (dmUserChatChannel) {
@@ -206,7 +87,7 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
       return <p className={styles.infoText}>No users found</p>;
     }
 
-    const options = dmUsers.map((user) => ({
+    const options: SelectOption[] = dmUsers.map((user) => ({
       value: user.userName,
       user: user,
       label: (
@@ -222,12 +103,18 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
       ),
     }));
 
+    const handleClick = (item: SelectOption) => {
+      if (!groupMessage) {
+        handleUserItemClick(item.user);
+      } else {
+        console.log("HANDLE GROUP MESSAGE");
+      }
+    };
+
     return (
       <>
         <Select
-          onChange={(item) =>
-            !groupMessage && item && handleUserItemClick((item as any).user)
-          }
+          onChange={(item) => handleClick(item as SelectOption)}
           isMulti={groupMessage}
           options={options}
           placeholder="Search"
@@ -245,34 +132,6 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
         {groupMessage && <Button>Create</Button>}
       </>
     );
-
-    // return (
-    //   <ul ref={listRef} className={styles.itemList}>
-    //     {filteredDMUsers.map((item, index) => {
-    //       const isActive = index === activeItemIndex;
-
-    //       return (
-    //         <li
-    //           key={item.uid}
-    //           className={styles.item}
-    //           tabIndex={0}
-    //           role="button"
-    //           aria-pressed={isActive}
-    //           onFocus={() => setActiveItemIndex(index)}
-    //           onMouseDown={(event) => event.preventDefault()}
-    //           onClick={() => handleUserItemClick(item)}
-    //         >
-    //           <DirectMessageUserItem
-    //             className={styles.userItem}
-    //             image={item.photoURL}
-    //             name={item.userName}
-    //             isActive={isActive}
-    //           />
-    //         </li>
-    //       );
-    //     })}
-    //   </ul>
-    // );
   };
 
   return (
@@ -285,13 +144,6 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
           <h3 className={styles.modalTitle}>
             {groupMessage ? "Group message" : "Direct message"}
           </h3>
-          {/* {!isChannelLoading && (
-            <SearchInput
-              value={searchText}
-              onChange={setSearchText}
-              autoFocus
-            />
-          )} */}
         </div>
       }
       isHeaderSticky
@@ -301,7 +153,6 @@ const DirectMessageModal: FC<DirectMessageModalProps> = (props) => {
         modalOverlay: styles.modalOverlay,
         headerWrapper: styles.modalHeaderWrapper,
         header: styles.modalHeader,
-        content: styles.modalContent,
         closeWrapper: styles.modalCloseWrapper,
       }}
     >
