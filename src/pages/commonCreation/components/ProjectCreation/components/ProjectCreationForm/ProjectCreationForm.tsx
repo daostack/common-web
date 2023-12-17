32daaaff5e19164940b4a21e988f88f279c86553
Roie Natan <roie.natan@gmail.com>
2023-12-17 14:32:43 -0600

    Allow using same space name if parent common is different

--- src/pages/commonCreation/components/ProjectCreation/components/ProjectCreationForm/ProjectCreationForm.tsx
+++ src/pages/commonCreation/components/ProjectCreation/components/ProjectCreationForm/ProjectCreationForm.tsx
@@ -130,7 +130,11 @@ const ProjectCreationForm: FC<ProjectCreationFormProps> = (props) => {
   );
   const projectId = initialCommon?.id || project?.id;
 
+  /**
+   * Existing projects names under the same direct parent only.
+   */
   const existingProjectsNames = projects
+    .filter((project) => project.directParent?.commonId === parentCommonId)
     .map((project) => project?.name)
     .filter((spaceName) => spaceName !== initialValues?.spaceName);
 
