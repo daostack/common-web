b8a0e132824625686f25af500f2e258be7518de5
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2023-12-19 12:29:50 +0400

    display common paths in tooltip

--- src/pages/common/components/FeedCard/components/LinkedItemMark/LinkedItemMark.tsx
+++ src/pages/common/components/FeedCard/components/LinkedItemMark/LinkedItemMark.tsx
@@ -1,7 +1,21 @@
-import React, { FC, MouseEventHandler, useEffect, useState } from "react";
+import React, {
+  FC,
+  MouseEventHandler,
+  ReactElement,
+  useEffect,
+  useState,
+} from "react";
+import { NavLink } from "react-router-dom";
 import { ButtonIcon } from "@/shared/components";
-import { Link4Icon } from "@/shared/icons";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/shared/ui-kit";
+import { useRoutesContext } from "@/shared/contexts";
+import { RightArrowThinIcon, Link4Icon } from "@/shared/icons";
+import { Common } from "@/shared/models";
+import {
+  Loader,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/shared/ui-kit";
 import { useCommonPaths } from "./hooks";
 import styles from "./LinkedItemMark.module.scss";
 
@@ -24,6 +38,7 @@ const LinkedItemMark: FC<LinkedItemMarkProps> = (props) => {
     fetched,
     fetchCommonPaths,
   } = useCommonPaths();
+  const { getCommonPagePath } = useRoutesContext();
 
   const toggleTooltip: MouseEventHandler = (event) => {
     event.stopPropagation();
@@ -42,6 +57,43 @@ const LinkedItemMark: FC<LinkedItemMarkProps> = (props) => {
     fetchCommonPaths(commonIds);
   }, [isOpen]);
 
+  const renderFullPath = (commons: Common[]): ReactElement => {
+    return (
+      <div className={styles.linkLeft}>
+        {commons.map((common, commonIndex) => (
+          <React.Fragment key={common.id}>
+            {common.name}
+            {commonIndex !== commons.length - 1 && (
+              <RightArrowThinIcon className={styles.arrowIcon} />
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  };
+
+  const renderCutPath = (commons: Common[]): ReactElement => {
+    const lastCommon = commons[commons.length - 1];
+    const parentCommons = commons.slice(0, -1);
+
+    return (
+      <>
+        <div className={styles.linkLeft}>
+          {parentCommons.map((common, commonIndex) => (
+            <React.Fragment key={common.id}>
+              {common.name}
+              {commonIndex !== commons.length - 1 && (
+                <RightArrowThinIcon className={styles.arrowIcon} />
+              )}
+            </React.Fragment>
+          ))}
+        </div>
+        <div className={styles.ellipsis}>…</div>
+        <div className={styles.linkRight}>{lastCommon?.name}</div>
+      </>
+    );
+  };
+
   return (
     <Tooltip open={isOpen} onOpenChange={setIsOpen} placement="right">
       <TooltipTrigger onClick={toggleTooltip} asChild>
@@ -51,6 +103,23 @@ const LinkedItemMark: FC<LinkedItemMarkProps> = (props) => {
       </TooltipTrigger>
       <TooltipContent className={styles.tooltipContent}>
         <span className={styles.contentTitle}>Also appears in:</span>
+        {loading && <Loader className={styles.loader} />}
+        {!loading &&
+          commonPaths.map((commons, index) => {
+            const lastCommon = commons[commons.length - 1];
+            const key = lastCommon?.id || String(index);
+
+            return (
+              <NavLink
+                key={key}
+                className={styles.link}
+                to={getCommonPagePath(lastCommon?.id || "")}
+                title={lastCommon?.name}
+              >
+                {renderCutPath(commons)}
+              </NavLink>
+            );
+          })}
       </TooltipContent>
     </Tooltip>
   );
