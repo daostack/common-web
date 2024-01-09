d0e5a233a9c928bc1270d300c8545e92c9101c9f
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2024-01-09 14:27:10 +0300

    refactor toggleCheckboxItem logic

--- src/shared/ui-kit/TextEditor/utils/toggleCheckboxItem.ts
+++ src/shared/ui-kit/TextEditor/utils/toggleCheckboxItem.ts
@@ -1,17 +1,36 @@
-import { Editor } from "slate";
+import { Editor, Element as SlateElement, Transforms } from "slate";
 import { ElementType } from "../constants";
 import { CheckboxItemElement } from "../types";
-import { toggleElement } from "./toggleElement";
+import { isElementActive } from "./isElementActive";
 
 export const toggleCheckboxItem = (
   editor: Editor,
   checked = false,
   onlyIfInactive = false,
 ) => {
-  const checkboxItemElement: CheckboxItemElement = {
+  const isActive = isElementActive(editor, ElementType.CheckboxItem);
+
+  if (isActive && onlyIfInactive) {
+    return;
+  }
+  if (isActive) {
+    Transforms.setNodes<SlateElement>(editor, { type: ElementType.Paragraph });
+    return;
+  }
+
+  const { selection } = editor;
+  const checkboxItemElement: Partial<CheckboxItemElement> = {
     type: ElementType.CheckboxItem,
     checked,
-    children: [{ text: "" }],
   };
-  toggleElement(editor, checkboxItemElement, { onlyIfInactive });
+
+  Transforms.setNodes<SlateElement>(editor, checkboxItemElement);
+  Transforms.delete(editor, {
+    at: selection?.anchor.path,
+    unit: "line",
+  });
+
+  if (selection) {
+    Transforms.select(editor, selection.anchor.path);
+  }
 };
