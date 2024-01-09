b9f339237afa39ade602e6a569c5f432307e08af
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2024-01-09 12:22:05 +0300

    allow to set whole item properties in toggleElement

--- src/shared/ui-kit/TextEditor/utils/toggleElement.ts
+++ src/shared/ui-kit/TextEditor/utils/toggleElement.ts
@@ -1,11 +1,26 @@
 import { Editor, Element as SlateElement, Transforms } from "slate";
 import { ElementType } from "../constants";
+import { CustomElement } from "../types";
 import { isElementActive } from "./isElementActive";
 
-export const toggleElement = (editor: Editor, elementType: ElementType) => {
-  const isActive = isElementActive(editor, elementType);
-  const newProperties: Partial<SlateElement> = {
-    type: isActive ? ElementType.Paragraph : elementType,
-  };
+export const toggleElement = (
+  editor: Editor,
+  element: ElementType | CustomElement,
+) => {
+  const isOnlyElementTypeProvided = typeof element !== "object";
+  const isActive = isElementActive(
+    editor,
+    isOnlyElementTypeProvided ? element : element.type,
+  );
+  let newProperties: Partial<SlateElement>;
+
+  if (isOnlyElementTypeProvided) {
+    newProperties = {
+      type: isActive ? ElementType.Paragraph : element,
+    };
+  } else {
+    newProperties = isActive ? { type: ElementType.Paragraph } : element;
+  }
+
   Transforms.setNodes<SlateElement>(editor, newProperties);
 };
