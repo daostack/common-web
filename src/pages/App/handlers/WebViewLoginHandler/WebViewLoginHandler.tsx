9ac6c0dd8fc73e43447be09ed8f143dd474be1bd
Pavel Meyer <meyerpavel1207@gmail.com>
2024-01-30 16:05:21 +0300

    CW-Webview-fix added support dark theme to app

--- src/pages/App/handlers/WebViewLoginHandler/WebViewLoginHandler.tsx
+++ src/pages/App/handlers/WebViewLoginHandler/WebViewLoginHandler.tsx
@@ -2,7 +2,7 @@ import React, { FC, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { webviewLogin } from "@/pages/Auth/store/actions";
 import { history } from "@/shared/appConfig";
-import { WebviewActions } from "@/shared/constants";
+import { Theme, WebviewActions } from "@/shared/constants";
 import { FirebaseCredentials } from "@/shared/interfaces/FirebaseCredentials";
 import { getInboxPagePath } from "@/shared/utils";
 import firebase from "@/shared/utils/firebase";
@@ -33,6 +33,13 @@ const WebViewLoginHandler: FC = () => {
           payload: data,
           callback: (isLoggedIn) => {
             if (isLoggedIn) {
+              const isDarkThemePreferred = window.matchMedia(
+                `(prefers-color-scheme: ${Theme.Dark})`,
+              );
+
+              if (isDarkThemePreferred) {
+                window.ReactNativeWebView.postMessage(Theme.Dark);
+              }
               window.ReactNativeWebView.postMessage(
                 WebviewActions.loginSuccess,
               );
