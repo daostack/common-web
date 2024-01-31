cfdfb632cf0f7d6f88efcb0612ea469e0bee616b
Pavel Meyer <meyerpavel1207@gmail.com>
2024-01-31 16:19:13 +0300

    CW-2403 Fixed stream names

--- src/shared/components/Chat/ChatMessage/utils/generateInternalLink.tsx
+++ src/shared/components/Chat/ChatMessage/utils/generateInternalLink.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { DiscussionService, ProposalService } from "@/services";
+import CommonFeed from "@/services/CommonFeed";
 import { BASE_URL } from "@/shared/constants";
-import { Text } from "@/shared/models";
+import { CommonFeedType, Text } from "@/shared/models";
 import { getCommonPagePath } from "@/shared/utils";
 import { getCommon } from "./getCommon";
 import { handleCommonClick } from "./handleCommonClick";
@@ -15,6 +17,27 @@ export const getQueryParam = (path: string, key: string): string | null => {
   return urlParams.get(key);
 };
 
+const getStreamNameByFeedItemId = async (
+  commonId: string,
+  feedItemId: string | null,
+): Promise<string | undefined> => {
+  const feetItem = feedItemId
+    ? await CommonFeed.getCommonFeedItemById(commonId, feedItemId)
+    : null;
+
+  if (!feetItem?.data) {
+    return;
+  }
+
+  if (feetItem?.data.type === CommonFeedType.Discussion) {
+    const item = await DiscussionService.getDiscussionById(feetItem?.data.id);
+    return item?.title;
+  } else (feetItem?.data.type === CommonFeedType.Proposal) {
+    const item = await ProposalService.getProposalById(feetItem?.data.id);
+    return item?.discussion?.title;
+  }
+};
+
 export const generateInternalLink = async (text: string): Promise<Text> => {
   const commonPath = text.split("/").pop();
   if (text.startsWith(BASE_URL) && commonPath) {
@@ -23,13 +46,15 @@ export const generateInternalLink = async (text: string): Promise<Text> => {
     if (commonId) {
       const common = await getCommon(commonId);
       if (common?.id && common.name) {
+        const itemTitle = await getStreamNameByFeedItemId(commonId, itemId);
+
         return (
           <>
             {renderLink({
               to: getCommonPagePath(common?.id, {
                 ...(itemId && { item: itemId }),
               }),
-              name: common.name,
+              name: itemTitle ?? common.name,
               onClick: () =>
                 handleCommonClick(common?.id, common?.rootCommonId),
               className: styles.internalLink,
