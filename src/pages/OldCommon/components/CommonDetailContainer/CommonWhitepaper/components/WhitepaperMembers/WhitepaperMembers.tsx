6f831dfa58616f0b0eb390044ab8e2e1b3fbf94d
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2024-04-22 12:18:17 +0300

    remove rest circle type logic

--- src/pages/OldCommon/components/CommonDetailContainer/CommonWhitepaper/components/WhitepaperMembers/WhitepaperMembers.tsx
+++ src/pages/OldCommon/components/CommonDetailContainer/CommonWhitepaper/components/WhitepaperMembers/WhitepaperMembers.tsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import classNames from "classnames";
 import { selectGovernance } from "@/pages/OldCommon/store/selectors";
 import { ProposalsTypes } from "@/shared/constants";
-import { AllowedActions, AllowedProposals, CircleType } from "@/shared/models";
+import { AllowedActions, AllowedProposals } from "@/shared/models";
 import { getTextForProposalType } from "@/shared/utils";
 import { generateCirclesBinaryNumber } from "../../utils";
 import { getTextForAction, checkShouldRemoveAction } from "./helpers";
@@ -22,20 +22,17 @@ export default function WhitepaperMembers({ isSubCommon }: Props) {
 
   const members = Object.values(governance?.circles || {})
     .slice(0, 5)
-    .map((circle, index) => {
-      if (circle?.type !== CircleType.Project)
-        return (
-          <li
-            key={index}
-            onClick={() => setSelectedMember({ ...circle, index })}
-            className={classNames({
-              active: selectedMember?.name === circle.name,
-            })}
-          >
-            {circle.name}
-          </li>
-        );
-    });
+    .map((circle, index) => (
+      <li
+        key={index}
+        onClick={() => setSelectedMember({ ...circle, index })}
+        className={classNames({
+          active: selectedMember?.name === circle.name,
+        })}
+      >
+        {circle.name}
+      </li>
+    ));
 
   const renderContent = () => {
     const circle = Object.values(governance?.circles || {}).filter(
