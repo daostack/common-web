9ebd06f335c09f0592e298a16d36977f63eea247
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2024-04-29 14:16:00 +0300

    add basic logic for preview data loading

--- src/pages/common/components/ChatComponent/components/MessageLinkPreview/MessageLinkPreview.tsx
+++ src/pages/common/components/ChatComponent/components/MessageLinkPreview/MessageLinkPreview.tsx
@@ -1,9 +1,10 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { ButtonIcon } from "@/shared/components";
+import { useLoadingState } from "@/shared/hooks";
 import { Close2Icon } from "@/shared/icons";
-import { TextEditorValue } from "@/shared/ui-kit";
-import { emptyFunction, extractUrls } from "@/shared/utils";
+import { serializeTextEditorValue, TextEditorValue } from "@/shared/ui-kit";
+import { extractUrls } from "@/shared/utils";
 import { selectFilesPreview } from "@/store/states";
 import styles from "./MessageLinkPreview.module.scss";
 
@@ -11,18 +12,82 @@ interface MessageLinkPreviewProps {
   message: TextEditorValue;
 }
 
-const MessageLinkPreview: React.FC<MessageLinkPreviewProps> = ({ message }) => {
+interface PreviewData {
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+}
+
+const MessageLinkPreview: React.FC<MessageLinkPreviewProps> = (props) => {
+  const { message } = props;
   const filesPreview = useSelector(selectFilesPreview());
-  const urls = useMemo(() => extractUrls(JSON.stringify(message)), [message]);
-  const data = {
-    title: "David Kushner - Mr. Forgettable [Official Music Video]",
-    description:
-      "HEADLINE SHOWSOctober 18th - Brooklyn, NY - https://bit.ly/3SkMxSlOctober 21st - Los Angeles, CA - https://bit.ly/3BPd9UCOctober 26th - Chicago, IL - https:/...",
-    image: "https://i.ytimg.com/vi/7TCncxWNcPU/maxresdefault.jpg",
-    url: "https://youtu.be/7TCncxWNcPU?list=RD7TCncxWNcPU",
+  const [currentUrl, setCurrentUrl] = useState("");
+  const [
+    { loading: isPreviewDataLoading, data: previewData },
+    setPreviewDataState,
+  ] = useLoadingState<PreviewData | null>(null);
+  const urls = useMemo(
+    () => extractUrls(serializeTextEditorValue(message)),
+    [message],
+  );
+
+  const clearPreviewData = () => {
+    // TODO: clear request
+    setPreviewDataState({
+      loading: false,
+      fetched: false,
+      data: null,
+    });
+    // TODO: clear data in ChatComponent
   };
 
-  if (filesPreview && filesPreview.length > 0) {
+  useEffect(() => {
+    if (urls.length === 0) {
+      setCurrentUrl("");
+      return;
+    }
+    if (urls.includes(currentUrl)) {
+      return;
+    }
+
+    setCurrentUrl(urls[0]);
+  }, [urls]);
+
+  useEffect(() => {
+    if (!currentUrl) {
+      if (previewData) {
+        clearPreviewData();
+      }
+
+      return;
+    }
+
+    (async () => {
+      setPreviewDataState({
+        loading: true,
+        fetched: false,
+        data: null,
+      });
+      await new Promise((res) => setTimeout(res, 2000));
+      setPreviewDataState({
+        loading: false,
+        fetched: true,
+        data: {
+          title: "David Kushner - Mr. Forgettable [Official Music Video]",
+          description:
+            "HEADLINE SHOWSOctober 18th - Brooklyn, NY - https://bit.ly/3SkMxSlOctober 21st - Los Angeles, CA - https://bit.ly/3BPd9UCOctober 26th - Chicago, IL - https:/...",
+          image: "https://i.ytimg.com/vi/7TCncxWNcPU/maxresdefault.jpg",
+          url: "https://youtu.be/7TCncxWNcPU?list=RD7TCncxWNcPU",
+        },
+      });
+    })();
+  }, [currentUrl]);
+
+  if (
+    (filesPreview && filesPreview.length > 0) ||
+    (!isPreviewDataLoading && !previewData)
+  ) {
     return null;
   }
 
@@ -30,22 +95,42 @@ const MessageLinkPreview: React.FC<MessageLinkPreviewProps> = ({ message }) => {
     <div className={styles.container}>
       <a
         className={styles.linkContainer}
-        href={data.url}
+        href={previewData?.url || currentUrl}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img className={styles.image} src={data.image} alt={data.title} />
-        <div className={styles.infoContainer}>
-          <span className={styles.title} title={data.title}>
-            {data.title}
-          </span>
-          <span className={styles.description} title={data.description}>
-            {data.description}
-          </span>
-          <span className={styles.url}>{data.url}</span>
-        </div>
+        {previewData && (
+          <>
+            <img
+              className={styles.image}
+              src={previewData.image}
+              alt={previewData.title}
+            />
+            <div className={styles.infoContainer}>
+              <span className={styles.title} title={previewData.title}>
+                {previewData.title}
+              </span>
+              <span
+                className={styles.description}
+                title={previewData.description}
+              >
+                {previewData.description}
+              </span>
+              <span className={styles.url}>{previewData.url}</span>
+            </div>
+          </>
+        )}
+        {isPreviewDataLoading && (
+          <div className={styles.loadingContainer}>
+            <span className={styles.loadingText}>Loading...</span>
+            <span className={styles.url}>{currentUrl}</span>
+          </div>
+        )}
       </a>
-      <ButtonIcon className={styles.closeIconWrapper} onClick={emptyFunction}>
+      <ButtonIcon
+        className={styles.closeIconWrapper}
+        onClick={clearPreviewData}
+      >
         <Close2Icon />
       </ButtonIcon>
     </div>
