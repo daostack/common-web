5e63d16b0935c615cba5fd3d6a6bae11cfefb40a
Pavel Meyer <meyerpavel1207@gmail.com>
2024-07-30 12:54:10 +0300

    CW-long-breadcrumbs Added tooltip logic
    Added tooltip logic
    Updated tooltip component
    Optimized Breadcrumbs

--- src/shared/layouts/MultipleSpacesLayout/components/Header/components/Breadcrumbs/components/BreadcrumbsItem/BreadcrumbsItem.tsx
+++ src/shared/layouts/MultipleSpacesLayout/components/Header/components/Breadcrumbs/components/BreadcrumbsItem/BreadcrumbsItem.tsx
@@ -1,7 +1,13 @@
-import React, { FC, useRef } from "react";
+import React, { FC, useRef, useMemo } from "react";
 import { useHistory } from "react-router";
+import { useMeasure } from "react-use";
 import { useRoutesContext } from "@/shared/contexts";
-import { ContextMenuRef } from "@/shared/ui-kit";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  ContextMenuRef,
+} from "@/shared/ui-kit";
 import { ProjectsStateItem } from "@/store/states";
 import { truncateBreadcrumbName } from "../../utils";
 import { BreadcrumbsMenu } from "../BreadcrumbsMenu";
@@ -18,6 +24,9 @@ export interface BreadcrumbsItemProps {
   onClick?: () => void;
 }
 
+const PADDING = 24;
+const OVERLAY_THRESHOLD = 5;
+
 const BreadcrumbsItem: FC<BreadcrumbsItemProps> = (props) => {
   const {
     activeItem,
@@ -30,8 +39,18 @@ const BreadcrumbsItem: FC<BreadcrumbsItemProps> = (props) => {
     onClick,
   } = props;
   const history = useHistory();
+  const [containerRef, { width: containerWidth }] = useMeasure<HTMLLIElement>();
+  const [buttonRef, { width: buttonWidth }] = useMeasure<HTMLButtonElement>();
+
+  const hasOverlay = useMemo(() => {
+    return (
+      Math.abs(
+        Math.floor(containerWidth) - Math.floor(buttonWidth + PADDING),
+      ) >= OVERLAY_THRESHOLD
+    );
+  }, [containerWidth, buttonWidth]);
+
   const { getCommonPagePath } = useRoutesContext();
-  const containerRef = useRef<HTMLLIElement>(null);
   const contextMenuRef = useRef<ContextMenuRef>(null);
 
   const handleButtonClick = () => {
@@ -40,17 +59,26 @@ const BreadcrumbsItem: FC<BreadcrumbsItemProps> = (props) => {
       onClick?.();
       return;
     }
-    if (containerRef.current) {
-      const { x, y, height } = containerRef.current.getBoundingClientRect();
-      contextMenuRef.current?.open(x, y + height);
-    }
   };
 
   return (
     <li ref={containerRef} className={styles.li}>
-      <button className={styles.button} onClick={handleButtonClick}>
-        {truncate ? truncateBreadcrumbName(activeItem.name) : activeItem.name}
-      </button>
+      <Tooltip placement="bottom-start">
+        <TooltipTrigger asChild>
+          <button
+            ref={buttonRef}
+            className={styles.button}
+            onClick={handleButtonClick}
+          >
+            {truncate
+              ? truncateBreadcrumbName(activeItem.name)
+              : activeItem.name}
+          </button>
+        </TooltipTrigger>
+        <TooltipContent withOverlay={hasOverlay}>
+          <span className={styles.tooltipContent}>{activeItem.name}</span>
+        </TooltipContent>
+      </Tooltip>
       {withMenu && (
         <BreadcrumbsMenu
           ref={contextMenuRef}
