a5d19af53ebdd41cb0a3de21ee631f95f211276a
Pavel Meyer <meyerpavel1207@gmail.com>
2024-08-09 11:11:39 +0300

    CW-mobile-performance Fix TreeItemTrigger component

--- src/shared/layouts/SidenavLayout/components/SidenavContent/components/ProjectsTree/components/TreeItem/TreeItem.tsx
+++ src/shared/layouts/SidenavLayout/components/SidenavContent/components/ProjectsTree/components/TreeItem/TreeItem.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useState, useMemo } from "react";
+import React, { FC, useCallback, useEffect, useState } from "react";
 import classNames from "classnames";
 import { Item } from "../../types";
 import { TreeItemTrigger } from "../TreeItemTrigger";
