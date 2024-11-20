2fa3c915f981ade58ab79f7b929d8ba5c3928981
Pavel Meyer <meyerpavel1207@gmail.com>
2024-11-20 15:59:02 +0300

    Update reducer.ts

--- src/store/states/common/reducer.ts
+++ src/store/states/common/reducer.ts
@@ -783,13 +783,13 @@ export const reducer = createReducer<CommonState, Action>(initialState)
       // Remove sharedFeedItemId from feedItems and pinnedFeedItems
       if (sharedFeedItemId) {
         nextState.feedItems[commonId].data =
-          nextState.feedItems[commonId]?.data &&
-          nextState.feedItems[commonId].data.filter(
+          nextState.feedItems?.[commonId]?.data &&
+          nextState.feedItems[commonId]?.data?.filter(
             (item) => item?.itemId !== sharedFeedItemId,
           );
         nextState.pinnedFeedItems[commonId].data =
-          nextState.pinnedFeedItems[commonId]?.data &&
-          nextState.pinnedFeedItems[commonId].data.filter(
+          nextState.pinnedFeedItems?.[commonId]?.data &&
+          nextState.pinnedFeedItems[commonId]?.data?.filter(
             (item) => item?.itemId !== sharedFeedItemId,
           );
       }
